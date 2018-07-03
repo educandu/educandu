434d91847acde81b2f97d2f9323fcd35901ac9af
Andreas Helmberger <public@ahelmberger.de>
2018-07-03 16:52:20 +0200

    Make grayscale images work with CSS

--- src/components/pages/index.jsx
+++ src/components/pages/index.jsx
@@ -1,51 +1,13 @@
 const React = require('react');
 const { Input } = require('antd');
-const HoverImage = require('../hover-image.jsx');
 
 const { Search } = Input;
-
-const categories = [
-  {
-    key: 'Musikhochschule',
-    src: '/images/Musikhochschule-grau.png',
-    hoverSrc: '/images/Musikhochschule-farbig.png'
-  },
-  {
-    key: 'Schule',
-    src: '/images/Schule-grau.png',
-    hoverSrc: '/images/Schule-farbig.png'
-  },
-  {
-    key: 'Musikschule',
-    src: '/images/Musikschule-grau.png',
-    hoverSrc: '/images/Musikschule-farbig.png'
-  },
-  {
-    key: 'Materialkiste',
-    src: '/images/Materialkiste-grau.png',
-    hoverSrc: '/images/Materialkiste-farbig.png'
-  }
-];
 
 function goToDocs() {
   document.location = '/docs';
 }
 
 function Index() {
-  const categoryElements = categories.map(img => {
-    return (
-      <div key={img.key} className="IndexPage-category">
-        <a className="IndexPage-categoryLink" href="/docs">
-          <HoverImage
-            imageClass="IndexPage-categoryImage"
-            src={img.src}
-            hoverSrc={img.hoverSrc}
-            />
-        </a>
-      </div>
-    );
-  });
-
   return (
     <main className="IndexPage">
       <div className="IndexPage-content">
@@ -59,7 +21,26 @@ function Index() {
             />
         </div>
         <div className="IndexPage-categories">
-          {categoryElements}
+          <div className="IndexPage-category">
+            <a className="IndexPage-categoryLink" href="/docs">
+              <img className="IndexPage-categoryImage u-img-color-flip" src="/images/Musikhochschule.png" />
+            </a>
+          </div>
+          <div className="IndexPage-category">
+            <a className="IndexPage-categoryLink" href="/docs">
+              <img className="IndexPage-categoryImage u-img-color-flip" src="/images/Schule.png" />
+            </a>
+          </div>
+          <div className="IndexPage-category">
+            <a className="IndexPage-categoryLink" href="/docs">
+              <img className="IndexPage-categoryImage u-img-color-flip" src="/images/Musikschule.png" />
+            </a>
+          </div>
+          <div className="IndexPage-category">
+            <a className="IndexPage-categoryLink" href="/docs">
+              <img className="IndexPage-categoryImage u-img-color-flip" src="/images/Materialkiste.png" />
+            </a>
+          </div>
         </div>
       </div>
     </main>
