70f91882f7949b79eeca1355c3da93dcf7bd978a
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2018-09-21 15:09:55 +0200

    Implement ant collapse component

--- src/plugins/credit/display/credit-display.jsx
+++ src/plugins/credit/display/credit-display.jsx
@@ -1,9 +1,12 @@
 const React = require('react');
+const Collapse = require('antd/lib/collapse');
 const autoBind = require('auto-bind');
 const PropTypes = require('prop-types');
 const { inject } = require('../../../components/container-context.jsx');
 const { sectionDisplayProps } = require('../../../ui/default-prop-types');
 const GithubFlavoredMarkdown = require('../../../common/github-flavored-markdown');
+
+const { Panel } = Collapse;
 
 class CreditDisplay extends React.PureComponent {
   constructor(props) {
@@ -13,13 +16,14 @@ class CreditDisplay extends React.PureComponent {
 
   render() {
     const { content, githubFlavoredMarkdown } = this.props;
-    const html = githubFlavoredMarkdown.render(content.text);
+    const html = githubFlavoredMarkdown.render(content.text || '');
 
     return (
-      <div
-        className="Credit"
-        dangerouslySetInnerHTML={{ __html: html }}
-        />
+      <Collapse accordion>
+        <Panel header="Credits" key="1">
+          <p className="Credit" dangerouslySetInnerHTML={{ __html: html }} />
+        </Panel>
+      </Collapse>
     );
   }
 }
