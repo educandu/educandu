5486dc7eb7918717a4fb532229953375b3c32509
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2018-09-29 12:44:01 +0200

    Add footer on index side and styles

--- src/components/page-footer.jsx
+++ src/components/page-footer.jsx
@@ -1,23 +1,17 @@
 const React = require('react');
 const urls = require('../utils/urls');
-const Anchor = require('antd/lib/anchor');
-const classNames = require('classnames');
-
-const { Link } = Anchor;
 
 function PageFooter() {
-  const classes = classNames({
-    PageFooter: true,
-  });
 
   return (
-    <footer className={classes}>
-      <Anchor>
-        <Link className="PageFooter-link" href={urls.getArticleUrl('ueber-elmu')} title="Über ELMU" />
-        <Link className="PageFooter-link" href={urls.getArticleUrl('organisation')} title="Organisation" />
-        <Link className="PageFooter-link" href={urls.getArticleUrl('nutzungsvertrag')} title="Nutzungsvertrag" />
-        <Link className="PageFooter-link" href={urls.getArticleUrl('datenschutz')} title="Datenschutz" />
-      </Anchor>
+    <footer className="PageFooter">
+      <a href={urls.getArticleUrl('ueber-elmu')}>Über ELMU</a>
+      <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+      <a href={urls.getArticleUrl('organisation')}>Organisation</a>
+      <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+      <a href={urls.getArticleUrl('nutzungsvertrag')}>Nutzungsvertrag</a>
+      <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+      <a href={urls.getArticleUrl('datenschutz')}>Datenschutz</a>
     </footer>
   );
 }
