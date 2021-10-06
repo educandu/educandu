b227c892a8cc0c6def42c8d287c49dec13300971
Laurentiu <laurentiu.ilici@truepill.com>
2021-10-06 14:56:19 +0200

    Add migration for image component structure change, solves ELMU-#119

---
+++ scripts/migrations/2021-10-05-01-migrate-image-section-schema.js
@@ -0,0 +1,90 @@
+import { EFFECT_TYPE } from '../../src/plugins/image/constants';
+import { updateAll } from './helpers';
+
+const updateToNewContentStructure = content => {
+  content.sourceType = content.type;
+  delete content.type;
+  content.sourceUrl = content.url;
+  delete content.url;
+
+  if (content.hover) {
+    content.effect = {
+      ...content.hover,
+      sourceType: content.hover.type,
+      type: EFFECT_TYPE.hover,
+      sourceUrl: content.hover.url
+    };
+
+    delete content.hover;
+    delete content.effect.url;
+  }
+};
+
+const updateToOldContentStructure = content => {
+  content.type = content.sourceType;
+  delete content.sourceType;
+
+  content.url = content.sourceUrl;
+  delete content.sourceUrl;
+
+  if (content.effect && content.effect.type === EFFECT_TYPE.hover) {
+    content.hover = {
+      ...content.effect,
+      type: content.effect.sourceType,
+      url: content.effect.sourceUrl
+    };
+
+    delete content.hover.sourceType;
+    delete content.hover.sourceUrl;
+  }
+
+  delete content.effect;
+};
+
+class Migration2021100601 {
+  constructor(db) {
+    this.db = db;
+  }
+
+  async up() {
+    await updateAll(this.db.getCollection('documents'), { 'sections.type': 'image' }, doc => {
+      doc.sections.forEach(section => {
+        if (section.type !== 'image') {
+          return;
+        }
+        updateToNewContentStructure(section.content);
+      });
+    });
+
+    await updateAll(this.db.getCollection('documentRevisions'), { 'sections.type': 'image' }, doc => {
+      doc.sections.forEach(section => {
+        if (section.type !== 'image') {
+          return;
+        }
+        updateToNewContentStructure(section.content);
+      });
+    });
+  }
+
+  async down() {
+    await updateAll(this.db.getCollection('documents'), { 'sections.type': 'image' }, doc => {
+      doc.sections.forEach(section => {
+        if (section.type !== 'image') {
+          return;
+        }
+        updateToOldContentStructure(section.content);
+      });
+    });
+
+    await updateAll(this.db.getCollection('documentRevisions'), { 'sections.type': 'image' }, doc => {
+      doc.sections.forEach(section => {
+        if (section.type !== 'image') {
+          return;
+        }
+        updateToOldContentStructure(section.content);
+      });
+    });
+  }
+}
+
+export default Migration2021100601;
