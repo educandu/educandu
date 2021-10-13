41c58a432e42e303d2202b7da9eead59f555baa1
Andreas Helmberger <public@ahelmberger.de>
2021-10-13 17:27:09 +0200

    ELMU #139 Unify modules used for memoization

--- src/utils/memoize-last.js
+++
@@ -1,8 +0,0 @@
-import mem from 'mem';
-import QuickLRU from 'quick-lru';
-
-function memoizeLast(func, maxSize, cacheKey) {
-  return mem(func, { cache: new QuickLRU({ maxSize: maxSize || 1 }), cacheKey });
-}
-
-export default memoizeLast;
