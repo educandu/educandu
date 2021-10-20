c6f30f565f3d060371dccf55429a3bc3fce7490e
Laurentiu <laurentiu.ilici@gmail.com>
2021-10-20 16:58:04 +0200

    ELMU #186 Default tags migration

---
+++ migrations/2021-10-20-01-create-tags-for-existing-document.js
@@ -0,0 +1,58 @@
+import { updateAll } from './helpers';
+import { deStopWords } from './de-stopwords';
+
+class Migration2021110201 {
+  constructor(db) {
+    this.db = db;
+  }
+
+  generateTags(revisionOrDocument) {
+    const isInGerman = revisionOrDocument.language.toLowerCase() === 'de';
+    const defaultTags = [isInGerman ? 'Musik' : 'music'];
+
+    if (!revisionOrDocument.slug || revisionOrDocument.slug.startsWith('http')) {
+      return defaultTags;
+    }
+
+    const splitByRegex = revisionOrDocument.slug.split(/(?:[_.\\/]|$)/);
+
+    const splitByDash = splitByRegex.reduce((acc, curr) => [...acc, ...curr.split('-')], []);
+
+    const stopWords = [...isInGerman ? deStopWords : []];
+
+    const tags = splitByDash
+      .filter(tag => tag.length > 2)
+      .map(tag => tag.toLowerCase().trim())
+      .filter(tag => !stopWords.some(x => x === tag));
+
+    if (!tags.length) {
+      return defaultTags;
+    }
+
+    return isInGerman
+      ? tags.map(tag => `${tag.slice(0, 1).toUpperCase()}${tag.slice(1).trim()}`)
+      : tags;
+  }
+
+  async up() {
+    await updateAll(this.db.collection('documentRevisions'), {}, revision => {
+      revision.tags = this.generateTags(revision);
+    });
+
+    await updateAll(this.db.collection('documents'), {}, doc => {
+      doc.tags = this.generateTags(doc);
+    });
+  }
+
+  async down() {
+    await updateAll(this.db.collection('documentRevisions'), {}, revision => {
+      delete revision.tags;
+    });
+
+    await updateAll(this.db.collection('documents'), {}, doc => {
+      delete doc.tags;
+    });
+  }
+}
+
+export default Migration2021110201;
