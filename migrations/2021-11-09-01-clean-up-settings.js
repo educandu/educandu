99bf93bb954d195dfc6f194bccf2a7a10614b4ae
Adriana Luchian <adriana.dumitras@gmail.de>
2021-11-09 16:22:53 +0100

    EDU-53 Add migration script to clean up settings

---
+++ migrations/2021-11-09-01-clean-up-settings.js
@@ -0,0 +1,25 @@
+class Migration2021110901 {
+  constructor(db) {
+    this.db = db;
+  }
+
+  async up() {
+    await this.db
+      .collection('settings')
+      .updateMany(
+        { _id: 'homeLanguages' },
+        { $unset: { 'value.$[].searchFieldButton': false, 'value.$[].searchFieldPlaceholder': false } }
+      );
+  }
+
+  async down() {
+    await this.db
+      .collection('settings')
+      .updateMany(
+        { _id: 'homeLanguages' },
+        { $set: { 'value.$[].searchFieldButton': 'Search', 'value.$[].searchFieldPlaceholder': 'Search' } }
+      );
+  }
+}
+
+export default Migration2021110901;
