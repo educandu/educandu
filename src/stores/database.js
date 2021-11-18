b6d7c3bd612a392a547111c08451a9335275cc79
Lau <laurentiu.ilici@gmail.com>
2021-11-18 16:03:07 +0100

    EDU-100 Exclude manually run files from db migrations

--- src/stores/database.js
+++ src/stores/database.js
@@ -94,6 +94,7 @@ class Database {
     const allFilesInMigrationDirectory = await pGlob(path.resolve(migrationsDirectory, './*.js'));
     const migrationFileNames = allFilesInMigrationDirectory
       .filter(fileName => MIGRATION_FILE_NAME_PATTERN.test(path.basename(fileName)))
+      .filter(filename => filename.includes('manually'))
       .sort();
 
     const migrations = await Promise.all(migrationFileNames.map(async fileName => {
