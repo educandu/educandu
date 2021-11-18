ccf30119119c8631adfd01eb356223a285c00eb6
Lau <laurentiu.ilici@gmail.com>
2021-11-18 10:10:04 +0100

    EDU-100 Take care of possible nulls

--- migrations/educandu-2021-11-17-02-migrate-cdn-resoures.js
+++ migrations/educandu-2021-11-17-02-migrate-cdn-resoures.js
@@ -90,12 +90,12 @@ export default class Educandu_2021_11_17_02_migrate_cdn_resources {
 
     await updateAll(this.db.collection('documents'), {}, (doc) => {
       const relevantSections = doc.sections.filter(section => pluginsWithCdnResources.has(section.type));
-      doc.cdnResourceUrls = [...new Set(aggregateSectionUrls(relevantSections))];
+      doc.cdnResourceUrls = [...new Set(aggregateSectionUrls(relevantSections).filter(url => !!url))];
     });
 
     await updateAll(this.db.collection('documentRevisions'), {}, (doc) => {
       const relevantSections = doc.sections.filter(section => pluginsWithCdnResources.has(section.type));
-      doc.cdnResourceUrls = [...new Set(aggregateSectionUrls(relevantSections))];
+      doc.cdnResourceUrls = [...new Set(aggregateSectionUrls(relevantSections).filter(url => !!url))];
     });
   }
 
