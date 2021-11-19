276418d36a99f5f0ac049144bf3a08906d488de1
Andreas Helmberger <public@ahelmberger.de>
2021-11-19 09:35:54 +0100

    EDU-106 Add permission tests for built-in user

--- src/domain/permissions.spec.js
+++ src/domain/permissions.spec.js
@@ -1,26 +1,54 @@
 import { ROLE } from './role.js';
+import { exchangeUser } from './built-in-users.js';
 import sut, { LIST_EXPORTABLE_CONTENT } from './permissions.js';
+
+const allNonTechnicalPermissions = Object.entries(sut)
+  .filter(entry => (/^[A-Z_]+$/).test(entry[0]))
+  .filter(entry => entry[1] !== LIST_EXPORTABLE_CONTENT)
+  .map(entry => entry[1]);
 
 describe('permissions', () => {
 
   describe('hasUserPermission', () => {
-    const user = {
-      roles: [ROLE.admin]
-    };
 
-    const permissions = Object.entries(sut)
-      .filter(entry => (/^[A-Z_]+$/).test(entry[0]))
-      .filter(entry => entry[1] !== LIST_EXPORTABLE_CONTENT)
-      .map(entry => entry[1]);
+    describe('when user has role "admin"', () => {
+      const adminUser = {
+        roles: [ROLE.admin]
+      };
 
-    permissions.forEach(permission => {
-      let result;
-      beforeEach(() => {
-        result = sut.hasUserPermission(user, permission);
+      allNonTechnicalPermissions.forEach(permission => {
+        let result;
+        beforeEach(() => {
+          result = sut.hasUserPermission(adminUser, permission);
+        });
+        it(`should be true for permission '${permission}'`, () => {
+          expect(result).toBe(true);
+        });
       });
-      it(`should be true for user in role '${ROLE.admin}' and permission '${permission}'`, () => {
-        expect(result).toBe(true);
+
+      it(`should be false for permission '${LIST_EXPORTABLE_CONTENT}'`, () => {
+        expect(sut.hasUserPermission(adminUser, LIST_EXPORTABLE_CONTENT)).toBe(false);
       });
+
+    });
+
+    describe('when user is builtin user "exchange"', () => {
+      const adminUser = exchangeUser;
+
+      allNonTechnicalPermissions.forEach(permission => {
+        let result;
+        beforeEach(() => {
+          result = sut.hasUserPermission(adminUser, permission);
+        });
+        it(`should be false for permission '${permission}'`, () => {
+          expect(result).toBe(false);
+        });
+      });
+
+      it(`should be true for permission '${LIST_EXPORTABLE_CONTENT}'`, () => {
+        expect(sut.hasUserPermission(adminUser, LIST_EXPORTABLE_CONTENT)).toBe(true);
+      });
+
     });
 
   });
