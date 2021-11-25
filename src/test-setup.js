b310cc5966bf8cb63649d02bca823f0a71abcc5d
Andreas Helmberger <public@ahelmberger.de>
2021-11-25 14:07:55 +0100

    NOTICKET: Remove obsolete logging test config

--- src/test-setup.js
+++ src/test-setup.js
@@ -3,4 +3,3 @@
 process.env.EDUCANDU_ENV = process.env.EDUCANDU_ENV || 'test';
 process.env.EDUCANDU_SKIP_MONGO_MIGRATIONS = process.env.EDUCANDU_SKIP_MONGO_MIGRATIONS || true.toString();
 process.env.EDUCANDU_SKIP_MONGO_CHECKS = process.env.EDUCANDU_SKIP_MONGO_CHECKS || true.toString();
-process.env.EDUCANDU_LOG_LEVEL = process.env.EDUCANDU_LOG_LEVEL || 'muted';
