4c65dcbea887f14953b0f7659c314eb8fb0e235e
Adriana Luchian <adriana.dumitras@gmail.de>
2021-12-03 14:07:47 +0100

    EDU-139 Uber-coverage boost

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,9 @@ export default {
     '<rootDir>/coverage/',
     '<rootDir>/dist/',
     '<rootDir>/test-app/',
+    '<rootDir>/src/components/',
+    '<rootDir>/src/plugins/',
+    '<rootDir>/src/stores/collection-specs/',
     '<rootDir>/src/test-helper\\.js',
     '<rootDir>/src/test-setup\\.js'
   ],
