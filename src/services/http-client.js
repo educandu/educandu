a02321ec6c6921f1b34e065c6be77b82c1d88f04
Adriana Luchian <adriana.dumitras@gmail.de>
2021-12-08 18:56:32 +0100

    EDU-155 Rethrow data part of error response from axios

--- src/services/http-client.js
+++ src/services/http-client.js
@@ -2,23 +2,33 @@ import axios from 'axios';
 
 class HttpClient {
   get(...args) {
-    return axios.get(...args);
+    return axios.get(...args).catch(error => {
+      throw error.response.data;
+    });
   }
 
   put(...args) {
-    return axios.put(...args);
+    return axios.put(...args).catch(error => {
+      throw error.response.data;
+    });
   }
 
   post(...args) {
-    return axios.post(...args);
+    return axios.post(...args).catch(error => {
+      throw error.response.data;
+    });
   }
 
   patch(...args) {
-    return axios.patch(...args);
+    return axios.patch(...args).catch(error => {
+      throw error.response.data;
+    });
   }
 
   delete(...args) {
-    return axios.delete(...args);
+    return axios.delete(...args).catch(error => {
+      throw error.response.data;
+    });
   }
 }
 
