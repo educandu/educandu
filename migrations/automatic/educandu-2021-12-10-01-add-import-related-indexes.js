7498d77b50b1a8c4dae40d33ae84aa968a3079ed
Lau <laurentiu.ilici@gmail.com>
2021-12-13 17:00:22 +0100

    EDU-166 Add extra indexes

--- migrations/automatic/educandu-2021-12-10-01-add-import-related-indexes.js
+++ migrations/automatic/educandu-2021-12-10-01-add-import-related-indexes.js
@@ -9,11 +9,21 @@ export default class Educandu_2021_12_10_01_add_import_related_indexes {
       {
         name: '_idx_batch_id_',
         key: { batchId: 1 }
+      },
+      {
+        name: '_idx_batch_id_processed_',
+        key: { batchId: 1, processed: 1 }
+      },
+      {
+        name: '_idx_task_id_processed_',
+        key: { taskId: 1, processed: 1 }
       }
     ]);
   }
 
   async down() {
     await this.db.collection('tasks').dropIndex('_idx_batch_id_');
+    await this.db.collection('tasks').dropIndex('_idx_batch_id_processed_');
+    await this.db.collection('tasks').dropIndex('_idx_task_id_processed_');
   }
 }
