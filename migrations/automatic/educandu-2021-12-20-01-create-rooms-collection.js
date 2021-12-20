31acfcce4c08a4d53926cf37650cfadfb619b9b1
Lau <laurentiu.ilici@gmail.com>
2021-12-20 10:44:12 +0100

    EDU-167 Add rooms related migration

---
+++ migrations/automatic/educandu-2021-12-20-01-create-rooms-collection.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line camelcase
+export default class Educandu_2021_12_20_01_create_rooms_colletion {
+  constructor(db) {
+    this.db = db;
+  }
+
+  async up() {
+    await this.db.createCollection('rooms');
+    await this.db.createCollection('roomInvitations');
+
+    await this.db.collection('rooms').createIndexes([
+      {
+        name: '_idx_owner_',
+        key: { owner: 1 }
+      },
+      {
+        name: '_idx_members_user_id',
+        key: { 'members.userId': 1 }
+      }
+    ]);
+
+    await this.db.collection('roomInvitations').createIndexes([
+      {
+        name: '_idx_expires_',
+        key: { expires: 1 },
+        expireAfterSeconds: 0
+      }
+    ]);
+  }
+
+  async down() {
+    await this.db.dropCollection('roomInvitations');
+    await this.db.dropCollection('rooms');
+  }
+}
