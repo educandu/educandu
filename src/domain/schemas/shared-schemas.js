e75431bc7af4b27ebd829590438b02aaabff63be
Adriana Luchian <adriana.dumitras@gmail.de>
2021-12-22 14:00:11 +0100

    EDU-204 Correct joi validation for http request booleans

--- src/domain/schemas/shared-schemas.js
+++ src/domain/schemas/shared-schemas.js
@@ -1,5 +1,5 @@
 import joi from 'joi';
 
 export const idOrKeySchema = joi.string().alphanum().min(15).max(30);
-export const boolStringSchema = joi.any().allow('true', 'false');
+export const boolStringSchema = joi.any().valid('true', 'false', true, false);
 
