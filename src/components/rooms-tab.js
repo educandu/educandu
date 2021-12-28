b9d9c080f9da6bf9b2f350056d73d47673dcc069
Adriana Luchian <adriana.dumitras@gmail.de>
2021-12-28 11:15:44 +0100

    EDU-205 Extract room creation modal

--- src/components/rooms-tab.js
+++ src/components/rooms-tab.js
@@ -1,63 +1,23 @@
 import by from 'thenby';
 import PropTypes from 'prop-types';
 import urls from '../utils/urls.js';
-import Logger from '../common/logger.js';
+import { Button, Table } from 'antd';
+import React, { useState } from 'react';
 import Restricted from './restricted.js';
 import { useUser } from './user-context.js';
 import { useTranslation } from 'react-i18next';
-import errorHelper from '../ui/error-helper.js';
-import React, { useState, useRef } from 'react';
 import { PlusOutlined } from '@ant-design/icons';
 import permissions from '../domain/permissions.js';
-import { useService } from './container-context.js';
 import { useDateFormat } from './language-context.js';
 import { roomShape } from '../ui/default-prop-types.js';
-import RoomApiClient from '../services/room-api-client.js';
-import { ROOM_ACCESS_LEVEL } from '../common/constants.js';
-import { Form, Button, Table, Modal, Input, Radio } from 'antd';
-
-const FormItem = Form.Item;
-const RadioGroup = Radio.Group;
-const RadioButton = Radio.Button;
-
-const logger = new Logger(import.meta.url);
+import RoomCreationModal from './room-creation-modal.js';
 
 function RoomsTab({ rooms }) {
   const user = useUser();
-  const newRoomFormRef = useRef(null);
   const { formatDate } = useDateFormat();
   const { t } = useTranslation('roomsTab');
-  const roomApiClient = useService(RoomApiClient);
 
-  const roomNameValidationRules = [
-    {
-      required: true,
-      message: t('roomNameRequired'),
-      whitespace: true
-    }
-  ];
-
-  const isNewRoomFormValid = async () => {
-    try {
-      await newRoomFormRef.current.validateFields(['name'], { force: true });
-      return true;
-    } catch {
-      return false;
-    }
-  };
-
-  const createNewRoomState = () => {
-    return {
-      name: t('newRoom'),
-      access: ROOM_ACCESS_LEVEL.private
-    };
-  };
-
-  const [state, setState] = useState({
-    isNewRoomModalVisible: false,
-    isNewRoomBeingCreated: false,
-    newRoom: createNewRoomState()
-  });
+  const [isRoomCreationModalVisible, setIsRoomCreationModalVisible] = useState(false);
 
   const renderName = (name, room) => {
     return <a href={urls.getRoomUrl(room._id)}>{name}</a>;
@@ -78,47 +38,15 @@ function RoomsTab({ rooms }) {
   };
 
   const renderAccess = access => {
-    return <span>{t(`accessType_${access}`)}</span>;
+    return <span>{t(`common:accessType_${access}`)}</span>;
   };
 
-  const handleNewRoomClick = () => {
-    const newRoom = createNewRoomState();
-    setState(prevState => ({ ...prevState, isNewRoomModalVisible: true, newRoom }));
-    if (newRoomFormRef.current) {
-      newRoomFormRef.current.setFieldsValue({ name: newRoom.name });
-    }
+  const handleCreateRoomClick = () => {
+    setIsRoomCreationModalVisible(true);
   };
 
-  const handleNewRoomOk = async () => {
-    try {
-      const isValid = await isNewRoomFormValid();
-      if (!isValid) {
-        return;
-      }
-
-      setState(prevState => ({ ...prevState, isNewRoomBeingCreated: true }));
-      const newRoom = await roomApiClient.addRoom({ name: state.newRoom.name, access: state.newRoom.access });
-      setState(prevState => ({ ...prevState, isNewRoomBeingCreated: false, isNewRoomModalVisible: false }));
-
-      window.location = urls.getRoomDetailsUrl(newRoom._id);
-    } catch (error) {
-      errorHelper.handleApiError({ error, logger, t });
-      setState(prevState => ({ ...prevState, isNewRoomBeingCreated: false }));
-    }
-  };
-
-  const handleNewRoomCancel = () => {
-    setState(prevState => ({ ...prevState, isNewRoomModalVisible: false }));
-  };
-
-  const handleNewRoomNameChange = event => {
-    const { value } = event.target;
-    setState(prevState => ({ ...prevState, newRoom: { ...prevState.newRoom, name: value } }));
-  };
-
-  const handleRoomAccessChange = event => {
-    const { value } = event.target;
-    setState(prevState => ({ ...prevState, newRoom: { ...prevState.newRoom, access: value } }));
+  const handleRoomCreationModalClose = () => {
+    setIsRoomCreationModalVisible(false);
   };
 
   const ownedRooms = rooms.filter(room => room.owner._id === user._id);
@@ -126,7 +54,7 @@ function RoomsTab({ rooms }) {
 
   const ownedRoomsColumns = [
     {
-      title: t('name'),
+      title: t('common:name'),
       dataIndex: 'name',
       key: 'name',
       render: renderName,
@@ -139,10 +67,10 @@ function RoomsTab({ rooms }) {
       render: renderCreatedOn,
       defaultSortOrder: 'descend',
       sorter: by(x => x.createdOn),
-      width: '150px'
+      width: '200px'
     },
     {
-      title: t('access'),
+      title: t('common:access'),
       dataIndex: 'access',
       key: 'access',
       render: renderAccess,
@@ -153,7 +81,7 @@ function RoomsTab({ rooms }) {
 
   const membershipRoomsColumns = [
     {
-      title: t('name'),
+      title: t('common:name'),
       dataIndex: 'name',
       key: 'name',
       render: renderName,
@@ -174,10 +102,10 @@ function RoomsTab({ rooms }) {
       render: renderJoinedOn,
       defaultSortOrder: 'descend',
       sorter: by(x => x.joinedOn),
-      width: '150px'
+      width: '200px'
     },
     {
-      title: t('access'),
+      title: t('common:access'),
       dataIndex: 'access',
       key: 'access',
       render: renderAccess,
@@ -214,34 +142,21 @@ function RoomsTab({ rooms }) {
         <h2>{t('ownedRoomsHeader')}</h2>
         <Table dataSource={ownedRoomsRows} columns={ownedRoomsColumns} size="middle" />
         <Restricted to={permissions.CREATE_ROOMS}>
-          <Button className="RoomsTab-newRoomButton" type="primary" shape="circle" icon={<PlusOutlined />} size="large" onClick={handleNewRoomClick} />
+          <Button
+            size="large"
+            type="primary"
+            shape="circle"
+            icon={<PlusOutlined />}
+            onClick={handleCreateRoomClick}
+            className="RoomsTab-createRoomButton"
+            />
         </Restricted>
       </section>
       <section className="RoomsTab-section">
         <h2>{t('joinedRoomsHeader')}</h2>
         <Table dataSource={membershipRoomsRows} columns={membershipRoomsColumns} size="middle" />
       </section>
-
-      <Modal
-        title={t('newRoom')}
-        onOk={handleNewRoomOk}
-        onCancel={handleNewRoomCancel}
-        maskClosable={false}
-        visible={state.isNewRoomModalVisible}
-        okButtonProps={{ loading: state.isNewRoomBeingCreated }}
-        >
-        <Form name="new-room-form" ref={newRoomFormRef} layout="vertical">
-          <FormItem label={t('name')} name="name" rules={roomNameValidationRules} initialValue={state.newRoom.name}>
-            <Input onChange={handleNewRoomNameChange} />
-          </FormItem>
-          <FormItem label={t('access')}>
-            <RadioGroup value={state.newRoom.access} onChange={handleRoomAccessChange}>
-              <RadioButton value={ROOM_ACCESS_LEVEL.private}>{t('accessType_private')}</RadioButton>
-              <RadioButton value={ROOM_ACCESS_LEVEL.public}>{t('accessType_public')}</RadioButton>
-            </RadioGroup>
-          </FormItem>
-        </Form>
-      </Modal>
+      <RoomCreationModal isVisible={isRoomCreationModalVisible} onClose={handleRoomCreationModalClose} />
     </div>
   );
 }
