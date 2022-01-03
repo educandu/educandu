ed7e7aa15640be53c17d09a643dad7bd15cc35e9
Andreas Helmberger <public@ahelmberger.de>
2022-01-03 16:23:14 +0100

    EDU-227 Always show markdown with same styling

--- src/plugins/annotation/display/annotation-display.js
+++ src/plugins/annotation/display/annotation-display.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import { Collapse } from 'antd';
-import ClientConfig from '../../../bootstrap/client-config.js';
-import { useService } from '../../../components/container-context.js';
+import Markdown from '../../../components/markdown.js';
 import { sectionDisplayProps } from '../../../ui/default-prop-types.js';
-import GithubFlavoredMarkdown from '../../../common/github-flavored-markdown.js';
 
 const { Panel } = Collapse;
 
 function AnnotationDisplay({ content }) {
-  const gfm = useService(GithubFlavoredMarkdown);
-  const { cdnRootUrl } = useService(ClientConfig);
-
-  const html = gfm.render(content.text || '', { cdnRootUrl });
-
   return (
-    <Collapse accordion>
-      <Panel header={content.title || '\u00A0'} key="1">
-        <p className="Annotation" dangerouslySetInnerHTML={{ __html: html }} />
-      </Panel>
-    </Collapse>
+    <div className="Annotation">
+      <Collapse accordion>
+        <Panel header={content.title || '\u00A0'} key="1">
+          <Markdown>{content.text}</Markdown>
+        </Panel>
+      </Collapse>
+    </div>
   );
 }
 
