001ed7f7f7d188b8f6148ee1806a885c175ad5f8
Lau <laurentiu.ilici@gmail.com>
2022-01-04 17:21:21 +0100

    EDU-232 Move slug tests and add section schema tests

--- src/domain/schemas/document-schemas.spec.js
+++ src/domain/schemas/document-schemas.spec.js
@@ -1,5 +1,4 @@
 import { validate } from '../validation.js';
-import { slugSchema } from './shared-schemas.js';
 import { createRevisionBodySchema } from './document-schemas.js';
 
 describe('createRevisionBodySchema', () => {
@@ -99,33 +98,6 @@ describe('createRevisionBodySchema', () => {
   invalidTestCases.forEach(({ description, data }) => {
     describe(`When called with ${description}`, () => {
       it('should fail validation', () => expect(() => validate(data, createRevisionBodySchema)).toThrow());
-    });
-  });
-});
-
-describe('slugSchema', () => {
-  const testCases = [
-    { description: 'trailing slash', slug: 'slug/', shouldSucceed: false },
-    { description: 'double slash', slug: 'slug//slug', shouldSucceed: false },
-    { description: 'upper case characters', slug: 'SLUUUUG', shouldSucceed: false },
-    { description: 'slash followed by hyphon', slug: 'slug/-', shouldSucceed: true },
-    { description: 'trailing hyphon', slug: 'slug-', shouldSucceed: true },
-    { description: 'hypho slash hyphon', slug: 'slug-/-', shouldSucceed: true },
-    { description: 'lower case chars and digits group', slug: 'slug123', shouldSucceed: true },
-    { description: 'lower case chars and digits group separated by hyphon', slug: 'slug123-slug123', shouldSucceed: true },
-    { description: 'lower case chars and digits group separated by hyphon and slash', slug: 'slug123-slug123/abc', shouldSucceed: true },
-    { description: 'multiple valid groups separated by slashes', slug: 'slug123-slug123/abc-abc123-abc/def-def', shouldSucceed: true }
-  ];
-
-  testCases.forEach(({ description, slug, shouldSucceed }) => {
-    describe(description, () => {
-      it(`should ${shouldSucceed ? 'not throw' : 'throw'}`, () => {
-        if (shouldSucceed) {
-          expect(() => validate(slug, slugSchema)).not.toThrow();
-        } else {
-          expect(() => validate(slug, slugSchema)).toThrow();
-        }
-      });
     });
   });
 });
