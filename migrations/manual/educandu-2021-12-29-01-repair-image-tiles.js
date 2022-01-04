af94573ee6a2dcf34e043526f649030cf18ec6ee
Lau <laurentiu.ilici@gmail.com>
2022-01-04 12:39:24 +0100

    EDU-222 Extract variables on the repair image tiles migration

--- migrations/manual/educandu-2021-12-29-01-repair-image-tiles.js
+++ migrations/manual/educandu-2021-12-29-01-repair-image-tiles.js
@@ -23,22 +23,25 @@ export default class Educandu_2021_12_29_01_repair_image_tiles {
         console.log(documentLinkInfo);
         console.log('Found unexpected number of tile sections', imageTilesSections.length);
       }
+      const tiles = imageTilesSections[0].content.tiles;
 
-      if (imageTilesSections[0].content.tiles.length < documentLinkInfo.tileIndex + 1) {
+      if (tiles.length < documentLinkInfo.tileIndex + 1) {
         console.log(documentLinkInfo);
-        console.log('Number of tile sections', imageTilesSections[0].content.tiles.length, 'tile index: ', documentLinkInfo.tileIndex, 'revision', docOrRevision._id);
+        console.log('Number of tile sections', tiles.length, 'tile index: ', documentLinkInfo.tileIndex, 'revision', docOrRevision._id);
       }
       // eslint-disable-next-line no-await-in-loop
       const doc = await this.db.collection('documents').findOne({ slug: documentLinkInfo.url });
 
-      if (imageTilesSections[0].content.tiles[documentLinkInfo.tileIndex]?.link.type !== 'internal') {
-        console.log(`Document info tile is not of expected type, but of type ${imageTilesSections[0].content.tiles[documentLinkInfo.tileIndex].link.type}`);
+      const linkType= tiles[documentLinkInfo.tileIndex]?.link.type;
+
+      if (linkType !== 'internal') {
+        console.log(`Document info tile is not of expected type, but of type ${linkType}`);
         console.log(documentLinkInfo);
       } else if (!doc) {
         console.log('Could not find document for slug: ', documentLinkInfo.url);
-        imageTilesSections[0].content.tiles[documentLinkInfo.tileIndex].link.url = '';
+        tiles[documentLinkInfo.tileIndex].link.url = '';
       } else {
-        imageTilesSections[0].content.tiles[documentLinkInfo.tileIndex].link.url = doc.key;
+        tiles[documentLinkInfo.tileIndex].link.url = doc.key;
       }
 
       // eslint-disable-next-line no-await-in-loop
