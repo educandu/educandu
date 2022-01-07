b8c3d6319a5bbe62f1a1f7ebddd411ffbd5aa988
Lau <laurentiu.ilici@gmail.com>
2022-01-07 14:22:49 +0100

    EDU-199 Fix proptypes for invitation shape to expect Date

--- src/ui/default-prop-types.js
+++ src/ui/default-prop-types.js
@@ -247,6 +247,6 @@ export const roomShape = PropTypes.shape({
 
 export const invitationShape = PropTypes.shape({
   email: PropTypes.string.isRequired,
-  sentOn: PropTypes.string.isRequired,
-  expires: PropTypes.string.isRequired
+  sentOn: PropTypes.instanceOf(Date).isRequired,
+  expires: PropTypes.instanceOf(Date).isRequired
 });
