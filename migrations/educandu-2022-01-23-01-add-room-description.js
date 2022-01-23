4a48954b611d97a2ec890a62189108ebe39b558d
Adriana Luchian <adriana.dumitras@gmail.de>
2022-01-23 11:47:28 +0100

    EDU-277 Add migration script for room description

---
+++ migrations/educandu-2022-01-23-01-add-room-description.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line camelcase
+export default class Educandu_2022_01_23_01_add_room_description {
+  constructor(db) {
+    this.db = db;
+  }
+
+  async up() {
+    await this.db.collection('rooms').updateMany({}, { $set: { description: '' } });
+  }
+
+  async down() {
+    await this.db.collection('rooms').updateMany({}, { $unset: { description: '' } });
+  }
+}
