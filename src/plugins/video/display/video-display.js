c70afba03b2504a192e689d2fbe9748b887885e9
Adriana Luchian <adriana.dumitras@gmail.de>
2022-02-08 22:59:07 +0100

    EDU-329 Convert video plugin to functional component

--- src/plugins/video/display/video-display.js
+++ src/plugins/video/display/video-display.js
@@ -1,139 +1,131 @@
-import React from 'react';
-import autoBind from 'auto-bind';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import reactPlayerNs from 'react-player';
 import { SOURCE_TYPE } from '../constants.js';
+import React, { useRef, useState } from 'react';
 import ClientConfig from '../../../bootstrap/client-config.js';
 import MediaControl from '../../../components/media-control.js';
-import { inject } from '../../../components/container-context.js';
+import { useService } from '../../../components/container-context.js';
+import { sectionDisplayProps } from '../../../ui/default-prop-types.js';
 import GithubFlavoredMarkdown from '../../../common/github-flavored-markdown.js';
-import { sectionDisplayProps, clientConfigProps } from '../../../ui/default-prop-types.js';
 
 const ReactPlayer = reactPlayerNs.default || reactPlayerNs;
 
-const playStates = {
-  INITIALIZING: 'initializing',
-  BUFFERING: 'buffering',
-  STOPPED: 'stopped',
-  PLAYING: 'playing',
-  PAUSING: 'pausing'
+const PLAY_STATES = {
+  initializing: 'initializing',
+  buffering: 'buffering',
+  stopped: 'stopped',
+  playing: 'playing',
+  pausing: 'pausing'
 };
 
-class VideoDisplay extends React.Component {
-  constructor(props) {
-    super(props);
-    autoBind(this);
-    this.playerRef = React.createRef();
-    this.state = {
-      playState: playStates.INITIALIZING,
-      durationInSeconds: 0,
-      playedSeconds: 0,
-      volume: 1
-    };
-  }
+function VideoDisplay({ content }) {
+  const playerRef = useRef();
+  const clientConfig = useService(ClientConfig);
+  const githubFlavoredMarkdown = useService(GithubFlavoredMarkdown);
 
-  handleReady() {
-    this.setState({ playState: playStates.STOPPED });
-  }
+  const [volume, setVolume] = useState(1);
+  const [playedSeconds, setPlayedSeconds] = useState(0);
+  const [durationInSeconds, setDurationInSeconds] = useState(0);
+  const [playState, setPlayState] = useState(PLAY_STATES.initializing);
 
-  handleBuffer() {
-    this.setState({ playState: playStates.BUFFERING });
-  }
+  const handleReady = () => {
+    setPlayState(PLAY_STATES.stopped);
+  };
 
-  handlePlay() {
-    this.setState({ playState: playStates.PLAYING });
-  }
+  const handleBuffer = () => {
+    setPlayState(PLAY_STATES.buffering);
+  };
 
-  handlePause() {
-    this.setState({ playState: playStates.PAUSING });
-  }
+  const handlePlay = () => {
+    setPlayState(PLAY_STATES.playing);
+  };
 
-  handleStop() {
-    this.setState({ playState: playStates.STOPPED });
-  }
+  const handlePause = () => {
+    setPlayState(PLAY_STATES.pausing);
+  };
 
-  handleMediaControlSeek(percentage) {
-    this.playerRef.current.seekTo(percentage);
-  }
+  const handleStop = () => {
+    setPlayState(PLAY_STATES.stopped);
+  };
 
-  handleMediaControlTogglePlay() {
-    this.setState(prevState => {
-      switch (prevState.playState) {
-        case playStates.INITIALIZING:
-          return { playState: playStates.STOPPED };
-        case playStates.BUFFERING:
-          return { playState: playStates.BUFFERING };
-        case playStates.PLAYING:
-          return { playState: playStates.PAUSING };
-        case playStates.PAUSING:
-        case playStates.STOPPED:
-          return { playState: playStates.PLAYING };
+  const handleMediaControlSeek = percentage => {
+    playerRef.current.seekTo(percentage);
+  };
+
+  const handleMediaControlTogglePlay = () => {
+    setPlayState(() => {
+      switch (playState) {
+        case PLAY_STATES.initializing:
+          return PLAY_STATES.stopped;
+        case PLAY_STATES.buffering:
+          return PLAY_STATES.buffering;
+        case PLAY_STATES.playing:
+          return PLAY_STATES.pausing;
+        case PLAY_STATES.pausing:
+        case PLAY_STATES.stopped:
+          return PLAY_STATES.playing;
         default:
-          throw new Error(`Invalid play state: ${prevState.playState}`);
+          throw new Error(`Invalid play state: ${playState}`);
       }
     });
+  };
+
+  const handleProgress = progress => {
+    setPlayedSeconds(progress.playedSeconds);
+  };
+
+  const handleDuration = duration => {
+    setDurationInSeconds(duration);
+  };
+
+  const handleVolumeChange = newVolume => {
+    setVolume(newVolume);
+  };
+
+  const html = githubFlavoredMarkdown.render(content.text || '');
+  const aspectRatio = content.aspectRatio || { h: 16, v: 9 };
+  const paddingTop = `${(aspectRatio.v / aspectRatio.h * 100).toFixed(2)}%`;
+  const width = content.width || 100;
+
+  let url;
+  switch (content.type) {
+    case SOURCE_TYPE.internal:
+      url = content.url ? `${clientConfig.cdnRootUrl}/${content.url}` : null;
+      break;
+    default:
+      url = content.url || null;
+      break;
   }
 
-  handleProgress({ playedSeconds }) {
-    this.setState({ playedSeconds });
-  }
+  const renderMediaControl = () => {
+    const classes = classNames(['VideoDisplay-mediaControlContainer', `u-width-${width}`]);
+    return (
+      <div className={classes}>
+        <MediaControl
+          isPlaying={playState === PLAY_STATES.playing}
+          durationInSeconds={durationInSeconds}
+          playedSeconds={playedSeconds}
+          volume={volume}
+          onSeek={handleMediaControlSeek}
+          onTogglePlay={handleMediaControlTogglePlay}
+          onVolumeChange={handleVolumeChange}
+          />
+      </div>
+    );
+  };
 
-  handleDuration(durationInSeconds) {
-    this.setState({ durationInSeconds });
-  }
-
-  handleVolumeChange(volume) {
-    this.setState({ volume });
-  }
-
-  render() {
-    const { content, clientConfig, githubFlavoredMarkdown } = this.props;
-    const { playState, durationInSeconds, playedSeconds, volume } = this.state;
-    const html = githubFlavoredMarkdown.render(content.text || '');
-    const aspectRatio = content.aspectRatio || { h: 16, v: 9 };
-    const paddingTop = `${(aspectRatio.v / aspectRatio.h * 100).toFixed(2)}%`;
-    const width = content.width || 100;
-
-    let url;
-    switch (content.type) {
-      case SOURCE_TYPE.internal:
-        url = content.url ? `${clientConfig.cdnRootUrl}/${content.url}` : null;
-        break;
-      default:
-        url = content.url || null;
-        break;
-    }
-
-    const mediaControlContainerClasses = classNames(['VideoDisplay-mediaControlContainer', `u-width-${width}`]);
-
-    const mediaControl = url && !content.showVideo
-      ? (
-        <div className={mediaControlContainerClasses}>
-          <MediaControl
-            isPlaying={playState === playStates.PLAYING}
-            durationInSeconds={durationInSeconds}
-            playedSeconds={playedSeconds}
-            volume={volume}
-            onSeek={this.handleMediaControlSeek}
-            onTogglePlay={this.handleMediaControlTogglePlay}
-            onVolumeChange={this.handleVolumeChange}
-            />
-        </div>
-      )
-      : null;
-
-    const containerInnerClasses = classNames({
+  const renderMainPlayer = () => {
+    const classes = classNames({
       'Video-mainPlayerContainer': true,
       [`u-width-${width}`]: !!content.showVideo,
       'Video-mainPlayerContainer--noDisplay': !content.showVideo
     });
 
-    const mainPlayer = (
-      <div className={containerInnerClasses}>
+    return (
+      <div className={classes}>
         <div className="Video-mainPlayerOuter" style={{ paddingTop }}>
           <ReactPlayer
-            ref={this.playerRef}
+            ref={playerRef}
             className="Video-mainPlayerInner"
             url={url}
             width="100%"
@@ -141,49 +133,38 @@ class VideoDisplay extends React.Component {
             controls
             volume={volume}
             progressInterval={100}
-            playing={playState === playStates.PLAYING || playState === playStates.BUFFERING}
-            onReady={this.handleReady}
-            onBuffer={this.handleBuffer}
-            onStart={this.handlePlay}
-            onPlay={this.handlePlay}
-            onPause={this.handlePause}
-            onEnded={this.handleStop}
-            onDuration={this.handleDuration}
-            onProgress={this.handleProgress}
+            playing={playState === PLAY_STATES.playing || playState === PLAY_STATES.buffering}
+            onReady={handleReady}
+            onBuffer={handleBuffer}
+            onStart={handlePlay}
+            onPlay={handlePlay}
+            onPause={handlePause}
+            onEnded={handleStop}
+            onDuration={handleDuration}
+            onProgress={handleProgress}
             />
         </div>
       </div>
     );
+  };
 
-    const players = url
-      ? (
-        <div className="Video-players">
-          {mainPlayer}
-          {mediaControl}
-        </div>
-      )
-      : null;
+  const renderPlayers = () => (
+    <div className="Video-players">
+      {renderMainPlayer()}
+      {url && !content.showVideo && renderMediaControl()}
+    </div>
+  );
 
-    const text = html
-      ? <div className="Video-text" dangerouslySetInnerHTML={{ __html: html }} />
-      : null;
-
-    return (
-      <div className="Video">
-        {players}
-        {text}
-      </div>
-    );
-  }
+  return (
+    <div className="Video">
+      {url && renderPlayers()}
+      {html && <div className="Video-text" dangerouslySetInnerHTML={{ __html: html }} />}
+    </div>
+  );
 }
 
 VideoDisplay.propTypes = {
-  ...sectionDisplayProps,
-  ...clientConfigProps,
-  githubFlavoredMarkdown: PropTypes.instanceOf(GithubFlavoredMarkdown).isRequired
+  ...sectionDisplayProps
 };
 
-export default inject({
-  clientConfig: ClientConfig,
-  githubFlavoredMarkdown: GithubFlavoredMarkdown
-}, VideoDisplay);
+export default VideoDisplay;
