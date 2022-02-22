8928f3a0f24e87affe1788e15302c998f75c3754
Andreas Helmberger <public@ahelmberger.de>
2022-02-22 15:31:09 +0100

    EDU-377 Make the login page always redirect back to the current URL

--- src/ui/browser-helper.js
+++ src/ui/browser-helper.js
@@ -1,3 +1,8 @@
 export function isBrowser() {
   return typeof window === 'object' && typeof document === 'object' && document.nodeType === 9;
 }
+
+export function getCurrentUrl() {
+  const { pathname, search, hash } = window.location;
+  return `${pathname}${search}${hash}`;
+}
