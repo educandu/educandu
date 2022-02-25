0c76b56811fd9de14153ff6f2f8e6dcf925de272
Andreas Helmberger <public@ahelmberger.de>
2022-02-25 11:30:07 +0100

    EDU-391 Fix passwordResetRequests indexes

---
+++ migrations/educandu-2022-02-25-01-add-password-reset-expiry-index.js
@@ -0,0 +1,20 @@
+/* eslint-disable camelcase */
+export default class Educandu_2022_02_25_01_add_password_reset_expiry_index {
+  constructor(db) {
+    this.db = db;
+  }
+
+  async up() {
+    await this.db.collection('passwordResetRequests').createIndexes([
+      {
+        name: '_idx_expires_',
+        key: { expires: 1 },
+        expireAfterSeconds: 0
+      }
+    ]);
+  }
+
+  async down() {
+    await this.db.collection('passwordResetRequests').dropIndex('_idx_expires_');
+  }
+}
