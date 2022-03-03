b72547fecc49cb9dba21a70531d757bba0864af7
Andreas Helmberger <public@ahelmberger.de>
2022-03-03 17:08:45 +0100

    EDU-410 Remove insufficient profile warning

--- src/ui/global-alerts.js
+++ src/ui/global-alerts.js
@@ -1,23 +1,11 @@
 import Markdown from '../components/markdown.js';
-import { PAGE_NAME } from '../domain/page-name.js';
 import React, { useEffect, useState } from 'react';
-import { useUser } from '../components/user-context.js';
 import { useSettings } from '../components/settings-context.js';
-import { usePageName } from '../components/page-name-context.js';
 import { getCookie, setSessionCookie } from '../common/cookie.js';
 import { ALERT_TYPE, ANNOUNCEMENT_COOKIE_NAME } from '../domain/constants.js';
-import InsufficientProfileWarning, { isProfileInsufficient } from '../components/insufficient-profile-warning.js';
 
-function getGlobalAlerts(pageName, user, settings) {
+function getGlobalAlerts(settings) {
   const globalAlerts = [];
-
-  if (user && isProfileInsufficient(user) && pageName !== PAGE_NAME.mySpace) {
-    globalAlerts.push({
-      message: <InsufficientProfileWarning />,
-      type: ALERT_TYPE.info,
-      showInFullScreen: false
-    });
-  }
 
   if (!getCookie(ANNOUNCEMENT_COOKIE_NAME) && settings.announcement) {
     globalAlerts.push({
@@ -33,14 +21,12 @@ function getGlobalAlerts(pageName, user, settings) {
 }
 
 export function useGlobalAlerts() {
-  const pageName = usePageName();
   const settings = useSettings();
-  const user = useUser();
   const [alerts, setAlerts] = useState([]);
 
   useEffect(() => {
-    setAlerts(getGlobalAlerts(pageName, user, settings));
-  }, [pageName, settings, user]);
+    setAlerts(getGlobalAlerts(settings));
+  }, [settings]);
 
   return alerts;
 }
