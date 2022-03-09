f7017c0f4e4aff3887399dddfe996b2525419440
Adriana Luchian <adriana.dumitras@gmail.de>
2022-03-09 14:44:39 +0100

    EDU-425 Add Dashboard header section

--- src/components/pages/dashboard.js
+++ src/components/pages/dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Tabs } from 'antd';
+import gravatar from 'gravatar';
 import PropTypes from 'prop-types';
+import { Avatar, Tabs } from 'antd';
 import RoomsTab from '../rooms-tab.js';
-import ProfileTab from '../profile-tab.js';
 import AccountTab from '../account-tab.js';
+import ProfileTab from '../profile-tab.js';
 import { useUser } from '../user-context.js';
 import UsedStorage from '../used-storage.js';
 import { useTranslation } from 'react-i18next';
@@ -14,14 +15,17 @@ import { useStoragePlan } from '../storage-plan-context.js';
 
 const { TabPane } = Tabs;
 
+const AVATAR_SIZE = 110;
+
 function Dashboard({ initialState, PageTemplate }) {
   const user = useUser();
   const storagePlan = useStoragePlan();
-
   const { t } = useTranslation('dashboard');
   const clientConfig = useService(ClientConfig);
 
   const { rooms } = initialState;
+  const gravatarUrl = gravatar.url(user.email, { s: AVATAR_SIZE, d: 'mp' });
+  const storagePlanName = storagePlan ? `"${storagePlan.name}" ${t('storagePlanLabel')}` : t('noStoragePlanLabel');
 
   const formItemLayout = {
     labelCol: {
@@ -47,13 +51,24 @@ function Dashboard({ initialState, PageTemplate }) {
     }
   };
 
-  const storagePlanName = storagePlan ? `"${storagePlan.name}" ${t('storagePlanLabel')}` : t('noStoragePlanLabel');
+  const personName = [user.profile?.firstName, user.profile?.lastName].filter(name => name).join(' ');
+  const headerTitle = personName || user.username;
+  const headerSubtitle = personName ? `${user.username} | ${user.email}` : user.email;
 
   return (
     <PageTemplate disableProfileWarning>
       <div className="DashboardPage">
 
-        <h1>{t('pageNames:dashboard')}</h1>
+        <section className="DashboardPage-headerSection">
+          <div className="DashboardPage-headerAvatar">
+            <Avatar className="Avatar" shape="circle" size={AVATAR_SIZE} src={gravatarUrl} alt={user.username} />
+          </div>
+          <div>
+            <span className="DashboardPage-headerTitle">{headerTitle}</span>
+            <span className="DashboardPage-headerSubtitle">{headerSubtitle}</span>
+          </div>
+        </section>
+
         <Tabs className="Tabs" defaultActiveKey="1" type="line" size="large">
           {clientConfig.areRoomsEnabled && (
             <TabPane className="Tabs-tabPane" tab={t('roomsTabTitle')} key="1">
