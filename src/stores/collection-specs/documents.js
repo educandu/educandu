9a8901b835b3d24d2940094f2a56f3c8f2f5cb19
Adriana Luchian <adriana.dumitras@gmail.de>
2022-03-10 14:49:10 +0100

    EDU-427 Add missing activities indices

--- src/stores/collection-specs/documents.js
+++ src/stores/collection-specs/documents.js
@@ -2,6 +2,14 @@ export default {
   name: 'documents',
   indexes: [
     {
+      name: '_idx_createdBy_',
+      key: { createdBy: -1 }
+    },
+    {
+      name: '_idx_updatedBy_',
+      key: { updatedBy: -1 }
+    },
+    {
       name: '_idx_updatedOn_',
       key: { updatedOn: -1 }
     },
