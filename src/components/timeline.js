3a9df8d35cfd4d6c9f0f8a70188cd471471d3e80
Adriana Luchian <adriana.dumitras@gmail.de>
2022-03-30 17:48:50 +0200

    EDU-455 Support timeline part deletion and startTimecode change

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,25 +1,83 @@
-import by from 'thenby';
-import React from 'react';
+import { Button } from 'antd';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import DeleteIcon from './icons/general/delete-icon.js';
+import React, { useCallback, useEffect, useState } from 'react';
+import { FlagOutlined } from '@ant-design/icons';
+
+const MIN_PART_LENGTH_IN_MS = 1000;
 
 function Timeline({ length, parts, onPartAdd, onPartDelete, onStartTimecodeChange }) {
-  const orderedParts = parts.sort(by(part => part.startTimecode));
+  const [dragState, setDragState] = useState(null);
+  const handlePartDelete = key => () => onPartDelete(key);
 
-  const renderBarPart = (part, index) => {
-    const nextPart = orderedParts[index + 1];
+  const handleMouseDown = (part, index) => () => {
+    setDragState(part);
+    const previousPart = parts[index - 1];
+    const nextPart = parts[index + 1];
+
+    const elementBounds = document.getElementById(part.key).getBoundingClientRect();
+    const previousElementBounds = document.getElementById(previousPart.key).getBoundingClientRect();
+    const maxBoundsInMs = { left: previousPart.startTimecode, right: nextPart?.startTimecode || length };
+    const maxBoundsInPx = { left: previousElementBounds.left, right: elementBounds.right };
+
+    const msToPxRatio = maxBoundsInPx.right / maxBoundsInMs.right;
+    const minPartLengthInPx = MIN_PART_LENGTH_IN_MS * msToPxRatio;
+
+    const boundsInPx = { left: maxBoundsInPx.left + minPartLengthInPx, right: maxBoundsInPx.right - minPartLengthInPx };
+
+    setDragState({ part, boundsInPx, msToPxRatio });
+  };
+
+  const handleMouseUp = useCallback(() => {
+    setDragState(null);
+  }, []);
+
+  const handleMouseMove = useCallback(event => {
+    if (dragState.boundsInPx.left <= event.clientX && event.clientX <= dragState.boundsInPx.right) {
+      const newStartTimecode = event.clientX / dragState.msToPxRatio;
+      onStartTimecodeChange(dragState.part.key, newStartTimecode);
+    }
+  }, [dragState, onStartTimecodeChange]);
+
+  useEffect(() => {
+    if (!dragState) {
+      return () => {};
+    }
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseup', handleMouseUp);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, [dragState, handleMouseMove, handleMouseUp]);
+
+  const renderPart = (part, index) => {
+    const nextPart = parts[index + 1];
     const stopTimecode = nextPart ? nextPart.startTimecode : length;
-    const width = (stopTimecode * 100) / length;
+    const width = ((stopTimecode - part.startTimecode) * 100) / length;
 
     return (
-      <div key={part.key} className="Timeline-barPart" style={{ width: `${width}%` }}>{part.title}</div>
+      <div key={part.key} id={part.key} className="Timeline-part" style={{ width: `${width}%` }}>
+        <div className="Timeline-partFlagsBar">
+          {index > 0 && <div className="Timeline-partStartFlag" onMouseDown={handleMouseDown(part, index)}><FlagOutlined /></div>}
+        </div>
+        <div className="Timeline-partTrackBar">{part.title}</div>
+        <div className="Timeline-partDeleteBar">
+          <Button
+            className="Timeline-partDeleteBarButton"
+            type="link"
+            icon={<DeleteIcon />}
+            onClick={handlePartDelete(part.key)}
+            disabled={parts.length === 1}
+            />
+        </div>
+      </div>
     );
   };
 
   return (
-    <div className="Timeline">
-      <div className="Timeline-bar">
-        {orderedParts.map(renderBarPart)}
-      </div>
+    <div className={classNames('Timeline', { 'is-dragging': !!dragState })}>
+      {parts.map(renderPart)}
     </div>
   );
 }
