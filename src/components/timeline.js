1c4a70bd352439d38ca9084fc59c3e42b6dfb559
Adriana Luchian <adriana.dumitras@gmail.de>
2022-03-31 17:54:08 +0200

    EDU-455 Add new marker logic

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -2,8 +2,9 @@ import { Button } from 'antd';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { FlagOutlined } from '@ant-design/icons';
+import CloseIcon from './icons/general/close-icon.js';
 import DeleteIcon from './icons/general/delete-icon.js';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { Fragment, useCallback, useEffect, useRef, useState } from 'react';
 
 const MIN_PART_WIDTH_IN_PX = 35;
 const MIN_PART_DURATION_IN_MS = 1000;
@@ -14,29 +15,30 @@ function Timeline({ length, parts, onPartAdd, onPartDelete, onStartTimecodeChang
   const [segments, setSegments] = useState([]);
   const [dragState, setDragState] = useState(null);
   const [msToPxRatio, setMsToPxRatio] = useState(0);
-  const [timelineBounds, setTimelineBounds] = useState({});
+  const [timelineBounds, setTimelineBounds] = useState({ left: 0, width: 0 });
+  const [newMarker, setNewMarker] = useState({ left: 0, bounds: [], isInBounds: false, isVisible: false });
 
-  const handlePartDelete = key => () => onPartDelete(key);
+  const handleSegmentDelete = key => () => onPartDelete(key);
 
-  const handleMouseDown = (marker, index) => () => {
+  const handleMarkerMouseDown = (marker, index) => () => {
     const prevMarker = markers[index - 1];
     const nextMarker = markers[index + 1];
 
-    const minPartLength = MIN_PART_DURATION_IN_MS * msToPxRatio;
+    const minSegmentLength = MIN_PART_DURATION_IN_MS * msToPxRatio;
     const bounds = {
-      left: (prevMarker?.left || 0) + minPartLength,
-      right: (nextMarker?.left || timelineBounds.width) - minPartLength
+      left: (prevMarker?.left || 0) + minSegmentLength,
+      right: (nextMarker?.left || timelineBounds.width) - minSegmentLength
     };
 
     setDragState({ marker, bounds });
   };
 
-  const handleMouseUp = useCallback(() => {
+  const handleWindowMouseUp = useCallback(() => {
     setDragState(null);
   }, []);
 
-  const handleMouseMove = useCallback(event => {
-    // Disable text selection
+  const handleWindowMouseMove = useCallback(event => {
+    // Disable selection of DOM elements (e.g. text, image)
     event.preventDefault();
 
     const currentLeft = event.clientX - timelineBounds.left;
@@ -58,55 +60,85 @@ function Timeline({ length, parts, onPartAdd, onPartDelete, onStartTimecodeChang
     onStartTimecodeChange(dragState.marker.key, newStartTimecode);
   }, [dragState, markers, timelineBounds, msToPxRatio, onStartTimecodeChange]);
 
+  const handleSegmentsBarMouseMove = event => {
+    if (dragState) {
+      return;
+    }
+
+    const currentLeft = event.clientX - timelineBounds.left;
+    const isInBounds = newMarker.bounds.some(bounds => bounds.leftMin <= currentLeft && currentLeft <= bounds.leftMax);
+
+    setNewMarker(prevState => ({ ...prevState, left: currentLeft, isInBounds, isVisible: true }));
+  };
+
+  const handleSegmentsBarMouseLeave = () => {
+    setNewMarker(prevState => ({ ...prevState, isVisible: false }));
+  };
+
   useEffect(() => {
     if (!dragState) {
       return () => { };
     }
-    window.addEventListener('mousemove', handleMouseMove);
-    window.addEventListener('mouseup', handleMouseUp);
+    window.addEventListener('mousemove', handleWindowMouseMove);
+    window.addEventListener('mouseup', handleWindowMouseUp);
     return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mousemove', handleWindowMouseMove);
     };
-  }, [dragState, handleMouseMove, handleMouseUp]);
+  }, [dragState, handleWindowMouseMove, handleWindowMouseUp]);
 
   useEffect(() => {
     if (!timelineRef.current) {
       return;
     }
 
-    const newRatio = timelineRef.current.clientWidth / length;
-    const newTimelineBounds = timelineRef.current.getBoundingClientRect();
-    const newMarkers = parts.slice(1).map(part => ({ key: part.key, left: part.startTimecode * newRatio }));
+    const currentRatio = timelineRef.current.clientWidth / length;
+    const currentTimelineBounds = timelineRef.current.getBoundingClientRect();
+    const currentMarkers = parts.slice(1).map(part => ({ key: part.key, left: part.startTimecode * currentRatio }));
 
-    const newSegments = parts.map((part, index) => {
+    const currentSegments = parts.map((part, index) => {
       const segment = { key: part.key, title: part.title };
 
       if (parts.length === 1) {
-        segment.width = newTimelineBounds.width;
+        segment.width = currentTimelineBounds.width;
         return segment;
       }
       if (index === 0) {
-        segment.width = newMarkers[index].left;
+        segment.width = currentMarkers[index].left;
         return segment;
       }
       if (index === parts.length - 1) {
-        segment.width = newTimelineBounds.width - newMarkers[index - 1].left;
+        segment.width = currentTimelineBounds.width - currentMarkers[index - 1].left;
         return segment;
       }
-      segment.width = newMarkers[index].left - newMarkers[index - 1].left;
+      segment.width = currentMarkers[index].left - currentMarkers[index - 1].left;
       return segment;
     });
 
-    setMarkers(newMarkers);
-    setMsToPxRatio(newRatio);
-    setSegments(newSegments);
-    setTimelineBounds(newTimelineBounds);
+    const minSegmentLength = MIN_PART_DURATION_IN_MS * currentRatio;
+
+    const currentNewMarkerBounds = parts.map((part, index) => {
+      const nextPartStartTimecode = parts[index + 1]?.startTimecode || length;
+      const currentPartDuration = nextPartStartTimecode - part.startTimecode;
+      if (currentPartDuration <= MIN_PART_DURATION_IN_MS) {
+        return null;
+      }
+      return {
+        leftMin: (part.startTimecode * currentRatio) + minSegmentLength,
+        leftMax: (nextPartStartTimecode * currentRatio) - minSegmentLength
+      };
+    }).filter(bound => bound);
+
+    setMarkers(currentMarkers);
+    setSegments(currentSegments);
+    setMsToPxRatio(currentRatio);
+    setTimelineBounds(currentTimelineBounds);
+    setNewMarker(prevState => ({ ...prevState, bounds: currentNewMarkerBounds }));
   }, [timelineRef, parts, length]);
 
   const renderMarker = (marker, index) => {
     return (
       <div key={marker.key} id={marker.key} className="Timeline-marker" style={{ left: `${marker.left}px` }}>
-        <div className="Timeline-partFlagsBar" onMouseDown={handleMouseDown(marker, index)}>
+        <div className="Timeline-partFlagsBar" onMouseDown={handleMarkerMouseDown(marker, index)}>
           <FlagOutlined />
         </div>
       </div>
@@ -124,7 +156,7 @@ function Timeline({ length, parts, onPartAdd, onPartDelete, onStartTimecodeChang
           className="Timeline-deleteButton"
           type="link"
           icon={<DeleteIcon />}
-          onClick={handlePartDelete(segment.key)}
+          onClick={handleSegmentDelete(segment.key)}
           disabled={segments.length === 1}
           />
       </div>
@@ -133,13 +165,22 @@ function Timeline({ length, parts, onPartAdd, onPartDelete, onStartTimecodeChang
 
   return (
     <div className={classNames('Timeline', { 'is-dragging': !!dragState })} ref={timelineRef}>
-      <div className={classNames('Timeline-markersBar')}>
+      <div className="Timeline-markersBar">
         {markers.map(renderMarker)}
       </div>
-      <div className={classNames('Timeline-segmentsBar')}>
+      <div className="Timeline-segmentsBar" onMouseMove={handleSegmentsBarMouseMove} onMouseLeave={handleSegmentsBarMouseLeave}>
+        { newMarker.isVisible && (
+          <Fragment>
+            <div className="Timeline-newMarker" style={{ left: `${newMarker.left}px` }}>
+              {newMarker.isInBounds && <FlagOutlined />}
+              {!newMarker.isInBounds && <div className="Timeline-newMarkerCloseIcon"><CloseIcon /></div>}
+            </div>
+            <div className="Timeline-newSegmentStart" style={{ left: `${newMarker.left}px` }} />
+          </Fragment>
+        )}
         {segments.map(renderSegment)}
       </div>
-      <div className={classNames('Timeline-deletionBar')}>
+      <div className="Timeline-deletionBar">
         {segments.map(renderDeleteSegment)}
       </div>
     </div>
