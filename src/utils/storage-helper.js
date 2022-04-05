39f894a59f983de29867fb66cd5bd9d8f7398406
Adriana Luchian <adriana.dumitras@gmail.de>
2022-04-05 11:00:02 +0200

    EDU-463 Define image optimization limits consistently

--- src/utils/storage-helper.js
+++ src/utils/storage-helper.js
@@ -1,21 +1,28 @@
-import { IMAGE_DOWN_SCALING_WIDTH } from '../domain/constants.js';
+import {
+  IMAGE_OPTIMIZATION_THRESHOLD_WIDTH,
+  IMAGE_OPTIMIZATION_MAX_SIZE_OVER_THRESHOLD_WIDTH_IN_BYTES,
+  IMAGE_OPTIMIZATION_MAX_SIZE_UNDER_THRESHOLD_WIDTH_IN_BYTES
+} from '../domain/constants.js';
 
-const getScaledDownDimensions = (img, widthThreshold) => {
-  if (img.naturalWidth <= widthThreshold) {
+const getScaledDownDimensions = img => {
+  if (img.naturalWidth <= IMAGE_OPTIMIZATION_THRESHOLD_WIDTH) {
     return { width: img.naturalWidth, height: img.naturalHeight };
   }
-  const ratio = img.naturalWidth / widthThreshold;
-  return { width: widthThreshold, height: Math.round(((img.naturalHeight / ratio) + Number.EPSILON) * 100) / 100 };
+
+  const ratio = img.naturalWidth / IMAGE_OPTIMIZATION_THRESHOLD_WIDTH;
+  return { width: IMAGE_OPTIMIZATION_THRESHOLD_WIDTH, height: Math.round(((img.naturalHeight / ratio) + Number.EPSILON) * 100) / 100 };
 };
 
 const scalableFileTypes = ['image/png', 'image/jpg', 'image/jpeg', 'image/webp'];
 
-const shouldOptimizeImage = ({ naturalSize, naturalWidth, widthThreshold }) => {
-  const optimize = (naturalWidth > widthThreshold && (naturalSize > 500 * 1000)) || (naturalSize > 1.5 * 1000 * 1000);
-  return optimize;
+const shouldOptimizeImage = ({ naturalSize, naturalWidth }) => {
+  const widthIsTooBig = naturalWidth > IMAGE_OPTIMIZATION_THRESHOLD_WIDTH && naturalSize > IMAGE_OPTIMIZATION_MAX_SIZE_OVER_THRESHOLD_WIDTH_IN_BYTES;
+  const sizeIsTooBig = naturalSize > IMAGE_OPTIMIZATION_MAX_SIZE_UNDER_THRESHOLD_WIDTH_IN_BYTES;
+
+  return widthIsTooBig || sizeIsTooBig;
 };
 
-const optimizeImage = ({ file, widthThreshold }) => {
+const optimizeImage = file => {
   if (!scalableFileTypes.includes(file.type)) {
     return file;
   }
@@ -28,14 +35,14 @@ const optimizeImage = ({ file, widthThreshold }) => {
       const img = new Image();
       img.src = reader.result;
       img.onload = () => {
-        if (!shouldOptimizeImage({ naturalSize: file.size, naturalWidth: img.naturalWidth, widthThreshold })) {
+        if (!shouldOptimizeImage({ naturalSize: file.size, naturalWidth: img.naturalWidth })) {
           resolve(file);
           return;
         }
         const canvas = document.createElement('canvas');
         const context = canvas.getContext('2d');
 
-        const size = getScaledDownDimensions(img, widthThreshold);
+        const size = getScaledDownDimensions(img);
         canvas.width = size.width;
         canvas.height = size.height;
         context.drawImage(img, 0, 0, size.width, size.height);
@@ -53,5 +60,5 @@ export const processFilesBeforeUpload = ({ files, optimizeImages }) => {
   if (!optimizeImages) {
     return files;
   }
-  return Promise.all(files.map(file => optimizeImage({ file, widthThreshold: IMAGE_DOWN_SCALING_WIDTH })));
+  return Promise.all(files.map(file => optimizeImage(file)));
 };
