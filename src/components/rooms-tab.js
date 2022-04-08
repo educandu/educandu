ccf522a7c459074096c2e3eb83fe215c7d76faa0
Adriana Luchian <adriana.dumitras@gmail.de>
2022-04-08 10:53:01 +0200

    EDU-480 Add sorting

--- src/components/rooms-tab.js
+++ src/components/rooms-tab.js
@@ -6,12 +6,13 @@ import urls from '../utils/urls.js';
 import Restricted from './restricted.js';
 import { useUser } from './user-context.js';
 import { useTranslation } from 'react-i18next';
-import React, { Fragment, useState } from 'react';
 import permissions from '../domain/permissions.js';
+import SortingSelector from './sorting-selector.js';
 import { useDateFormat } from './locale-context.js';
 import { roomShape } from '../ui/default-prop-types.js';
 import RoomCreationModal from './room-creation-modal.js';
 import { ROOM_ACCESS_LEVEL } from '../domain/constants.js';
+import React, { Fragment, useEffect, useMemo, useState } from 'react';
 import { PlusOutlined, LockOutlined, UnlockOutlined } from '@ant-design/icons';
 
 const ROOM_ROLE = {
@@ -24,7 +25,46 @@ function RoomsTab({ rooms }) {
   const { formatDate } = useDateFormat();
   const { t } = useTranslation('roomsTab');
 
+  const initialRows = rooms.map(room => {
+    const userAsMember = room.members.find(member => member.userId === user._id);
+    return {
+      _id: room._id,
+      key: room._id,
+      name: room.name,
+      slug: room.slug,
+      owner: room.owner,
+      access: room.access,
+      createdOn: room.createdOn,
+      updatedOn: room.updatedOn,
+      joinedOn: userAsMember?.joinedOn || '',
+      role: userAsMember ? ROOM_ROLE.member : ROOM_ROLE.owner
+    };
+  });
+
+  const [displayedRows, setDisplayedRows] = useState(initialRows);
+  const [sorting, setSorting] = useState({ value: 'role', direction: 'desc' });
   const [isRoomCreationModalVisible, setIsRoomCreationModalVisible] = useState(false);
+
+  const sortingOptions = [
+    { label: t('common:name'), appliedLabel: t('common:sortedByName'), value: 'name' },
+    { label: t('common:createdOn'), appliedLabel: t('common:sortedByCreatedOn'), value: 'createdOn' },
+    { label: t('common:owner'), appliedLabel: t('sortedByOwner'), value: 'owner' },
+    { label: t('common:access'), appliedLabel: t('sortedByAccess'), value: 'access' },
+    { label: t('role'), appliedLabel: t('sortedByRole'), value: 'role' }
+  ];
+
+  const sorters = useMemo(() => ({
+    name: rowsToSort => rowsToSort.sort(by(row => row.name, { direction: sorting.direction, ignoreCase: true })),
+    createdOn: rowsToSort => rowsToSort.sort(by(row => row.createdOn, sorting.direction)),
+    owner: rowsToSort => rowsToSort.sort(by(row => row.owner.username, sorting.direction)),
+    access: rowsToSort => rowsToSort.sort(by(row => row.access, sorting.direction)),
+    role: rowsToSort => rowsToSort.sort(by(row => row.role, sorting.direction).thenBy(row => row.joinedOn, 'desc').thenBy(row => row.createdOn, 'desc'))
+  }), [sorting.direction]);
+
+  useEffect(() => {
+    const sorter = sorters[sorting.value];
+    setDisplayedRows(oldDisplayedRows => sorter ? sorter(oldDisplayedRows.slice()) : oldDisplayedRows.slice());
+  }, [sorting, sorters]);
 
   const renderName = (name, room) => {
     const dates = [`${t('common:created')}: ${formatDate(room.createdOn)}`];
@@ -73,19 +113,19 @@ function RoomsTab({ rooms }) {
     setIsRoomCreationModalVisible(false);
   };
 
+  const handleSortingChange = ({ value, direction }) => setSorting({ value, direction });
+
   const columns = [
     {
       title: t('common:name'),
       dataIndex: 'name',
-      render: renderName,
-      sorter: by(x => x.name, { ignoreCase: true })
+      render: renderName
     },
     {
       title: t('common:owner'),
       dataIndex: 'owner',
       key: 'owner',
       render: renderOwner,
-      sorter: by(x => x.owner, { ignoreCase: true }),
       width: '200px',
       responsive: ['md']
     },
@@ -94,7 +134,6 @@ function RoomsTab({ rooms }) {
       dataIndex: 'access',
       key: 'access',
       render: renderAccess,
-      sorter: by(x => x.access),
       width: '150px',
       responsive: ['sm']
     },
@@ -103,35 +142,16 @@ function RoomsTab({ rooms }) {
       dataIndex: 'role',
       key: 'role',
       render: renderRole,
-      sorter: by(x => x.role),
       width: '100px'
     }
   ];
 
-  const roomsRows = rooms.map(room => {
-    const userAsMember = room.members.find(member => member.userId === user._id);
-
-    return {
-      _id: room._id,
-      key: room._id,
-      name: room.name,
-      slug: room.slug,
-      owner: room.owner,
-      access: room.access,
-      createdOn: room.createdOn,
-      updatedOn: room.updatedOn,
-      joinedOn: userAsMember ? userAsMember.joinedOn : null,
-      role: userAsMember ? ROOM_ROLE.member : ROOM_ROLE.owner
-    };
-  });
-
   return (
     <div className="RoomsTab">
-      <Table
-        dataSource={[...roomsRows]}
-        columns={columns}
-        pagination
-        />
+      <div className="RoomsTab-sortingSelector" >
+        <SortingSelector size="large" sorting={sorting} options={sortingOptions} onChange={handleSortingChange} />
+      </div>
+      <Table dataSource={[...displayedRows]} columns={columns} pagination />
       <Restricted to={permissions.OWN_ROOMS}>
         <Button
           size="large"
