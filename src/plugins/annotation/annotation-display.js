d559d4891abab33dd43ecd401be172288e1b8146
Adriana Luchian <adriana.dumitras@gmail.de>
2022-04-20 14:17:45 +0200

    EDU-476 Rework annotation display

--- src/plugins/annotation/annotation-display.js
+++ src/plugins/annotation/annotation-display.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import { Collapse, Card } from 'antd';
+import classNames from 'classnames';
 import { INTENT, STATE } from './constants.js';
+import React, { Fragment, useState } from 'react';
 import Markdown from '../../components/markdown.js';
 import { sectionDisplayProps } from '../../ui/default-prop-types.js';
-import { AlertOutlined, InfoCircleOutlined, CheckCircleOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
-
-const { Panel } = Collapse;
+import { AlertOutlined, InfoCircleOutlined, CheckCircleOutlined, ExclamationCircleOutlined, RightOutlined } from '@ant-design/icons';
 
 export default function AnnotationDisplay({ content }) {
   const { state, intent } = content;
-  const collapsePanelKey = 'annotation-panel';
+
+  const [isExpanded, setIsExpanded] = useState(state === STATE.expanded);
+
+  const handleHeaderClick = () => {
+    setIsExpanded(!isExpanded);
+  };
 
   const renderIntentIcon = () => {
     return (
@@ -22,47 +25,38 @@ export default function AnnotationDisplay({ content }) {
     );
   };
 
-  const renderHeader = () => {
-    const title = content.title || '\u00A0';
+  const renderContent = ({ standalone }) => {
     return (
-      <div className="Annotation-header">
-        {renderIntentIcon()}
-        {title}
-      </div>
-    );
-  };
-
-  const renderContent = ({ withIntentIcon }) => {
-    return (
-      <div className="Annotation-content">
-        {withIntentIcon && <div className="Annotation-contentIntentIcon">{renderIntentIcon()}</div>}
+      <div className={classNames('Annotation-content', { 'Annotation-content--standalone': standalone })}>
+        {standalone && <div className="Annotation-contentIntentIcon">{renderIntentIcon()}</div>}
         <Markdown renderMedia={content.renderMedia}>{content.text}</Markdown>
       </div>
     );
   };
 
   return (
-    <div className={`Annotation Annotation--${intent}`}>
-      {state === STATE.static && content.title && (
-        <Collapse accordion activeKey={collapsePanelKey}>
-          <Panel header={renderHeader()} key={collapsePanelKey} showArrow={false}>
-            {renderContent({ withIntentIcon: false })}
-          </Panel>
-        </Collapse>
-      )}
+    <div className="Annotation">
+      {state === STATE.static && !content.title && renderContent({ standalone: true })}
 
-      {state === STATE.static && !content.title && (
-        <Card>
-          {renderContent({ withIntentIcon: true })}
-        </Card>
+      {state === STATE.static && content.title && (
+        <Fragment>
+          <div className="Annotation-header is-above-content">
+            {renderIntentIcon()}
+            {content.title}
+          </div>
+          {renderContent({ standalone: false })}
+        </Fragment>
       )}
 
       {state !== STATE.static && (
-        <Collapse accordion expandIconPosition="right" defaultActiveKey={state === STATE.expanded ? collapsePanelKey : null}>
-          <Panel header={renderHeader()} key={collapsePanelKey}>
-            {renderContent({ withIntentIcon: false })}
-          </Panel>
-        </Collapse>
+        <Fragment>
+          <a className={classNames('Annotation-header ', { 'is-above-content': isExpanded })} onClick={handleHeaderClick}>
+            {renderIntentIcon()}
+            {content.title}
+            <RightOutlined className={classNames('Annotation-headerArrow', { 'is-rotated-downwards': isExpanded })} />
+          </a>
+          {isExpanded && renderContent({ standalone: false })}
+        </Fragment>
       )}
     </div>
   );
