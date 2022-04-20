5e3f71b95c78a3fff76cd0b54a1448b273a76a1a
Adriana Luchian <adriana.dumitras@gmail.de>
2022-04-20 11:02:38 +0200

    EDU-476 Render annotation variations

--- src/plugins/annotation/annotation-display.js
+++ src/plugins/annotation/annotation-display.js
@@ -1,32 +1,66 @@
 import React from 'react';
-import { Collapse } from 'antd';
-import { STATE } from './constants.js';
+import { Collapse, Card } from 'antd';
+import { INTENT, STATE } from './constants.js';
 import Markdown from '../../components/markdown.js';
 import { sectionDisplayProps } from '../../ui/default-prop-types.js';
+import { AlertOutlined, InfoCircleOutlined, CheckCircleOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
 
 const { Panel } = Collapse;
 
 export default function AnnotationDisplay({ content }) {
-  const { state } = content;
-  const panelKey = 'annotation-panel';
-  const title = content.title || '\u00A0';
+  const { state, intent } = content;
+  const collapsePanelKey = 'annotation-panel';
 
-  const renderContentText = () => <Markdown renderMedia={content.renderMedia}>{content.text}</Markdown>;
+  const renderIntentIcon = () => {
+    return (
+      <div className="Annotation-intentIcon">
+        {intent === INTENT.confirm && <CheckCircleOutlined />}
+        {intent === INTENT.inform && <InfoCircleOutlined />}
+        {intent === INTENT.warn && <ExclamationCircleOutlined />}
+        {intent === INTENT.alert && <AlertOutlined />}
+      </div>
+    );
+  };
+
+  const renderHeader = () => {
+    const title = content.title || '\u00A0';
+    return (
+      <div className="Annotation-header">
+        {renderIntentIcon()}
+        {title}
+      </div>
+    );
+  };
+
+  const renderContent = ({ withIntentIcon }) => {
+    return (
+      <div className="Annotation-content">
+        {withIntentIcon && <div className="Annotation-contentIntentIcon">{renderIntentIcon()}</div>}
+        <Markdown renderMedia={content.renderMedia}>{content.text}</Markdown>
+      </div>
+    );
+  };
 
   return (
-    <div className="Annotation">
-      {state === STATE.static && (
-        <Collapse accordion activeKey={panelKey}>
-          <Panel header={title} key={panelKey} showArrow={false}>
-            {renderContentText()}
+    <div className={`Annotation Annotation--${intent}`}>
+      {state === STATE.static && content.title && (
+        <Collapse accordion activeKey={collapsePanelKey}>
+          <Panel header={renderHeader()} key={collapsePanelKey} showArrow={false}>
+            {renderContent({ withIntentIcon: false })}
           </Panel>
         </Collapse>
       )}
 
+      {state === STATE.static && !content.title && (
+        <Card>
+          {renderContent({ withIntentIcon: true })}
+        </Card>
+      )}
+
       {state !== STATE.static && (
-        <Collapse accordion defaultActiveKey={state === STATE.expanded ? panelKey : null}>
-          <Panel header={title} key={panelKey}>
-            {renderContentText()}
+        <Collapse accordion expandIconPosition="right" defaultActiveKey={state === STATE.expanded ? collapsePanelKey : null}>
+          <Panel header={renderHeader()} key={collapsePanelKey}>
+            {renderContent({ withIntentIcon: false })}
           </Panel>
         </Collapse>
       )}
