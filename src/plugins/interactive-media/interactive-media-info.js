046e8d99f687832c0c2eca0c2296c8b61ecd7087
Adriana Luchian <adriana.dumitras@gmail.de>
2022-04-21 17:52:46 +0200

    EDU-519 Set up data model

--- src/plugins/interactive-media/interactive-media-info.js
+++ src/plugins/interactive-media/interactive-media-info.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { SOURCE_TYPE } from './constants.js';
 import cloneDeep from '../../utils/clone-deep.js';
 import InteractiveMediaIcon from './interactive-media-icon.js';
 import InteractiveMediaDisplay from './interactive-media-display.js';
+import { isAccessibleStoragePath } from '../../ui/path-helper.js';
 
 class InteractiveMediaInfo {
   static get typeName() { return 'interactive-media'; }
 
-  constructor() {
+  constructor(gfm) {
+    this.gfm = gfm;
     this.type = 'interactive-media';
   }
 
@@ -27,19 +30,48 @@ class InteractiveMediaInfo {
   }
 
   getDefaultContent() {
-    return {};
+    return {
+      sourceType: SOURCE_TYPE.internal,
+      sourceUrl: '',
+      text: '',
+      width: 100,
+      aspectRatio: {
+        h: 16,
+        v: 9
+      },
+      showVideo: true
+    };
   }
 
   cloneContent(content) {
     return cloneDeep(content);
   }
 
-  redactContent(content) {
-    return cloneDeep(content);
+  redactContent(content, targetRoomId) {
+    const redactedContent = cloneDeep(content);
+
+    redactedContent.text = this.gfm.redactCdnResources(
+      redactedContent.text,
+      url => isAccessibleStoragePath(url, targetRoomId) ? url : ''
+    );
+
+    if (redactedContent.sourceType === SOURCE_TYPE.internal && !isAccessibleStoragePath(redactedContent.sourceUrl, targetRoomId)) {
+      redactedContent.sourceUrl = '';
+    }
+
+    return redactedContent;
   }
 
-  getCdnResources() {
-    return [];
+  getCdnResources(content) {
+    const cdnResources = [];
+
+    cdnResources.push(...this.gfm.extractCdnResources(content.text || ''));
+
+    if (content.sourceType === SOURCE_TYPE.internal && content.sourceUrl) {
+      cdnResources.push(content.sourceUrl);
+    }
+
+    return cdnResources;
   }
 }
 
