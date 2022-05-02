b80ceff4ab56df743bb509cfbff8901562c6f7b0
Andreas Helmberger <public@ahelmberger.de>
2022-05-02 14:24:09 +0200

    EDU-523 Prevent Markdown class from being overridden

--- src/components/markdown.js
+++ src/components/markdown.js
@@ -4,19 +4,20 @@ import { useService } from './container-context.js';
 import ClientConfig from '../bootstrap/client-config.js';
 import GithubFlavoredMarkdown from '../common/github-flavored-markdown.js';
 
-function Markdown({ children, inline, renderMedia, tag, ...rest }) {
+function Markdown({ className, children, inline, renderMedia, tag, ...rest }) {
   const gfm = useService(GithubFlavoredMarkdown);
   const { cdnRootUrl } = useService(ClientConfig);
 
   const Tag = tag || 'div';
 
   return typeof children === 'string'
-    ? <Tag className="Markdown" {...rest} dangerouslySetInnerHTML={{ __html: gfm[inline ? 'renderInline' : 'render'](children, { cdnRootUrl, renderMedia }) }} />
-    : <Tag className="Markdown" {...rest}>{children}</Tag>;
+    ? <Tag className={`Markdown ${className}`} {...rest} dangerouslySetInnerHTML={{ __html: gfm[inline ? 'renderInline' : 'render'](children, { cdnRootUrl, renderMedia }) }} />
+    : <Tag className={`Markdown ${className}`} {...rest}>{children}</Tag>;
 }
 
 Markdown.propTypes = {
   children: PropTypes.node,
+  className: PropTypes.string,
   inline: PropTypes.bool,
   renderMedia: PropTypes.bool,
   tag: PropTypes.string
@@ -24,6 +25,7 @@ Markdown.propTypes = {
 
 Markdown.defaultProps = {
   children: null,
+  className: '',
   inline: false,
   renderMedia: false,
   tag: 'div'
