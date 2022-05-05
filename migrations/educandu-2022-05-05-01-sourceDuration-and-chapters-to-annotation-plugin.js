5d37419e10046ff106329de36cc6b9aa4d94c23b
Adriana Luchian <adriana.dumitras@gmail.de>
2022-05-05 09:45:17 +0200

    EDU-531 Update and rename migration script

--- migrations/educandu-2022-05-05-01-sourceDuration-and-chapters-to-annotation-plugin.js
+++ migrations/educandu-2022-05-05-01-sourceDuration-and-chapters-to-annotation-plugin.js
@@ -1,5 +1,7 @@
+import uniqueId from '../src/utils/unique-id.js';
+
 /* eslint-disable camelcase, no-await-in-loop, no-console */
-export default class Educandu_2022_05_03_01_add_sourceDuration_and_chapters_to_interactive_media_plugin {
+export default class Educandu_2022_05_05_01_add_sourceDuration_and_chapters_to_interactive_media_plugin {
   constructor(db) {
     this.db = db;
   }
@@ -10,7 +12,7 @@ export default class Educandu_2022_05_03_01_add_sourceDuration_and_chapters_to_i
       {
         $set: {
           'sections.$[sectionElement].content.sourceDuration': 0,
-          'sections.$[sectionElement].content.chapters': [{ startTimecode: 0, title: '', key: '' }]
+          'sections.$[sectionElement].content.chapters': [{ key: uniqueId.create(), title: 'Chapter', startTimecode: 0 }]
         }
       },
       {
@@ -33,7 +35,7 @@ export default class Educandu_2022_05_03_01_add_sourceDuration_and_chapters_to_i
       {
         $unset: {
           'sections.$[sectionElement].content.sourceDuration': 0,
-          'sections.$[sectionElement].content.chapters': [{ startTimecode: 0, title: '', key: '' }]
+          'sections.$[sectionElement].content.chapters': null
         }
       },
       {
