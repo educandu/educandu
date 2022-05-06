69aa89ef47422e3e0c7240c537b44314ed2ee6a4
Andreas Helmberger <public@ahelmberger.de>
2022-05-06 15:54:29 +0200

    EDU-536 Use media player in interactive media plugin display

--- src/plugins/interactive-media/interactive-media-display.js
+++ src/plugins/interactive-media/interactive-media-display.js
@@ -1,8 +1,46 @@
 import React from 'react';
+import { SOURCE_TYPE } from './constants.js';
+import Markdown from '../../components/markdown.js';
+import ClientConfig from '../../bootstrap/client-config.js';
+import { useService } from '../../components/container-context.js';
 import { sectionDisplayProps } from '../../ui/default-prop-types.js';
+import MediaPlayer, { ASPECT_RATIO } from '../../components/media-player.js';
 
 function InteractiveMediaDisplay({ content }) {
-  return <div className="InteractiveMediaDisplay">Display - content: {JSON.stringify(content)}</div>;
+  const clientConfig = useService(ClientConfig);
+
+  let sourceUrl;
+  switch (content.sourceType) {
+    case SOURCE_TYPE.internal:
+      sourceUrl = content.sourceUrl ? `${clientConfig.cdnRootUrl}/${content.sourceUrl}` : null;
+      break;
+    default:
+      sourceUrl = content.sourceUrl || null;
+      break;
+  }
+
+  const aspectRatio = content.aspectRatio?.h === 4 && content.aspectRatio?.v === 3
+    ? ASPECT_RATIO.fourToThree
+    : ASPECT_RATIO.sixteenToNine;
+
+  return (
+    <div className="InteractiveMediaDisplay">
+      <div className={`InteractiveMediaDisplay-content u-width-${content.width || 100}`}>
+        {sourceUrl && (
+          <MediaPlayer
+            sourceUrl={sourceUrl}
+            audioOnly={!content.showVideo}
+            aspectRatio={aspectRatio}
+            />
+        )}
+        {content.text && (
+          <div className="InteractiveMediaDisplay-text">
+            <Markdown>{content.text}</Markdown>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 InteractiveMediaDisplay.propTypes = {
