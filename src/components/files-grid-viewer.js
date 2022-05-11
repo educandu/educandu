722c2fd4d619817e937fdfb12125788b957c2447
Adriana Luchian <adriana.dumitras@gmail.de>
2022-05-11 11:17:18 +0200

    EDU-551 Pass files to component

--- src/components/files-grid-viewer.js
+++ src/components/files-grid-viewer.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function FilesGridViewer() {
-  return <div className="FilesGridViewer" />;
+function FilesGridViewer({ files }) {
+  const renderFile = file => {
+    return (
+      <div className="FilesGridViewer-file" key={file.path}>{file.name}</div>
+    );
+  };
+
+  return (
+    <div className="FilesGridViewer">
+      {files.map(file => renderFile(file))}
+    </div>
+  );
 }
+
+FilesGridViewer.propTypes = {
+  files: PropTypes.arrayOf(PropTypes.shape({
+    path: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    size: PropTypes.number
+  })).isRequired
+};
 
 export default FilesGridViewer;
--- src/components/pages/tests.js
+++ src/components/pages/tests.js
@@ -1,12 +1,38 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React, { useCallback, useEffect, useState } from 'react';
 import FilesGridViewer from '../files-grid-viewer.js';
+import { useSessionAwareApiClient } from '../../ui/api-helper.js';
+import StorageApiClient from '../../api-clients/storage-api-client.js';
 
 function Tests({ PageTemplate }) {
+  const [files, setFiles] = useState([]);
+  const storageApiClient = useSessionAwareApiClient(StorageApiClient);
+
+  const convertCdnObjectsToRecords = useCallback(objects => {
+    return objects.map(obj => {
+      const record = {
+        path: obj.name,
+        name: obj.name,
+        size: obj.size,
+        lastModified: obj.lastModified,
+        originalObject: obj
+      };
+
+      return record;
+    });
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const { objects } = await storageApiClient.getObjects('rooms/hZ8U4z5jf1d2cu23U8oPmV/media/');
+      setFiles(convertCdnObjectsToRecords(objects));
+    })();
+  }, [storageApiClient, convertCdnObjectsToRecords]);
+
   return (
     <PageTemplate>
       <div className="TestsPage">
-        <FilesGridViewer />
+        <FilesGridViewer files={files} />
       </div>
     </PageTemplate>
   );
