1a361861bb359c09073db4f509a2556d0be1f31f
Adriana Luchian <adriana.dumitras@gmail.de>
2022-05-20 14:00:38 +0200

    EDU-572 Adjust PDF icons a little

--- src/components/icons/files/file-video-filled-icon.js
+++ src/components/icons/files/file-video-filled-icon.js
@@ -8,7 +8,7 @@ export function FileVideoFilledIconComponent() {
     <svg height="1em" style={{ enableBackground: 'new 0 0 1000 1000' }} width="1em" viewBox="0 0 1000 1000">
       <path style={{ fill: '#f2f2f2', stroke: 'currentColor', strokeWidth: '20', strokeLinecap: 'round', strokeMiterlimit: '10' }} d="M138.72 363.74V101.9c0-43.96 35.63-79.59 79.59-79.59H605.9c14.55 0 28.47 5.94 38.54 16.45l202.01 210.87a53.346 53.346 0 0 1 14.83 36.92V898.1c0 43.96-35.63 79.59-79.59 79.59H218.31c-43.96 0-79.59-35.63-79.59-79.59V521.3" transform="translate(-3.654 1.44)" />
       <path style={{ fill: '#fff', stroke: 'currentColor', strokeWidth: '20', strokeLinecap: 'round', strokeMiterlimit: '10' }} d="M861.28 292.23v-5.69c0-13.76-5.31-26.98-14.83-36.92L644.44 38.76a53.373 53.373 0 0 0-38.54-16.45h-7.14v186.72c0 45.95 37.26 83.2 83.21 83.2h156.8z" transform="translate(-3.654 1.44)" />
-      <path style={{ display: 'inline', fill: 'currentColor', strokeWidth: '1' }} d="M627.436 612.302 433.094 473.43c-21.22-15.162-50.708 0-50.708 26.089v277.744c0 26.078 29.488 41.25 50.708 26.089L627.436 664.48c17.886-12.793 17.886-39.386 0-52.178z" />
+      <path style={{ display: 'inline', fill: 'currentColor', strokeWidth: '1' }} d="M627.436 602.302 433.094 463.43c-21.22-15.162-50.708 0-50.708 26.089v277.744c0 26.078 29.488 41.25 50.708 26.089L627.436 654.48c17.886-12.793 17.886-39.386 0-52.178z" />
     </svg>
   );
 }
--- src/components/icons/files/file-video-icon.js
+++ src/components/icons/files/file-video-icon.js
@@ -6,7 +6,7 @@ const Icon = iconNs.default || iconNs;
 export function FileVideoIconComponent() {
   return (
     <svg height="1em" style={{ enableBackground: 'new 0 0 1000 1000' }} width="1em" viewBox="0 0 1000 1000">
-      <path style={{ display: 'inline', fill: 'currentColor', strokeWidth: '1' }} d="M628.616 614.045 434.275 475.173c-21.22-15.162-50.709 0-50.709 26.088v277.744c0 26.079 29.488 41.251 50.709 26.09l194.341-138.873c17.886-12.792 17.886-39.385 0-52.177z" />
+      <path style={{ display: 'inline', fill: 'currentColor', strokeWidth: '1.07229' }} d="M628.616 596.045 434.275 457.173c-21.22-15.162-50.709 0-50.709 26.088v277.744c0 26.079 29.488 41.251 50.709 26.09l194.341-138.873c17.886-12.792 17.886-39.385 0-52.177z" />
       <g style={{ display: 'inline' }}>
         <path style={{ fill: 'none', stroke: 'currentColor', strokeWidth: '45', strokeLinecap: 'round', strokeMiterlimit: '10' }} d="M859.09 287.84v607.84c0 43.69-35.42 79.11-79.11 79.11H220.02c-43.69 0-79.11-35.42-79.11-79.11V104.32c0-43.69 35.42-79.11 79.11-79.11h385.24c14.46 0 28.3 5.91 38.3 16.35l200.78 209.59a52.995 52.995 0 0 1 14.75 36.69z" transform="translate(2.086 1.914)" />
         <path style={{ fill: 'none', stroke: 'currentColor', strokeWidth: '45', strokeLinecap: 'round', strokeMiterlimit: '10' }} d="M598.16 44.47V210.8c0 45.67 37.03 82.7 82.71 82.7h155.85" transform="translate(2.086 1.914)" />
