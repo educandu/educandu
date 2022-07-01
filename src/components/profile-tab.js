6e6121a380ab9ca15708d6797ddf60fc43688c08
Adriana Luchian <adriana.dumitras@gmail.de>
2022-07-01 14:46:19 +0200

    EDU-647 Phrase info texts

--- src/components/profile-tab.js
+++ src/components/profile-tab.js
@@ -2,7 +2,7 @@ import by from 'thenby';
 import Alert from './alert.js';
 import gravatar from 'gravatar';
 import memoizee from 'memoizee';
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import Logger from '../common/logger.js';
 import localeCompare from 'locale-compare';
 import errorHelper from '../ui/error-helper.js';
@@ -22,7 +22,7 @@ const logger = new Logger(import.meta.url);
 const FormItem = Form.Item;
 const Option = Select.Option;
 
-const AVATAR_SIZE = 256;
+const AVATAR_SIZE = 110;
 
 const createCountryNames = memoizee((countryNameProvider, uiLanguage) => {
   return Object.entries(countryNameProvider.getData(uiLanguage))
@@ -85,57 +85,60 @@ function ProfileTab({ formItemLayout, tailFormItemLayout }) {
   );
 
   return (
-    <Form onFinish={handleProfileFinish} scrollToFirstError>
-      <FormItem {...tailFormItemLayout}>
-        <Avatar className="Avatar" shape="circle" size={AVATAR_SIZE} src={gravatarUrl} alt={user.username} />
-        <br />
-        <a onClick={handleShowAvatarDescriptionClick}>{t('changePicture')}</a>
-        <br />
-        {showAvatarDescription && (
-        <Alert
-          message={t('howToChangePicture')}
-          description={avatarDescription}
-          closable
-          afterClose={handleAvatarDescriptionAfterClose}
-          />
-        )}
-      </FormItem>
-      <FormItem {...formItemLayout} label={t('firstName')} name="firstName" initialValue={profile.firstName || ''}>
-        <Input type="text" />
-      </FormItem>
-      <FormItem {...formItemLayout} label={t('lastName')} name="lastName" initialValue={profile.lastName || ''}>
-        <Input type="text" />
-      </FormItem>
-      <FormItem {...formItemLayout} label={t('street')} name="street" initialValue={profile.street || ''}>
-        <Input type="text" />
-      </FormItem>
-      <FormItem {...formItemLayout} label={t('streetSupplement')} name="streetSupplement" initialValue={profile.streetSupplement || ''}>
-        <Input type="text" />
-      </FormItem>
-      <FormItem {...formItemLayout} label={t('postalCode')} name="postalCode" initialValue={profile.postalCode || ''}>
-        <Input type="text" />
-      </FormItem>
-      <FormItem {...formItemLayout} label={t('city')} name="city" initialValue={profile.city || ''}>
-        <Input type="text" />
-      </FormItem>
-      <FormItem {...formItemLayout} label={t('country')} name="country" initialValue={profile.country || ''}>
-        <Select
-          optionFilterProp="title"
-          showSearch
-          allowClear
-          autoComplete="none"
-          >
-          {createCountryNames(countryNameProvider, uiLanguage).map(cn => (
-            <Option key={cn.key} value={cn.key} title={cn.name}>
-              <CountryFlagAndName code={cn.key} name={cn.name} />
-            </Option>
-          ))}
-        </Select>
-      </FormItem>
-      <FormItem {...tailFormItemLayout}>
-        <Button type="primary" htmlType="submit">{t('common:save')}</Button>
-      </FormItem>
-    </Form>
+    <Fragment>
+      <div className="DashboardPage-tabInfo">{t('info')}</div>
+      <Form onFinish={handleProfileFinish} scrollToFirstError>
+        <FormItem {...tailFormItemLayout}>
+          <Avatar className="Avatar" shape="circle" size={AVATAR_SIZE} src={gravatarUrl} alt={user.username} />
+          <br />
+          <a onClick={handleShowAvatarDescriptionClick}>{t('changePicture')}</a>
+          <br />
+          {showAvatarDescription && (
+          <Alert
+            message={t('howToChangePicture')}
+            description={avatarDescription}
+            closable
+            afterClose={handleAvatarDescriptionAfterClose}
+            />
+          )}
+        </FormItem>
+        <FormItem {...formItemLayout} label={t('firstName')} name="firstName" initialValue={profile.firstName || ''}>
+          <Input type="text" />
+        </FormItem>
+        <FormItem {...formItemLayout} label={t('lastName')} name="lastName" initialValue={profile.lastName || ''}>
+          <Input type="text" />
+        </FormItem>
+        <FormItem {...formItemLayout} label={t('street')} name="street" initialValue={profile.street || ''}>
+          <Input type="text" />
+        </FormItem>
+        <FormItem {...formItemLayout} label={t('streetSupplement')} name="streetSupplement" initialValue={profile.streetSupplement || ''}>
+          <Input type="text" />
+        </FormItem>
+        <FormItem {...formItemLayout} label={t('postalCode')} name="postalCode" initialValue={profile.postalCode || ''}>
+          <Input type="text" />
+        </FormItem>
+        <FormItem {...formItemLayout} label={t('city')} name="city" initialValue={profile.city || ''}>
+          <Input type="text" />
+        </FormItem>
+        <FormItem {...formItemLayout} label={t('country')} name="country" initialValue={profile.country || ''}>
+          <Select
+            optionFilterProp="title"
+            showSearch
+            allowClear
+            autoComplete="none"
+            >
+            {createCountryNames(countryNameProvider, uiLanguage).map(cn => (
+              <Option key={cn.key} value={cn.key} title={cn.name}>
+                <CountryFlagAndName code={cn.key} name={cn.name} />
+              </Option>
+            ))}
+          </Select>
+        </FormItem>
+        <FormItem {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit">{t('common:save')}</Button>
+        </FormItem>
+      </Form>
+    </Fragment>
   );
 }
 
