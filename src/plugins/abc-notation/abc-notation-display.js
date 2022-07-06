56acc8fe492620b85449a97e9b9a8de023e83ff4
Andreas Helmberger <public@ahelmberger.de>
2022-07-06 17:42:09 +0200

    EDU-637 Add ABC notation preview to ABC notation and ear training plugin editors

--- src/plugins/abc-notation/abc-notation-display.js
+++ src/plugins/abc-notation/abc-notation-display.js
@@ -1,38 +1,13 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import Markdown from '../../components/markdown.js';
+import AbcNotation from '../../components/abc-notation.js';
 import { sectionDisplayProps } from '../../ui/default-prop-types.js';
 
-const abcOptions = {
-  paddingtop: 0,
-  paddingbottom: 0,
-  paddingright: 0,
-  paddingleft: 0,
-  responsive: 'resize'
-};
-
-const midiOptions = {
-  generateDownload: false,
-  generateInline: true
-};
-
 function AbcNotationDisplay({ content }) {
-  const abcContainerRef = useRef(null);
-  const midiContainerRef = useRef(null);
-
-  useEffect(() => {
-    (async () => {
-      const { default: abcjs } = await import('abcjs/midi.js');
-
-      abcjs.renderAbc(abcContainerRef.current, content.abcCode, abcOptions);
-      abcjs.renderMidi(midiContainerRef.current, content.abcCode, midiOptions);
-    })();
-  });
-
   return (
     <div className="AbcNotation fa5">
       <div className={`AbcNotation-wrapper u-width-${content.width}`}>
-        <div ref={abcContainerRef} />
-        {content.displayMidi && <div ref={midiContainerRef} />}
+        <AbcNotation abcCode={content.abcCode} displayMidi={content.displayMidi} />
         <div className="AbcNotation-copyrightInfo">
           <Markdown>{content.text}</Markdown>
         </div>
--- src/plugins/abc-notation/abc-notation-editor.js
+++ src/plugins/abc-notation/abc-notation-editor.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Form, Switch } from 'antd';
 import { useTranslation } from 'react-i18next';
+import AbcNotation from '../../components/abc-notation.js';
 import MarkdownInput from '../../components/markdown-input.js';
+import InputAndPreview from '../../components/input-and-preview.js';
 import { sectionEditorProps } from '../../ui/default-prop-types.js';
 import ObjectWidthSlider from '../../components/object-width-slider.js';
 import NeverScrollingTextArea from '../../components/never-scrolling-text-area.js';
@@ -13,6 +15,11 @@ function AbcNotationEditor({ content, onContentChanged }) {
   const formItemLayout = {
     labelCol: { span: 4 },
     wrapperCol: { span: 14 }
+  };
+
+  const formItemLayoutVertical = {
+    labelCol: { span: 24 },
+    wrapperCol: { span: 24 }
   };
 
   const changeContent = newContentValues => {
@@ -40,8 +47,11 @@ function AbcNotationEditor({ content, onContentChanged }) {
   return (
     <div>
       <Form layout="horizontal">
-        <Form.Item label={t('abcCode')} {...formItemLayout}>
-          <NeverScrollingTextArea value={abcCode} onChange={handleCurrentAbcCodeChanged} minRows={5} />
+        <Form.Item label={t('abcCode')} {...formItemLayoutVertical}>
+          <InputAndPreview
+            input={<NeverScrollingTextArea minRows={6} value={abcCode} onChange={handleCurrentAbcCodeChanged} />}
+            preview={<AbcNotation abcCode={abcCode} />}
+            />
         </Form.Item>
         <Form.Item label={t('midiSound')} {...formItemLayout}>
           <Switch checked={!!displayMidi} onChange={handleDisplayMidiChanged} />
