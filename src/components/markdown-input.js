9f46ee6b35bc8e65ec238bb40cdffafc890d8057
Andreas Helmberger <public@ahelmberger.de>
2022-07-07 13:06:11 +0200

    EDU-637 Use input-and-preview component also in markdown input

--- src/components/markdown-input.js
+++ src/components/markdown-input.js
@@ -4,52 +4,58 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Markdown from './markdown.js';
 import MarkdownHelp from './markdown-help.js';
+import InputAndPreview from './input-and-preview.js';
 import NeverScrollingTextArea from './never-scrolling-text-area.js';
 
 function MarkdownInput({ minRows, disabled, inline, renderMedia, value, onChange, preview, embeddable, ...rest }) {
+  const renderInlineInput = () => (
+    <Input
+      {...rest}
+      className={classNames('MarkdownInput-input', { 'is-disabled': disabled })}
+      value={value}
+      onChange={onChange}
+      disabled={disabled}
+      addonAfter={<MarkdownHelp disabled={disabled} inline />}
+      />
+  );
+
+  const renderBlockInput = () => (
+    <div className="MarkdownInput-textareaContainer">
+      <NeverScrollingTextArea
+        {...rest}
+        className="MarkdownInput-textarea"
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+        minRows={minRows}
+        embeddable={embeddable}
+        />
+      <div
+        className={classNames(
+          'MarkdownInput-blockHelpContainer',
+          { 'MarkdownInput-blockHelpContainer--embeddable': embeddable }
+        )}
+        >
+        <MarkdownHelp size={embeddable ? 'small' : 'normal'} disabled={disabled} />
+      </div>
+    </div>
+  );
+
+  const renderInput = () => inline ? renderInlineInput() : renderBlockInput();
+
+  const renderPreview = () => (
+    <Markdown
+      className={classNames('MarkdownInput-preview', { 'MarkdownInput-preview--inline': inline })}
+      inline={inline}
+      renderMedia={renderMedia}
+      >
+      {value}
+    </Markdown>
+  );
+
   return (
-    <div className={classNames('MarkdownInput', { 'MarkdownInput--withPreview': preview })}>
-      {inline && (
-        <Input
-          {...rest}
-          className={classNames('MarkdownInput-input', { 'is-disabled': disabled })}
-          value={value}
-          onChange={onChange}
-          disabled={disabled}
-          addonAfter={<MarkdownHelp disabled={disabled} inline />}
-          />
-      )}
-      {!inline && (
-        <div className="MarkdownInput-textareaContainer">
-          <NeverScrollingTextArea
-            {...rest}
-            className="MarkdownInput-textarea"
-            value={value}
-            onChange={onChange}
-            disabled={disabled}
-            minRows={minRows}
-            embeddable={embeddable}
-            />
-          <div
-            className={classNames(
-              'MarkdownInput-blockHelpContainer',
-              { 'MarkdownInput-blockHelpContainer--embeddable': embeddable }
-            )}
-            >
-            <MarkdownHelp size={embeddable ? 'small' : 'normal'} disabled={disabled} />
-          </div>
-        </div>
-      )}
-      {preview && (
-        <div
-          className={classNames(
-            'MarkdownInput-preview',
-            { 'MarkdownInput-preview--inline': inline, 'is-disabled': disabled }
-          )}
-          >
-          <Markdown inline={inline} renderMedia={renderMedia}>{value}</Markdown>
-        </div>
-      )}
+    <div className="MarkdownInput">
+      {preview ? <InputAndPreview input={renderInput()} preview={renderPreview()} /> : renderInput()}
     </div>
   );
 }
