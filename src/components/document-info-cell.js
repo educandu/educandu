6e74eb9007029429ee07f11edf710d68c4ba8d9f
Adriana Luchian <adriana.dumitras@gmail.de>
2022-07-21 15:54:35 +0200

    EDU-684 Restyle document info cell to fit room info

--- src/components/document-info-cell.js
+++ src/components/document-info-cell.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import urls from '../utils/routes.js';
+import classNames from 'classnames';
+import routes from '../utils/routes.js';
 import { useTranslation } from 'react-i18next';
 import { useDateFormat } from './locale-context.js';
+import RoomJoinedIcon from './icons/user-activities/room-joined-icon.js';
 import { documentMetadataShape, roomMinimalMetadataShape } from '../ui/default-prop-types.js';
 
 function DocumentInfoCell({ doc, room }) {
   const { formatDate } = useDateFormat();
   const { t } = useTranslation('documentInfoCell');
+
+  const showFrame = !!room;
   const dates = [
     `${t('common:created')}: ${formatDate(doc.createdOn)}`,
     `${t('updatedOn')}: ${formatDate(doc.updatedOn)}`
   ];
 
   return (
-    <a className="InfoCell" href={urls.getDocUrl({ id: doc._id, slug: doc.slug })}>
-      {room?.name}
-      <div className="InfoCell-mainText">{doc.title}</div>
-      {doc.description && <div className="InfoCell-description">{doc.description}</div>}
-      <div className="InfoCell-subtext">{dates.join(' | ')}</div>
-    </a>
+    <div className="DocumentInfoCell" >
+      {showFrame && (
+        <a href={routes.getRoomUrl(room._id, room.slug)} className="DocumentInfoCell-topFrame">
+          <RoomJoinedIcon />{room.name}
+        </a>
+      )}
+
+      <div className="DocumentInfoCell-infoContainer">
+        {showFrame && <div className="DocumentInfoCell-leftFrame" />}
+        <a
+          href={routes.getDocUrl({ id: doc._id, slug: doc.slug })}
+          className={classNames('DocumentInfoCell-infoContent', { 'DocumentInfoCell-infoContent--framed': showFrame })}
+          >
+          <div>
+            <div className="DocumentInfoCell-mainText">{doc.title}</div>
+            {doc.description && <div className="DocumentInfoCell-description">{doc.description}</div>}
+            <div className="DocumentInfoCell-subtext">{dates.join(' | ')}</div>
+          </div>
+        </a>
+      </div>
+    </div>
   );
 }
 
