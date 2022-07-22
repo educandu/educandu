0b7a0ab546db2c8939b095ad3810aaa1e70e18e9
Andreas Helmberger <public@ahelmberger.de>
2022-07-22 13:23:09 +0200

    EDU-688 Add media player to test page

--- src/components/pages/tests.js
+++ src/components/pages/tests.js
@@ -1,60 +1,122 @@
 /* eslint-disable no-console, max-len */
 
-import { Collapse, Form, Input } from 'antd';
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import MediaPlayer from '../media-player.js';
+import React, { useRef, useState } from 'react';
+import { Button, Form, Input, Tabs } from 'antd';
 import ResourcePicker from '../resource-picker.js';
+import { useRequest } from '../request-context.js';
+import ResourceSelector from '../resource-selector.js';
 import NeverScrollingTextArea from '../never-scrolling-text-area.js';
 import { HORIZONTAL_ALIGNMENT, STORAGE_LOCATION_TYPE, VERTICAL_ALIGNMENT } from '../../domain/constants.js';
-import ResourceSelector from '../resource-selector.js';
 
-const { Panel } = Collapse;
+const { TabPane } = Tabs;
 
 function Tests({ PageTemplate }) {
-  const [ta1, setTa1] = useState('Hello World');
-  const [ta2, setTa2] = useState('Hello World');
-  const [ta3, setTa3] = useState('Hello World');
-  const [ta4, setTa4] = useState('Hello World');
-  const [ta5, setTa5] = useState('Hello World');
-  const [ta6, setTa6] = useState('Hello World');
-  const [resourceUrl, setResourceUrl] = useState('');
+  // Page
+  const req = useRequest();
+  const initialTab = req.query.tab || null;
+  const handleTabChange = newTab => {
+    const url = new URL(window.document.location.href);
+    url.searchParams.set('tab', newTab);
+    window.history.replaceState(null, null, url.href);
+  };
+
+  // MediaPlayer
+  const mpPlayerRef = useRef();
+  const mpEventLogRef = useRef();
+  const [mpSource, setMpSource] = useState(null);
+  const [mpEventLog, setMpEventLog] = useState('');
+  const handleMpEvent = (eventName, ...args) => {
+    setMpEventLog(currentLog => `${currentLog}${eventName}: ${JSON.stringify(args.length > 1 ? args : args[0])}\n`);
+    setTimeout(() => {
+      const textarea = mpEventLogRef.current;
+      textarea.scrollTop = textarea.scrollHeight;
+    }, 0);
+  };
+
+  // ResourcePicker
+  const [rsResourceUrl, setRsResourceUrl] = useState('');
+
+  // NeverScrollingTextArea
+  const [nstaValue1, setNstaValue1] = useState('Hello World');
+  const [nstaValue2, setNstaValue2] = useState('Hello World');
+  const [nstaValue3, setNstaValue3] = useState('Hello World');
+  const [nstaValue4, setNstaValue4] = useState('Hello World');
+  const [nstaValue5, setNstaValue5] = useState('Hello World');
+  const [nstaValue6, setNstaValue6] = useState('Hello World');
 
   return (
     <PageTemplate>
       <div className="TestsPage">
-        <Collapse defaultActiveKey={['1']} accordion>
-          <Panel header="ResourceSelector" key="1">
+        <Tabs defaultActiveKey={initialTab} onChange={handleTabChange} destroyInactiveTabPane>
+          <TabPane tab="MediaPlayer" key="MediaPlayer">
+            <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '25px' }}>
+              <div>
+                <MediaPlayer
+                  source={mpSource}
+                  mediaPlayerRef={mpPlayerRef}
+                  onPartEndReached={(...args) => handleMpEvent('onPartEndReached', ...args)}
+                  onEndReached={(...args) => handleMpEvent('onEndReached', ...args)}
+                  onPlayStateChange={(...args) => handleMpEvent('onPlayStateChange', ...args)}
+                  onPlayingPartIndexChange={(...args) => handleMpEvent('onPlayingPartIndexChange', ...args)}
+                  onReady={(...args) => handleMpEvent('onReady', ...args)}
+                  onSeek={(...args) => handleMpEvent('onSeek', ...args)}
+                  />
+              </div>
+              <div>
+                <h6>Source</h6>
+                <div>
+                  <Button onClick={() => setMpSource('https://www.youtube.com/watch?v=H3hBitGg_NI')}>Set to YouTube</Button>
+                  <Button onClick={() => setMpSource('https://cdn.openmusic.academy/media/fQugKEp8XCKJTVKVhiRdeJ/2022-04-05-5-te-sinfonie-v1-bLf7WqJAaf4y8AsPRnWG8R.mp4')}>Set to External</Button>
+                </div>
+                <div>{typeof mpSource === 'function' ? '<FUNC>' : String(mpSource)}</div>
+                <h6 style={{ marginTop: '15px' }}>Programmatic control</h6>
+                <div>
+                  <Button onClick={() => mpPlayerRef.current.play()}>play</Button>
+                  <Button onClick={() => mpPlayerRef.current.pause()}>pause</Button>
+                  <Button onClick={() => mpPlayerRef.current.togglePlay()}>togglePlay</Button>
+                  <Button onClick={() => mpPlayerRef.current.reset()}>reset</Button>
+                </div>
+                <h6 style={{ marginTop: '15px' }}>Event Log</h6>
+                <div ref={mpEventLogRef} style={{ height: '140px', overflow: 'auto', border: '1px solid #ddd', backgroundColor: '#fbfbfb', fontSize: '10px' }}>
+                  <pre>{mpEventLog}</pre>
+                </div>
+              </div>
+            </div>
+          </TabPane>
+          <TabPane tab="ResourceSelector" key="ResourceSelector">
             <ResourceSelector
               allowedLocationTypes={Object.values(STORAGE_LOCATION_TYPE)}
               onSelect={ev => console.log('select', ev)}
               onCancel={ev => console.log('cancel', ev)}
               />
-          </Panel>
-          <Panel header="ResourcePicker" key="2">
+          </TabPane>
+          <TabPane tab="ResourcePicker" key="ResourcePicker">
             <div style={{ display: 'grid', gap: '15px', gridTemplateColumns: '1fr auto' }}>
               <Input
-                value={resourceUrl}
-                onChange={event => setResourceUrl(event.target.value)}
+                value={rsResourceUrl}
+                onChange={event => setRsResourceUrl(event.target.value)}
                 />
               <ResourcePicker
-                url={resourceUrl}
-                onUrlChange={setResourceUrl}
+                url={rsResourceUrl}
+                onUrlChange={setRsResourceUrl}
                 />
             </div>
-          </Panel>
-          <Panel header="NeverScrollingTextArea" key="3">
+          </TabPane>
+          <TabPane tab="NeverScrollingTextArea" key="NeverScrollingTextArea">
             <h4>Grid aligned</h4>
             <div style={{ display: 'grid', gridAutoFlow: 'column', gridTemplateColumns: 'repeat(3, 1fr)', gap: '10px', backgroundColor: '#f5f5f5', padding: '5px' }}>
-              <NeverScrollingTextArea value={ta1} onChange={event => setTa1(event.target.value)} minRows={3} />
-              <NeverScrollingTextArea value={ta2} onChange={event => setTa2(event.target.value)} minRows={4} />
-              <NeverScrollingTextArea value={ta3} onChange={event => setTa3(event.target.value)} minRows={5} />
+              <NeverScrollingTextArea value={nstaValue1} onChange={event => setNstaValue1(event.target.value)} minRows={3} />
+              <NeverScrollingTextArea value={nstaValue2} onChange={event => setNstaValue2(event.target.value)} minRows={4} />
+              <NeverScrollingTextArea value={nstaValue3} onChange={event => setNstaValue3(event.target.value)} minRows={5} />
             </div>
             <br />
             <h4>Alignment</h4>
             <div style={{ display: 'grid', gridAutoFlow: 'column', gridTemplateColumns: 'repeat(3, 1fr)', gap: '10px', backgroundColor: '#f5f5f5', padding: '5px', minHeight: '100px' }}>
-              <NeverScrollingTextArea value={ta4} onChange={event => setTa4(event.target.value)} minRows={1} verticalAlignment={VERTICAL_ALIGNMENT.top} horizontalAlignment={HORIZONTAL_ALIGNMENT.left} />
-              <NeverScrollingTextArea value={ta5} onChange={event => setTa5(event.target.value)} minRows={1} verticalAlignment={VERTICAL_ALIGNMENT.middle} horizontalAlignment={HORIZONTAL_ALIGNMENT.center} />
-              <NeverScrollingTextArea value={ta6} onChange={event => setTa6(event.target.value)} minRows={1} verticalAlignment={VERTICAL_ALIGNMENT.bottom} horizontalAlignment={HORIZONTAL_ALIGNMENT.right} />
+              <NeverScrollingTextArea value={nstaValue4} onChange={event => setNstaValue4(event.target.value)} minRows={1} verticalAlignment={VERTICAL_ALIGNMENT.top} horizontalAlignment={HORIZONTAL_ALIGNMENT.left} />
+              <NeverScrollingTextArea value={nstaValue5} onChange={event => setNstaValue5(event.target.value)} minRows={1} verticalAlignment={VERTICAL_ALIGNMENT.middle} horizontalAlignment={HORIZONTAL_ALIGNMENT.center} />
+              <NeverScrollingTextArea value={nstaValue6} onChange={event => setNstaValue6(event.target.value)} minRows={1} verticalAlignment={VERTICAL_ALIGNMENT.bottom} horizontalAlignment={HORIZONTAL_ALIGNMENT.right} />
             </div>
             <br />
             <h4>Within Form</h4>
@@ -80,8 +142,8 @@ function Tests({ PageTemplate }) {
                 </Form.Item>
               </div>
             </Form>
-          </Panel>
-        </Collapse>
+          </TabPane>
+        </Tabs>
       </div>
     </PageTemplate>
   );
