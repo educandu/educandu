6c469b8fb77c8b22636331d530b0429df0a88cbc
Adriana Luchian <adriana.dumitras@gmail.de>
2022-08-03 09:29:06 +0200

    EDU-722 Add width control

--- src/plugins/multitrack-media/multitrack-media-display.js
+++ src/plugins/multitrack-media/multitrack-media-display.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { sectionDisplayProps } from '../../ui/default-prop-types.js';
 
 function MultitrackMediaDisplay({ content }) {
-  const { mainTrack, secondaryTracks } = content;
+  const { width, mainTrack, secondaryTracks } = content;
 
   return (
     <div className="MultitrackMediaDisplay">
+      <div>Width: {width}</div>
       <div>{mainTrack.name}</div>
       {secondaryTracks.map(secondaryTrack => <div key={secondaryTrack.name}>{secondaryTrack.name}</div>)}
     </div>
--- src/plugins/multitrack-media/multitrack-media-editor.js
+++ src/plugins/multitrack-media/multitrack-media-editor.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Form } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { PlusOutlined } from '@ant-design/icons';
 import ItemPanel from '../../components/item-panel.js';
 import { sectionEditorProps } from '../../ui/default-prop-types.js';
 import { removeItemAt, swapItemsAt } from '../../utils/array-utils.js';
+import ObjectWidthSlider from '../../components/object-width-slider.js';
 import { createDefaultSecondaryTrack } from './multitrack-media-utils.js';
 
+const FormItem = Form.Item;
+
+const formItemLayout = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 14 }
+};
+
 function MultitrackMediaEditor({ content, onContentChanged }) {
-  const { secondaryTracks } = content;
   const { t } = useTranslation('multitrackMedia');
+
+  const { width, mainTrack, secondaryTracks } = content;
 
   const changeContent = newContentValues => {
     const newContent = { ...content, ...newContentValues };
     const isInvalid = false;
     onContentChanged(newContent, isInvalid);
+  };
+
+  const handleWidthChanged = newValue => {
+    changeContent({ width: newValue });
   };
 
   const handleMoveTrackUp = index => {
@@ -37,29 +50,35 @@ function MultitrackMediaEditor({ content, onContentChanged }) {
 
   return (
     <div className="MultitrackMediaEditor">
-      <ItemPanel header={t('mainTrack')}>
-        Main track
-      </ItemPanel>
-
-      {secondaryTracks.map((secondaryTrack, index) => (
-        <ItemPanel
-          index={index}
-          key={index.toString()}
-          itemsCount={secondaryTracks.length}
-          header={t('secondaryTrack', { number: index + 1 })}
-          onMoveUp={handleMoveTrackUp}
-          onMoveDown={handleMoveTrackDown}
-          onDelete={handleDeleteTrack}
-          >
-          {secondaryTrack.name}
+      <Form layout="horizontal">
+        <ItemPanel header={t('mainTrack')}>
+          {mainTrack.name}
         </ItemPanel>
-      ))}
-      <Button type="primary" icon={<PlusOutlined />} onClick={handleAddTrackButtonClick}>
-        {t('addTrack')}
-      </Button>
-      <ItemPanel header={t('trackMixer')}>
-        Track mixer
-      </ItemPanel>
+
+        {secondaryTracks.map((secondaryTrack, index) => (
+          <ItemPanel
+            index={index}
+            key={index.toString()}
+            itemsCount={secondaryTracks.length}
+            header={t('secondaryTrack', { number: index + 1 })}
+            onMoveUp={handleMoveTrackUp}
+            onMoveDown={handleMoveTrackDown}
+            onDelete={handleDeleteTrack}
+            >
+            {secondaryTrack.name}
+          </ItemPanel>
+        ))}
+        <Button type="primary" icon={<PlusOutlined />} onClick={handleAddTrackButtonClick}>
+          {t('addTrack')}
+        </Button>
+        <ItemPanel header={t('trackMixer')}>
+          Track mixer
+        </ItemPanel>
+
+        <FormItem label={t('common:width')} {...formItemLayout}>
+          <ObjectWidthSlider value={width} onChange={handleWidthChanged} />
+        </FormItem>
+      </Form>
     </div>
   );
 }
