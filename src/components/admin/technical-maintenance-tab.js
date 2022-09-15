5b78ea649dbdc5f82d275543f7d875d23a4c69b6
Adriana Luchian <adriana.dumitras@gmail.de>
2022-09-15 11:11:39 +0200

    EDU-801 Reorganize tab content

--- src/components/admin/technical-maintenance-tab.js
+++ src/components/admin/technical-maintenance-tab.js
@@ -58,35 +58,54 @@ function TechnicalMaintenanceTab({
   };
 
   const renderLastBatchExecution = batch => batch && (
-    <span>{t('lastExecution')}: <a href={routes.getBatchUrl(batch._id)}>{formatDate(batch.createdOn)}</a></span>
+    <span className="TechnicalMaintenanceTab-buttonSubtext">
+      {t('lastExecution')}: <a href={routes.getBatchUrl(batch._id)}>{formatDate(batch.createdOn)}</a>
+    </span>
   );
 
   return (
     <div className="TechnicalMaintenanceTab">
-      <div className="TechnicalMaintenanceTab-row">
-        <Button onClick={handleStartDocumentValidationClick} danger>
-          {t('documentValidationButton')}
-        </Button>
-        {renderLastBatchExecution(lastDocumentValidationBatch)}
-      </div>
-      <div className="TechnicalMaintenanceTab-row">
-        <Button onClick={handleStartDocumentRegenerationClick} danger>
-          {t('documentRegenerationButton')}
-        </Button>
-        {renderLastBatchExecution(lastDocumentRegenerationBatch)}
-      </div>
-      <div className="TechnicalMaintenanceTab-row">
-        <Button onClick={handleStartCdnResourcesConsolidationClick} danger>
-          {t('cdnResourcesConsolidationButton')}
-        </Button>
-        {renderLastBatchExecution(lastCdnResourcesConsolidationBatch)}
-      </div>
-      <div className="TechnicalMaintenanceTab-row">
-        <Button onClick={handleStartCdnUploadDirectoryCreationClick} danger>
-          {t('cdnUploadDirectoryCreationButton')}
-        </Button>
-        {renderLastBatchExecution(lastCdnUploadDirectoryCreationBatch)}
-      </div>
+      <div className="TechnicalMaintenanceTab-tabInfo">{t('tabInfo')}</div>
+      <section className="TechnicalMaintenanceTab-section">
+        <div className="TechnicalMaintenanceTab-sectionHeader">{t('documentsValidationHeader')}</div>
+        <div className="TechnicalMaintenanceTab-sectionDescription">{t('documentsValidationDescription')}</div>
+        <div className="TechnicalMaintenanceTab-button">
+          <Button onClick={handleStartDocumentValidationClick} danger>
+            {t('start')}
+          </Button>
+          {renderLastBatchExecution(lastDocumentValidationBatch)}
+        </div>
+      </section>
+      <section className="TechnicalMaintenanceTab-section">
+        <div className="TechnicalMaintenanceTab-sectionHeader">{t('documentsRegenerationHeader')}</div>
+        <div className="TechnicalMaintenanceTab-sectionDescription">{t('documentsRegenerationDescription')}</div>
+        <div className="TechnicalMaintenanceTab-button">
+          <Button onClick={handleStartDocumentRegenerationClick} danger>
+            {t('start')}
+          </Button>
+          {renderLastBatchExecution(lastDocumentRegenerationBatch)}
+        </div>
+      </section>
+      <section className="TechnicalMaintenanceTab-section">
+        <div className="TechnicalMaintenanceTab-sectionHeader">{t('cdnResourcesConsolidationHeader')}</div>
+        <div className="TechnicalMaintenanceTab-sectionDescription">{t('cdnResourcesConsolidationDescription')}</div>
+        <div className="TechnicalMaintenanceTab-button">
+          <Button onClick={handleStartCdnResourcesConsolidationClick} danger>
+            {t('start')}
+          </Button>
+          {renderLastBatchExecution(lastCdnResourcesConsolidationBatch)}
+        </div>
+      </section>
+      <section className="TechnicalMaintenanceTab-section">
+        <div className="TechnicalMaintenanceTab-sectionHeader">{t('cdnDirectoriesCreationHeader')}</div>
+        <div className="TechnicalMaintenanceTab-sectionDescription">{t('cdnDirectoriesCreationDescription')}</div>
+        <div className="TechnicalMaintenanceTab-button">
+          <Button onClick={handleStartCdnUploadDirectoryCreationClick} danger>
+            {t('start')}
+          </Button>
+          {renderLastBatchExecution(lastCdnUploadDirectoryCreationBatch)}
+        </div>
+      </section >
     </div>
   );
 }
