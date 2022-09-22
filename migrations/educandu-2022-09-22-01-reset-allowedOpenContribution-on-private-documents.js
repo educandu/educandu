9c92c165d439d8034f55599083329308d577b20f
Adriana Luchian <adriana.dumitras@gmail.de>
2022-09-22 11:31:40 +0200

    EDU-819 Reset  allowedOpenContribution on existing private documents

---
+++ migrations/educandu-2022-09-22-01-reset-allowedOpenContribution-on-private-documents.js
@@ -0,0 +1,22 @@
+/* eslint-disable camelcase */
+export default class Educandu_2022_09_22_01_reset_allowedOpenContribution_on_private_documents {
+  constructor(db) {
+    this.db = db;
+  }
+
+  async up() {
+    await this.db.collection('documentRevisions').updateMany(
+      { roomId: { $ne: null } },
+      { $set: { allowedOpenContribution: 'metadataAndContent' } }
+    );
+
+    await this.db.collection('documents').updateMany(
+      { roomId: { $ne: null } },
+      { $set: { allowedOpenContribution: 'metadataAndContent' } }
+    );
+  }
+
+  down() {
+    throw new Error('Not implemented');
+  }
+}
