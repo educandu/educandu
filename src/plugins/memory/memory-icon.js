0a3034521a159e0c0a503360571f9213cff0cbdb
Adriana Luchian <adriana.dumitras@gmail.de>
2022-10-28 16:01:27 +0200

    EDU-906 Change memory icon

--- src/plugins/memory/memory-icon.js
+++ src/plugins/memory/memory-icon.js
@@ -5,8 +5,21 @@ const Icon = iconNs.default || iconNs;
 
 export function MemoryIconComponent() {
   return (
-    <svg height="1em" style={{ enableBackground: 'new 0 0 1000 1000' }} width="1em" viewBox="0 0 1000 1000">
-      <path style={{ fill: '#f2f2f2', stroke: '#666', strokeWidth: '40', strokeLinecap: 'round', strokeLinejoin: 'round', strokeMiterlimit: '10' }} d="M459.02 462.09H67.54a3.26 3.26 0 0 1-3.26-3.26V150.86c0-1.8 1.46-3.26 3.26-3.26h391.47c1.8 0 3.26 1.46 3.26 3.26v307.97c.01 1.8-1.45 3.26-3.25 3.26zm0 390.31H67.54a3.26 3.26 0 0 1-3.26-3.26V541.17c0-1.8 1.46-3.26 3.26-3.26h391.47c1.8 0 3.26 1.46 3.26 3.26v307.97c.01 1.8-1.45 3.26-3.25 3.26zm473.44-390.31H540.98a3.26 3.26 0 0 1-3.26-3.26V150.86c0-1.8 1.46-3.26 3.26-3.26h391.47c1.8 0 3.26 1.46 3.26 3.26v307.97c.01 1.8-1.45 3.26-3.25 3.26zm0 390.31H540.98a3.26 3.26 0 0 1-3.26-3.26V541.17c0-1.8 1.46-3.26 3.26-3.26h391.47c1.8 0 3.26 1.46 3.26 3.26v307.97c.01 1.8-1.45 3.26-3.25 3.26z" />
+    <svg width="1em" height="1em" viewBox="0 0 1000 1000" style={{ enableBackground: 'new 0 0 1000 1000' }} role="img" >
+      <path style={{ fill: '#f2f2f2', fillOpacity: 1, stroke: '#666', strokeWidth: 42.281, strokeLinecap: 'round', strokeLinejoin: 'round', strokeMiterlimit: 10, strokeDasharray: 'none', paintOrder: 'normal' }} d="M53.196 56.141h242.235v242.235H53.196z" />
+      <path d="m232.56 166.776-97.014-57.053c-7.138-4.198-16.055 1.045-16.055 9.44V233.27c0 8.395 8.917 13.638 16.055 9.44l97.015-57.054c7.138-4.197 7.138-14.682 0-18.88z" style={{ fill: '#666', strokeWidth: 0.726155 }} />
+      <path style={{ fill: '#666', fillOpacity: 1, stroke: '#666', strokeWidth: 42.281, strokeLinecap: 'round', strokeLinejoin: 'round', strokeMiterlimit: 10, strokeDasharray: 'none', paintOrder: 'normal' }} d="M377.789 55.944h242.235v242.235H377.789z" />
+      <path style={{ fill: '#f2f2f2', fillOpacity: 1, stroke: '#666', strokeWidth: 42.281, strokeLinecap: 'round', strokeLinejoin: 'round', strokeMiterlimit: 10, strokeDasharray: 'none', paintOrder: 'normal' }} d="M705.375 55.419H947.61v242.235H705.375z" />
+      <ellipse transform="rotate(-22.711)" cx={671.753} cy={521.352} rx={48.697} ry={32.463} style={{ fill: '#666', strokeWidth: 0.573448 }} />
+      <path className="st56" style={{ fill: 'none', stroke: '#666', strokeWidth: 25, strokeLinecap: 'round', strokeLinejoin: 'round', strokeMiterlimit: 10, strokeDasharray: 'none' }} d="M855.047 208.068v-96.109" />
+      <path style={{ fill: '#666', fillOpacity: 1, stroke: '#666', strokeWidth: 42.281, strokeLinecap: 'round', strokeLinejoin: 'round', strokeMiterlimit: 10, strokeDasharray: 'none', paintOrder: 'normal' }} d="M51.903 380.544h242.235v242.235H51.903zm325.283.158h242.235v242.235H377.186z" />
+      <path style={{ fill: '#f2f2f2', fillOpacity: 1, stroke: '#666', strokeWidth: 42.281, strokeLinecap: 'round', strokeLinejoin: 'round', strokeMiterlimit: 10, strokeDasharray: 'none', paintOrder: 'normal' }} d="M705.564 379.375h242.235V621.61H705.564z" />
+      <path style={{ fill: '#666', fillOpacity: 1, stroke: '#666', strokeWidth: 42.281, strokeLinecap: 'round', strokeLinejoin: 'round', strokeMiterlimit: 10, strokeDasharray: 'none', paintOrder: 'normal' }} d="M53.798 699.971h242.235v242.235H53.798z" />
+      <path style={{ fill: '#f2f2f2', fillOpacity: 1, stroke: '#666', strokeWidth: 42.281, strokeLinecap: 'round', strokeLinejoin: 'round', strokeMiterlimit: 10, strokeDasharray: 'none', paintOrder: 'normal' }} d="M377.042 699.239h242.235v242.235H377.042z" />
+      <ellipse transform="rotate(-22.711)" cx={121.091} cy={987.37} rx={48.697} ry={32.463} style={{ fill: '#666', strokeWidth: 0.573448 }} />
+      <path className="st56" style={{ fill: 'none', stroke: '#666', strokeWidth: 25, strokeLinecap: 'round', strokeLinejoin: 'round', strokeMiterlimit: 10, strokeDasharray: 'none' }} d="M527.003 850.555v-96.109" />
+      <path style={{ fill: '#666', fillOpacity: 1, stroke: '#666', strokeWidth: 42.281, strokeLinecap: 'round', strokeLinejoin: 'round', strokeMiterlimit: 10, strokeDasharray: 'none', paintOrder: 'normal' }} d="M705.02 698.56h242.235v242.235H705.02z" />
+      <path d="m882.348 490.473-97.014-57.054c-7.138-4.197-16.056 1.046-16.056 9.44v114.108c0 8.394 8.918 13.637 16.056 9.44l97.014-57.054c7.138-4.197 7.138-14.683 0-18.88z" style={{ fill: '#666', strokeWidth: 0.726155 }} />
     </svg>
   );
 }
