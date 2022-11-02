74d31b5b0db93c645e39b1611838d49969b4f2ff
Adriana Luchian <adriana.dumitras@gmail.de>
2022-11-02 16:12:29 +0100

    EDU-908 Make flip-card flip

--- src/components/flip-card.js
+++ src/components/flip-card.js
@@ -1,19 +1,47 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import React, { useState } from 'react';
 
-function FlipCard({ frontContent }) {
+function FlipCard({ frontContent, frontColor, backContent, backColor }) {
+  const [isFrontVisible, setIsFrontVisible] = useState(false);
+
+  const handleClick = () => {
+    setIsFrontVisible(prevState => !prevState);
+  };
+
+  const frontFaceStyle = frontColor ? { backgroundColor: frontColor } : null;
+  const backFaceStyle = backColor ? { backgroundColor: backColor } : null;
 
   return (
-    <div className="FlipCard">
-      <div className="FlipCard-frontContent">
-        {frontContent}
+    <div className="FlipCard" onClick={handleClick}>
+      <div className={classNames('FlipCard-content', { 'is-flipped': isFrontVisible })}>
+        <div className="FlipCard-face FlipCard-face--front" style={frontFaceStyle}>
+          <div className="FlipCard-faceContent">
+            {frontContent}
+          </div>
+        </div>
+        <div className="FlipCard-face FlipCard-face--back" style={backFaceStyle}>
+          <div className="FlipCard-faceContent">
+            {backContent}
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
 FlipCard.propTypes = {
-  frontContent: PropTypes.any.isRequired
+  backColor: PropTypes.string,
+  backContent: PropTypes.any,
+  frontColor: PropTypes.string,
+  frontContent: PropTypes.any
+};
+
+FlipCard.defaultProps = {
+  backColor: '',
+  backContent: null,
+  frontColor: '',
+  frontContent: null
 };
 
 export default FlipCard;
