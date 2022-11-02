c7731126b684e282edffc1c58284507401d8172d
Adriana Luchian <adriana.dumitras@gmail.de>
2022-11-02 13:34:14 +0100

    EDU-908 Display tiles as responsive squares

--- src/plugins/memory/memory-display.js
+++ src/plugins/memory/memory-display.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import classNames from 'classnames';
+import { SIZE } from './constants.js';
+import React, { useEffect, useState } from 'react';
+import Markdown from '../../components/markdown.js';
+import { getRandomizedTilesFromPairs } from './memory-utils.js';
 import { sectionDisplayProps } from '../../ui/default-prop-types.js';
 
-// eslint-disable-next-line no-unused-vars
 function MemoryDisplay({ content }) {
+  const { size, tilePairs, width } = content;
+  const [tiles, setTiles] = useState([]);
+
+  useEffect(() => {
+    setTiles(getRandomizedTilesFromPairs(tilePairs));
+  }, [tilePairs]);
+
+  const mainClasses = classNames(
+    'MemoryDisplay',
+    `u-width-${width}`,
+    { 'MemoryDisplay--3x3': size === SIZE.threeByThree },
+    { 'MemoryDisplay--4x4': size === SIZE.fourByFour }
+  );
+
+  const renderTile = (tile, index) => {
+    const reserveCentralSpace = size === SIZE.threeByThree && index === 4;
+    const elementsToRender = [];
+
+    if (reserveCentralSpace) {
+      elementsToRender.push((
+        <div key="emptyTile" className="MemoryDisplay-tile--empty" />
+      ));
+    }
+
+    elementsToRender.push((
+      <div key={index} className="MemoryDisplay-tile">
+        <div className="MemoryDisplay-tileContent">
+          <Markdown>{tile.text}</Markdown>
+        </div>
+      </div>
+    ));
+
+    return elementsToRender;
+  };
+
   return (
-    <div>Memory display</div>
+    <div className={mainClasses}>
+      {tiles.map(renderTile)}
+    </div>
   );
 }
 
--- src/plugins/memory/memory-utils.js
+++ src/plugins/memory/memory-utils.js
@@ -1,4 +1,5 @@
 import { SIZE } from './constants.js';
+import { shuffleItems } from '../../utils/array-utils.js';
 
 export const createDefaultTile = () => ({
   text: ''
@@ -23,4 +24,9 @@ export const resizeTilePairs = (oldTilePairs, newSize) => {
   );
 
   return newTilePairs;
+};
+
+export const getRandomizedTilesFromPairs = tilePairs => {
+  const tiles = tilePairs.flat();
+  return shuffleItems(tiles);
 };
