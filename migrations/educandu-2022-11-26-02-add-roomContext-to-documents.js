50bd45808714c8d2fc482b9bd45f72fe6e52cda5
Adriana Luchian <adriana.dumitras@gmail.de>
2022-11-28 17:01:15 +0700

    EDU-926 Migrate document model to include roomContext

---
+++ migrations/educandu-2022-11-26-02-add-roomContext-to-documents.js
@@ -0,0 +1,31 @@
+/* eslint-disable camelcase, no-console */
+export default class Educandu_2022_11_26_02_add_roomContext_to_documents {
+  constructor(db) {
+    this.db = db;
+  }
+
+  async collectionUp(collectionName) {
+    await this.db.collection(collectionName).updateMany(
+      { roomId: null },
+      { $set: { roomContext: null } }
+    );
+    await this.db.collection(collectionName).updateMany(
+      { roomId: { $ne: null } },
+      { $set: { 'roomContext.draft': false } }
+    );
+  }
+
+  async collectionDown(collectionName) {
+    await this.db.collection(collectionName).updateMany({ $unset: { roomContext: null } });
+  }
+
+  async up() {
+    await this.collectionUp('documentRevisions');
+    await this.collectionUp('documents');
+  }
+
+  async down() {
+    await this.collectionDown('documentRevisions');
+    await this.collectionDown('documents');
+  }
+}
