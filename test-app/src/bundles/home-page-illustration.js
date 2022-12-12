eac38fcb2a1a108bc6d0a02adcd89cf23de7a0a4
Adriana Luchian <adriana.dumitras@gmail.de>
2022-12-12 13:59:22 +0100

    EDU-934 Remove wrong (and redundant) SVG attribute

--- test-app/src/bundles/home-page-illustration.js
+++ test-app/src/bundles/home-page-illustration.js
@@ -12,7 +12,6 @@ function HomePageIllustration() {
       width="300"
       height="143"
       xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/2000/svg"
       >
       <rect
         x="0"
