cce0ad597bb3c2e534b1e62891d1b8e1f22fa885
Andreas Helmberger <public@ahelmberger.de>
2023-01-12 18:57:46 +0100

    EDU-1006 Replace media player in ear-training with plyr-based player

--- src/plugins/ear-training/ear-training-display.js
+++ src/plugins/ear-training/ear-training-display.js
@@ -14,7 +14,7 @@ import IterationPanel from '../../components/iteration-panel.js';
 import { useService } from '../../components/container-context.js';
 import CopyrightNotice from '../../components/copyright-notice.js';
 import { sectionDisplayProps } from '../../ui/default-prop-types.js';
-import MediaPlayer from '../../components/media-player/media-player.js';
+import MediaPlayer from '../../components/media-player/plyr/media-player.js';
 import { getAccessibleUrl, isInternalSourceType } from '../../utils/source-utils.js';
 
 const RadioButton = Radio.Button;
@@ -72,7 +72,7 @@ function EarTrainingDisplay({ content }) {
       <div className="EarTrainingDisplay-soundPlayer">
         {!!url && (
           <MediaPlayer
-            source={url}
+            sourceUrl={url}
             canDownload={canDownload}
             screenMode={MEDIA_SCREEN_MODE.none}
             />
