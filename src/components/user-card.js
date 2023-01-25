d5d5912ed0bd7d3bedfdc867892521681f0f65d4
Adriana Luchian <adriana.dumitras@gmail.de>
2023-01-25 20:00:44 +0100

    EDU-1063 Show default gravatar icon when no url is provided

--- src/components/user-card.js
+++ src/components/user-card.js
@@ -2,10 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Avatar, Button } from 'antd';
 import routes from '../utils/routes.js';
+import urlUtils from '../utils/url-utils.js';
 import { useTranslation } from 'react-i18next';
 import { AVATAR_SIZE } from '../domain/constants.js';
 
-function UserCard({ userId, displayName, avatarUrl }) {
+function UserCard({ userId, displayName, avatarUrl, detail }) {
   const { t } = useTranslation('userCard');
 
   const handleButtonClick = () => {
@@ -14,9 +15,15 @@ function UserCard({ userId, displayName, avatarUrl }) {
 
   return (
     <div className="UserCard">
-      <div>{displayName}</div>
-      <div>
-        <Avatar className="Avatar" shape="circle" size={AVATAR_SIZE} src={avatarUrl} />
+      <div className="UserCard-displayName">{displayName}</div>
+      <div className="UserCard-avatarWrapper">
+        <Avatar
+          shape="circle"
+          size={AVATAR_SIZE}
+          className="UserCard-avatar u-avatar"
+          src={avatarUrl || urlUtils.getGravatarUrl()}
+          />
+        {detail}
       </div>
       <Button type="primary" className="UserCard-button" onClick={handleButtonClick} disabled={!userId}>
         {t('profileButton')}
@@ -28,12 +35,14 @@ function UserCard({ userId, displayName, avatarUrl }) {
 UserCard.propTypes = {
   userId: PropTypes.string,
   avatarUrl: PropTypes.string,
-  displayName: PropTypes.string.isRequired
+  displayName: PropTypes.string.isRequired,
+  detail: PropTypes.node
 };
 
 UserCard.defaultProps = {
   userId: null,
-  avatarUrl: ''
+  avatarUrl: '',
+  detail: null
 };
 
 export default UserCard;
