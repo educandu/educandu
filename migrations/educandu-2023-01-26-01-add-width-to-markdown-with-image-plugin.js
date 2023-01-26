1f6eae8b414c47ed32aa217f9aefa509dee44ad6
Adriana Luchian <adriana.dumitras@gmail.de>
2023-01-26 19:36:10 +0100

    EDU-1055 Add width to markdown-with-image plugin

---
+++ migrations/educandu-2023-01-26-01-add-width-to-markdown-with-image-plugin.js
@@ -0,0 +1,59 @@
+export default class Educandu_2023_01_26_01_add_width_to_markdown_with_image_plugin {
+  constructor(db) {
+    this.db = db;
+  }
+
+  async collectionUp(collectionName) {
+    const result = await this.db.collection(collectionName).updateMany(
+      {},
+      {
+        $set: {
+          'sections.$[sectionElement].content.width': 100
+        }
+      },
+      {
+        arrayFilters: [
+          {
+            'sectionElement.type': 'markdown-with-image',
+            'sectionElement.content': { $ne: null }
+          }
+        ],
+        multi: true
+      }
+    );
+
+    console.log(`Updated ${collectionName}: ${JSON.stringify(result)}`);
+  }
+
+  async collectionDown(collectionName) {
+    const result = await this.db.collection(collectionName).updateMany(
+      {},
+      {
+        $unset: {
+          'sections.$[sectionElement].content.width': null
+        }
+      },
+      {
+        arrayFilters: [
+          {
+            'sectionElement.type': 'markdown-with-image',
+            'sectionElement.content': { $ne: null }
+          }
+        ],
+        multi: true
+      }
+    );
+
+    console.log(`Updated ${collectionName}: ${JSON.stringify(result)}`);
+  }
+
+  async up() {
+    await this.collectionUp('documents');
+    await this.collectionUp('documentRevisions');
+  }
+
+  async down() {
+    await this.collectionDown('documents');
+    await this.collectionDown('documentRevisions');
+  }
+}
