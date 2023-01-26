89aed2b4e4bd22dddb647dbb59354e14d8a28e0f
Adriana Luchian <adriana.dumitras@gmail.de>
2023-01-26 12:26:28 +0100

    EDU-1065 Improve badges and language icon styling

--- src/components/localization/language-icon.js
+++ src/components/localization/language-icon.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Tooltip } from 'antd';
 import PropTypes from 'prop-types';
+import { useLocale } from '../locale-context.js';
+import { useService } from '../container-context.js';
+import LanguageDataProvider from '../../localization/language-data-provider.js';
 
 export default function LanguageIcon({ language }) {
-  return <div className="LanguageIcon">{language.toUpperCase()}</div>;
+  const { uiLanguage } = useLocale();
+  const languageDataProvider = useService(LanguageDataProvider);
+  const { name: languageName } = languageDataProvider.getLanguageData(language, uiLanguage);
+
+  return (
+    <Tooltip title={languageName}>
+      <div className="LanguageIcon">{language.toUpperCase()}</div>
+    </Tooltip>
+  );
 }
 
 LanguageIcon.propTypes = {
