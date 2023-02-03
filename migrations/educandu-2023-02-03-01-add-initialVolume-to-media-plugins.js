5aa58be141fb1cecabd89fe466e3fab5593a1237
Adriana Luchian <adriana.dumitras@gmail.de>
2023-02-03 15:24:45 +0100

    EDU-1094 Merge all migration scripts into one

---
+++ migrations/educandu-2023-02-03-01-add-initialVolume-to-media-plugins.js
@@ -0,0 +1,69 @@
+export default class Educandu_2023_02_03_01_add_initialVolume_to_media_plugins {
+  constructor(db) {
+    this.db = db;
+  }
+
+  async pluginUp(collectionName, pluginName) {
+    await this.db.collection(collectionName).updateMany(
+      {},
+      {
+        $set: {
+          'sections.$[sectionElement].content.initialVolume': 1
+        }
+      },
+      {
+        arrayFilters: [
+          {
+            'sectionElement.type': pluginName,
+            'sectionElement.content': { $ne: null }
+          }
+        ],
+        multi: true
+      }
+    );
+  }
+
+  async pluginDown(collectionName, pluginName) {
+    await this.db.collection(collectionName).updateMany(
+      {},
+      {
+        $unset: {
+          'sections.$[sectionElement].content.initialVolume': null
+        }
+      },
+      {
+        arrayFilters: [
+          {
+            'sectionElement.type': pluginName,
+            'sectionElement.content': { $ne: null }
+          }
+        ],
+        multi: true
+      }
+    );
+  }
+
+  async collectionUp(collectionName) {
+    await this.pluginUp(collectionName, 'interactive-media');
+    await this.pluginUp(collectionName, 'media-slideshow');
+    await this.pluginUp(collectionName, 'multitrack-media');
+    await this.pluginUp(collectionName, 'media-analysis');
+  }
+
+  async collectionDown(collectionName) {
+    await this.pluginDown(collectionName, 'interactive-media');
+    await this.pluginDown(collectionName, 'media-slideshow');
+    await this.pluginDown(collectionName, 'multitrack-media');
+    await this.pluginDown(collectionName, 'media-analysis');
+  }
+
+  async up() {
+    await this.collectionUp('documents');
+    await this.collectionUp('documentRevisions');
+  }
+
+  async down() {
+    await this.collectionDown('documents');
+    await this.collectionDown('documentRevisions');
+  }
+}
