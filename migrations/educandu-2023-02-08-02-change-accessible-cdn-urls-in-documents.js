400f68ab4f832370b89dc7514c4ad3ffe112a04b
Adriana Luchian <adriana.dumitras@gmail.de>
2023-02-08 13:16:10 +0100

    EDU-1090 Correct migration scripts to consider URLs to other external CDN

--- migrations/educandu-2023-02-08-02-change-accessible-cdn-urls-in-documents.js
+++ migrations/educandu-2023-02-08-02-change-accessible-cdn-urls-in-documents.js
@@ -41,19 +41,27 @@ const redactUrl = (url = '') => {
     portableUrl = portableUrl.replace(OLD_MEDIA_CDN_PREFIX, 'cdn://document-media/');
   }
 
-  return portableUrl;
+  const isRedacted = url !== portableUrl;
+
+  if (isRedacted) {
+    console.log(`Redacted:\n  ${url}\n  ${portableUrl}`);
+  }
+
+  return {
+    url: portableUrl,
+    isRedacted
+  };
 };
 
 const redactMarkdown = text => {
   let isRedacted = false;
 
   const redactUrlCallback = url => {
-    const redactedUrl = redactUrl(url);
-    if (!isRedacted && redactedUrl !== url) {
+    const result = redactUrl(url);
+    if (result.isRedacted) {
       isRedacted = true;
-      console.log(`Redacted:\n  ${url}\n  ${redactedUrl}`);
     }
-    return redactedUrl;
+    return result.url;
   };
 
   const redactedMarkdown = redactUrlsInMarkdown(text, redactUrlCallback);
@@ -83,104 +91,165 @@ export default class Educandu_2023_02_08_02_change_accessible_cdn_urls_in_docume
           result = redactMarkdown(section.content.copyrightNotice);
           if (result.isRedacted) {
             section.content.copyrightNotice = result.markdown;
-            isUpdated ||= result.isRedacted;
+            isUpdated = true;
           }
           break;
         case 'annotation':
           result = redactMarkdown(section.content.title);
           if (result.isRedacted) {
             section.content.title = result.markdown;
-            isUpdated ||= result.isRedacted;
+            isUpdated = true;
           }
           result = redactMarkdown(section.content.text);
           if (result.isRedacted) {
             section.content.text = result.markdown;
-            isUpdated ||= result.isRedacted;
+            isUpdated = true;
           }
           break;
         case 'audio':
+          result = redactUrl(section.content.sourceUrl);
+          if (result.isRedacted) {
+            section.content.sourceUrl = result.url;
+            isUpdated = true;
+          }
           result = redactMarkdown(section.content.copyrightNotice);
           if (result.isRedacted) {
             section.content.copyrightNotice = result.markdown;
-            isUpdated ||= result.isRedacted;
+            isUpdated = true;
+          }
+          break;
+        case 'audio-waveform':
+          result = redactUrl(section.content.sourceUrl);
+          if (result.isRedacted) {
+            section.content.sourceUrl = result.url;
+            isUpdated = true;
+          }
+          break;
+        case 'catalog':
+          for (const item of section.content.items) {
+            result = redactUrl(item.image.sourceUrl);
+            if (result.isRedacted) {
+              item.image.sourceUrl = result.url;
+              isUpdated = true;
+            }
           }
           break;
         case 'ear-training':
           result = redactMarkdown(section.content.title);
           if (result.isRedacted) {
             section.content.title = result.markdown;
-            isUpdated ||= result.isRedacted;
+            isUpdated = true;
           }
           for (const test of section.content.tests) {
             if (test.sound) {
+              result = redactUrl(test.sound.sourceUrl);
+              if (result.isRedacted) {
+                test.sound.sourceUrl = result.url;
+                isUpdated = true;
+              }
               result = redactMarkdown(test.sound.copyrightNotice);
               if (result.isRedacted) {
                 test.sound.copyrightNotice = result.markdown;
-                isUpdated ||= result.isRedacted;
+                isUpdated = true;
               }
             }
             if (test.questionImage) {
+              result = redactUrl(test.questionImage.sourceUrl);
+              if (result.isRedacted) {
+                test.questionImage.sourceUrl = result.url;
+                isUpdated = true;
+              }
               result = redactMarkdown(test.questionImage.copyrightNotice);
               if (result.isRedacted) {
                 test.questionImage.copyrightNotice = result.markdown;
-                isUpdated ||= result.isRedacted;
+                isUpdated = true;
               }
             }
             if (test.answerImage) {
+              result = redactUrl(test.answerImage.sourceUrl);
+              if (result.isRedacted) {
+                test.answerImage.sourceUrl = result.url;
+                isUpdated = true;
+              }
               result = redactMarkdown(test.answerImage.copyrightNotice);
               if (result.isRedacted) {
                 test.answerImage.copyrightNotice = result.markdown;
-                isUpdated ||= result.isRedacted;
+                isUpdated = true;
               }
             }
           }
           break;
         case 'image':
+          result = redactUrl(section.content.sourceUrl);
+          if (result.isRedacted) {
+            section.content.sourceUrl = result.url;
+            isUpdated = true;
+          }
           result = redactMarkdown(section.content.copyrightNotice);
           if (result.isRedacted) {
             section.content.copyrightNotice = result.markdown;
-            isUpdated ||= result.isRedacted;
+            isUpdated = true;
           }
           if (section.content.hoverEffect) {
+            result = redactUrl(section.content.hoverEffect.sourceUrl);
+            if (result.isRedacted) {
+              section.content.hoverEffect.sourceUrl = result.url;
+              isUpdated = true;
+            }
             result = redactMarkdown(section.content.hoverEffect.copyrightNotice);
             if (result.isRedacted) {
               section.content.hoverEffect.copyrightNotice = result.markdown;
-              isUpdated ||= result.isRedacted;
+              isUpdated = true;
             }
           }
           if (section.content.revealEffect) {
+            result = redactUrl(section.content.revealEffect.sourceUrl);
+            if (result.isRedacted) {
+              section.content.revealEffect.sourceUrl = result.url;
+              isUpdated = true;
+            }
             result = redactMarkdown(section.content.revealEffect.copyrightNotice);
             if (result.isRedacted) {
               section.content.revealEffect.copyrightNotice = result.markdown;
-              isUpdated ||= result.isRedacted;
+              isUpdated = true;
             }
           }
           break;
         case 'interactive-media':
+          result = redactUrl(section.content.sourceUrl);
+          if (result.isRedacted) {
+            section.content.sourceUrl = result.url;
+            isUpdated = true;
+          }
           result = redactMarkdown(section.content.copyrightNotice);
           if (result.isRedacted) {
             section.content.copyrightNotice = result.markdown;
-            isUpdated ||= result.isRedacted;
+            isUpdated = true;
           }
           break;
         case 'markdown':
           result = redactMarkdown(section.content.text);
           if (result.isRedacted) {
             section.content.text = result.markdown;
-            isUpdated ||= result.isRedacted;
+            isUpdated = true;
           }
           break;
         case 'markdown-with-image':
           result = redactMarkdown(section.content.text);
           if (result.isRedacted) {
             section.content.text = result.markdown;
-            isUpdated ||= result.isRedacted;
+            isUpdated = true;
           }
           if (section.content.image) {
+            result = redactUrl(section.content.image.sourceUrl);
+            if (result.isRedacted) {
+              section.content.image.sourceUrl = result.url;
+              isUpdated = true;
+            }
             result = redactMarkdown(section.content.image.copyrightNotice);
             if (result.isRedacted) {
               section.content.image.copyrightNotice = result.markdown;
-              isUpdated ||= result.isRedacted;
+              isUpdated = true;
             }
           }
           break;
@@ -190,76 +259,125 @@ export default class Educandu_2023_02_08_02_change_accessible_cdn_urls_in_docume
               result = redactMarkdown(tile.text);
               if (result.isRedacted) {
                 tile.text = result.markdown;
-                isUpdated ||= result.isRedacted;
+                isUpdated = true;
+              }
+              result = redactUrl(tile.sourceUrl);
+              if (result.isRedacted) {
+                tile.sourceUrl = result.url;
+                isUpdated = true;
               }
             }
           }
           break;
         case 'media-analysis':
+          result = redactUrl(section.content.mainTrack.sourceUrl);
+          if (result.isRedacted) {
+            section.content.mainTrack.sourceUrl = result.url;
+            isUpdated = true;
+          }
           result = redactMarkdown(section.content.mainTrack.copyrightNotice);
           if (result.isRedacted) {
             section.content.mainTrack.copyrightNotice = result.markdown;
-            isUpdated ||= result.isRedacted;
+            isUpdated = true;
           }
           for (const secondaryTrack of section.content.secondaryTracks) {
+            result = redactUrl(secondaryTrack.sourceUrl);
+            if (result.isRedacted) {
+              secondaryTrack.sourceUrl = result.url;
+              isUpdated = true;
+            }
             result = redactMarkdown(secondaryTrack.copyrightNotice);
             if (result.isRedacted) {
               secondaryTrack.copyrightNotice = result.markdown;
-              isUpdated ||= result.isRedacted;
+              isUpdated = true;
             }
           }
           break;
         case 'media-slideshow':
+          result = redactUrl(section.content.sourceUrl);
+          if (result.isRedacted) {
+            section.content.sourceUrl = result.url;
+            isUpdated = true;
+          }
           result = redactMarkdown(section.content.copyrightNotice);
           if (result.isRedacted) {
             section.content.copyrightNotice = result.markdown;
-            isUpdated ||= result.isRedacted;
+            isUpdated = true;
           }
           for (const chapter of section.content.chapters) {
             if (chapter.image) {
+              result = redactUrl(chapter.image.sourceUrl);
+              if (result.isRedacted) {
+                chapter.image.sourceUrl = result.url;
+                isUpdated = true;
+              }
               result = redactMarkdown(chapter.image.copyrightNotice);
               if (result.isRedacted) {
                 chapter.image.copyrightNotice = result.markdown;
-                isUpdated ||= result.isRedacted;
+                isUpdated = true;
               }
             }
           }
           break;
         case 'multitrack-media':
+          result = redactUrl(section.content.mainTrack.sourceUrl);
+          if (result.isRedacted) {
+            section.content.mainTrack.sourceUrl = result.url;
+            isUpdated = true;
+          }
           result = redactMarkdown(section.content.mainTrack.copyrightNotice);
           if (result.isRedacted) {
             section.content.mainTrack.copyrightNotice = result.markdown;
-            isUpdated ||= result.isRedacted;
+            isUpdated = true;
           }
           for (const secondaryTrack of section.content.secondaryTracks) {
+            result = redactUrl(secondaryTrack.sourceUrl);
+            if (result.isRedacted) {
+              secondaryTrack.sourceUrl = result.url;
+              isUpdated = true;
+            }
             result = redactMarkdown(secondaryTrack.copyrightNotice);
             if (result.isRedacted) {
               secondaryTrack.copyrightNotice = result.markdown;
-              isUpdated ||= result.isRedacted;
+              isUpdated = true;
             }
+          }
+          break;
+        case 'music-xml-viewer':
+          result = redactUrl(section.content.sourceUrl);
+          if (result.isRedacted) {
+            section.content.sourceUrl = result.url;
+            isUpdated = true;
+          }
+          break;
+        case 'pdf-viewer':
+          result = redactUrl(section.content.sourceUrl);
+          if (result.isRedacted) {
+            section.content.sourceUrl = result.url;
+            isUpdated = true;
           }
           break;
         case 'quick-tester':
           result = redactMarkdown(section.content.title);
           if (result.isRedacted) {
             section.content.title = result.markdown;
-            isUpdated ||= result.isRedacted;
+            isUpdated = true;
           }
           result = redactMarkdown(section.content.teaser);
           if (result.isRedacted) {
             section.content.teaser = result.markdown;
-            isUpdated ||= result.isRedacted;
+            isUpdated = true;
           }
           for (const test of section.content.tests) {
             result = redactMarkdown(test.question);
             if (result.isRedacted) {
               test.question = result.markdown;
-              isUpdated ||= result.isRedacted;
+              isUpdated = true;
             }
             result = redactMarkdown(test.answer);
             if (result.isRedacted) {
               test.answer = result.markdown;
-              isUpdated ||= result.isRedacted;
+              isUpdated = true;
             }
           }
           break;
@@ -268,7 +386,7 @@ export default class Educandu_2023_02_08_02_change_accessible_cdn_urls_in_docume
             result = redactMarkdown(cell.text);
             if (result.isRedacted) {
               cell.text = result.markdown;
-              isUpdated ||= result.isRedacted;
+              isUpdated = true;
             }
           }
           break;
@@ -276,14 +394,26 @@ export default class Educandu_2023_02_08_02_change_accessible_cdn_urls_in_docume
           result = redactMarkdown(section.content.text);
           if (result.isRedacted) {
             section.content.text = result.markdown;
-            isUpdated ||= result.isRedacted;
+            isUpdated = true;
           }
           break;
         case 'video':
+          result = redactUrl(section.content.sourceUrl);
+          if (result.isRedacted) {
+            section.content.sourceUrl = result.url;
+            isUpdated = true;
+          }
           result = redactMarkdown(section.content.copyrightNotice);
           if (result.isRedacted) {
             section.content.copyrightNotice = result.markdown;
-            isUpdated ||= result.isRedacted;
+            isUpdated = true;
+          }
+          if (section.content.posterImage) {
+            result = redactUrl(section.content.posterImage.sourceUrl);
+            if (result.isRedacted) {
+              section.content.posterImage.sourceUrl = result.url;
+              isUpdated = true;
+            }
           }
           break;
         default:
