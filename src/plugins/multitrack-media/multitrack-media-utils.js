b20280e4bf055b1ee0fc72b362ce7ee883de1366
Adriana Luchian <adriana.dumitras@gmail.de>
2023-02-10 15:03:15 +0100

    EDU-1115 Correct joi validation for multitrack-media plugin

--- src/plugins/multitrack-media/multitrack-media-utils.js
+++ src/plugins/multitrack-media/multitrack-media-utils.js
@@ -34,13 +34,13 @@ export function createDefaultContent(t) {
 
 export function validateContent(content) {
   const schema = joi.object({
-    tracks: joi.object({
+    tracks: joi.array().items(joi.object({
       key: joi.string().required(),
       name: joi.string().allow('').required(),
       sourceUrl: joi.string().allow('').required(),
       copyrightNotice: joi.string().allow('').required(),
       playbackRange: joi.array().items(joi.number().min(0).max(1)).required()
-    }).min(1).required(),
+    })).min(1).required(),
     volumePresets: joi.array().items(joi.object({
       name: joi.string().required(),
       tracks: joi.array().items(joi.number().min(0).max(1)).min(1).required()
