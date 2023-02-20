0d92caa5c2d07feb50ec2c711f4c1b0b01fe0feb
Adriana Luchian <adriana.dumitras@gmail.de>
2023-02-20 16:39:46 +0100

    EDU-1145 Remove homepageInfo usage from homepage

--- test-app/src/bundles/home-page-template.js
+++ test-app/src/bundles/home-page-template.js
@@ -2,15 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SiteLogo from './site-logo.js';
 import { useTranslation } from 'react-i18next';
-import Markdown from '../../../src/components/markdown.js';
 import HomePageIllustration from './home-page-illustration.js';
 import ConsentDialog from '../../../src/components/consent-dialog.js';
-import { useSettings } from '../../../src/components/settings-context.js';
 import DefaultPageFooter from '../../../src/components/default-page-footer.js';
 import DefaultPageHeader from '../../../src/components/default-page-header.js';
 
 function HomePageTemplate({ children }) {
-  const settings = useSettings();
   const { t } = useTranslation('testApp');
 
   return (
@@ -24,9 +21,6 @@ function HomePageTemplate({ children }) {
           </div>
           <div>
             {children}
-            {!!settings.homepageInfo && (
-              <div className="HomePageTemplate-info"><Markdown>{settings.homepageInfo}</Markdown></div>
-            )}
           </div>
           <div className="HomePageTemplate-illustration">
             <HomePageIllustration />
