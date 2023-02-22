152422487ebb056340059fc0a7f16900fb95df53
Adriana Luchian <adriana.dumitras@gmail.de>
2023-02-22 20:25:31 +0100

    EDU-1149 Replace fold effect SVGs

--- src/components/homepage/homepage-fold-divider-bottom.js
+++ src/components/homepage/homepage-fold-divider-bottom.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 function HomepageFoldDividerBottom() {
   return (
-    <svg viewBox="0 0 1260 179" fill="none">
-      <path d="M0 0.285156H1259V177.285H0V0.285156Z" fill="#F2F5F8" />
-      <path d="M1260 0.285156C777.806 236.635 501.606 238.529 0 0.285156V178.285H1260V0.285156Z" fill="#F8FBFE" />
-      <path d="M1260 72.2893C772.069 212.715 495.945 211.88 0 72.2893V178.57H1260V72.2893Z" fill="white" />
+    <svg viewBox="0 0 1260 179" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 0H1260V178H0V0Z" fill="#F2F5F8" />
+      <path d="M1260 0C777.806 236.35 501.606 238.244 0 0V178H1260V0Z" fill="#F8FBFE" />
+      <path d="M1260 72.0042C772.069 212.429 495.945 211.595 0 72.0042V178.285H1260V72.0042Z" fill="white" />
     </svg>
+
   );
 }
 
--- src/components/homepage/homepage-fold-divider-top.js
+++ src/components/homepage/homepage-fold-divider-top.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function HomepageFoldDividerTop() {
   return (
-    <svg viewBox="0 0 1260 181" fill="none">
-      <path d="M0 0.999969H1259V178H0V0.999969Z" fill="white" />
-      <path d="M1260 -3.05176e-05C777.806 236.35 501.606 238.244 0 -3.05176e-05V178H1260V-3.05176e-05Z" fill="#F8FBFE" />
-      <path d="M1260 74.0042C772.069 214.429 495.945 213.595 0 74.0042V180.285L1260 179.285V74.0042Z" fill="#F2F5F8" />
+    <svg viewBox="0 0 1260 179" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 0H1260V179H0V0Z" fill="white" />
+      <path d="M1260 0C777.807 237.362 501.604 239.272 0 0V179H1260V0Z" fill="#F8FBFE" />
+      <path d="M1260 73.7191C772.069 214.144 495.945 213.31 0 73.7191V179H1260V73.7191Z" fill="#F2F5F8" />
     </svg>
   );
 }
