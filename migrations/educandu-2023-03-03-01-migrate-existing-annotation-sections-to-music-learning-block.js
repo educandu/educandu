b2eedd8d113522d6466241e881f878abc5a35146
Adriana Luchian <adriana.dumitras@gmail.de>
2023-03-06 13:41:34 +0100

    EDU-1198 Adjust migration script to only target documents by specific users

--- migrations/educandu-2023-03-03-01-migrate-existing-annotation-sections-to-music-learning-block.js
+++ migrations/educandu-2023-03-03-01-migrate-existing-annotation-sections-to-music-learning-block.js
@@ -6,13 +6,29 @@ const intentToColorSchemeMap = {
   discourage: 'red'
 };
 
+const userIdsForWhichToMigrateAnnotations = [
+  'uJyqKB3Gj9k6QQd1mMpTL6', // 'Ulrich Kaiser'
+  'jasrYVbSkCPW5sRZ5xRrPM', // 'Ulrich Kaiser'
+  'eJegGPCwStwqoPTvkrh7Hm', // 'uk-noAdmin'
+  '4tJUQpXVTnxCkXoaCiJrsj', // 'Ilka von der OMA'
+  'tfRAdWo2efidGWjcfiybo5', // 'Ilka testet'
+  'nDYm85bAobMGo74ftDtaAn', // 'Mani Vieregg'
+  'pJ9fxdW9wWoXv1iEs9Vf81', // 'Mathias Kupczyk'
+  'sUZWHnscM62MWD66aw8tZJ' // 'Lukas-Eichenauer'
+];
+
 export default class Educandu_2023_03_03_01_migrate_existing_annotation_sections_to_music_learning_block {
   constructor(db) {
     this.db = db;
   }
 
   async processCollection(collectionName) {
-    const cursor = this.db.collection(collectionName).find({ 'sections.type': 'annotation' });
+    const cursor = this.db.collection(collectionName).find({
+      $and: [
+        { 'sections.type': 'annotation' },
+        { createdBy: { $in: userIdsForWhichToMigrateAnnotations } }
+      ]
+    });
 
     let count = 0;
     while (await cursor.hasNext()) {
