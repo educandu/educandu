a4046051b58983751ecd182638662fff6be1d77e
Adriana Luchian <adriana.dumitras@gmail.de>
2023-03-07 15:54:34 +0100

    EDU-1193 Add filter to rooms tag

--- src/components/dashboard/rooms-tab.js
+++ src/components/dashboard/rooms-tab.js
@@ -2,24 +2,49 @@ import by from 'thenby';
 import PropTypes from 'prop-types';
 import { Button, Spin } from 'antd';
 import RoomCard from '../room-card.js';
-import React, { useState } from 'react';
-import Restricted from '../restricted.js';
 import { useUser } from '../user-context.js';
+import FilterInput from '../filter-input.js';
 import { useTranslation } from 'react-i18next';
-import permissions from '../../domain/permissions.js';
+import React, { useEffect, useState } from 'react';
 import RoomCreationModal from '../room-creation-modal.js';
 
 function RoomsTab({ rooms, invitations, loading }) {
   const user = useUser();
   const { t } = useTranslation('roomsTab');
 
-  const ownedRooms = rooms.filter(room => room.owner._id === user._id)
-    .sort(by(room => room.createdOn));
-
-  const memberOfRooms = rooms.filter(room => room.owner._id !== user._id)
-    .sort(by(room => room.members.find(member => member.userId === user._id).joinedOn));
-
+  const [filterText, setFilterText] = useState(null);
+  const [displayedOwnedRooms, setDisplayedOwnedRooms] = useState([]);
+  const [displayedInvitations, setDisplayedInvitations] = useState([]);
+  const [displayedMemberOfRooms, setDisplayedMemberOfRooms] = useState([]);
   const [isRoomCreationModalOpen, setIsRoomCreationModalOpen] = useState(false);
+
+  useEffect(() => {
+    const lowerCasedFilter = (filterText || '').toLowerCase();
+
+    const ownedRooms = rooms
+      .filter(room => room.owner._id === user._id)
+      .sort(by(room => room.createdOn));
+
+    const filteredOwnedRooms = filterText
+      ? ownedRooms.filter(room => room.name.toLowerCase().includes(lowerCasedFilter))
+      : ownedRooms;
+
+    const memberOfRooms = rooms
+      .filter(room => room.owner._id !== user._id)
+      .sort(by(room => room.members.find(member => member.userId === user._id).joinedOn));
+
+    const filteredMemberOfRooms = filterText
+      ? memberOfRooms.filter(room => room.name.toLowerCase().includes(lowerCasedFilter))
+      : memberOfRooms;
+
+    const filteredInvitations = filterText
+      ? invitations.filter(invitation => invitation.room.name.toLowerCase().includes(lowerCasedFilter))
+      : invitations;
+
+    setDisplayedOwnedRooms(filteredOwnedRooms);
+    setDisplayedMemberOfRooms(filteredMemberOfRooms);
+    setDisplayedInvitations(filteredInvitations);
+  }, [user, rooms, invitations, filterText]);
 
   const handleCreateRoomClick = () => {
     setIsRoomCreationModalOpen(true);
@@ -29,29 +54,36 @@ function RoomsTab({ rooms, invitations, loading }) {
     setIsRoomCreationModalOpen(false);
   };
 
+  const handleFilterTextChange = event => {
+    const { value } = event.target;
+    setFilterText(value);
+  };
+
   return (
     <div className="RoomsTab">
       <div className="RoomsTab-info">{t('info')}</div>
 
-      <Restricted to={permissions.OWN_ROOMS}>
-        <Button className="RoomsTab-createRoomButton" type="primary" onClick={handleCreateRoomClick}>
-          {t('common:createRoom')}
-        </Button>
-      </Restricted>
+      <Button className="RoomsTab-createRoomButton" type="primary" onClick={handleCreateRoomClick}>
+        {t('common:createRoom')}
+      </Button>
+
+      <div className="RoomsTab-filter">
+        <FilterInput value={filterText} onChange={handleFilterTextChange} disabled={!!loading || !rooms.length} />
+      </div>
 
       <div className="RoomsTab-roomsGroupHeadline">{t('ownedRoomsHeadline')}</div>
       <section className="RoomsTab-roomsGroup">
         {!!loading && <Spin className="u-spin" />}
-        {!loading && ownedRooms.map(room => (<RoomCard key={room._id} room={room} />))}
-        {!loading && !ownedRooms.length && <div className="RoomsTab-noRoomsInGroup">{t('noOwnedRooms')}</div>}
+        {!loading && displayedOwnedRooms.map(room => (<RoomCard key={room._id} room={room} />))}
+        {!loading && !displayedOwnedRooms.length && <div className="RoomsTab-noRoomsInGroup">{t('noOwnedRooms')}</div>}
       </section>
 
       <div className="RoomsTab-roomsGroupHeadline">{t('memberRoomsHeadline')}</div>
       <section className="RoomsTab-roomsGroup">
         {!!loading && <Spin className="u-spin" />}
-        {!loading && memberOfRooms.map(room => <RoomCard key={room._id} room={room} />)}
-        {!loading && invitations.map(invitation => <RoomCard key={invitation._id} room={invitation.room} invitation={invitation} />)}
-        {!loading && !memberOfRooms.length && !invitations.length && <div className="RoomsTab-noRoomsInGroup">{t('noMemberRooms')}</div>}
+        {!loading && displayedMemberOfRooms.map(room => <RoomCard key={room._id} room={room} />)}
+        {!loading && displayedInvitations.map(invitation => <RoomCard key={invitation._id} room={invitation.room} invitation={invitation} />)}
+        {!loading && !displayedMemberOfRooms.length && !displayedInvitations.length && <div className="RoomsTab-noRoomsInGroup">{t('noMemberRooms')}</div>}
       </section>
 
       <RoomCreationModal isOpen={isRoomCreationModalOpen} onClose={handleRoomCreationModalClose} />
