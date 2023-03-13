2bfa2ca19bd4f31691cba332a683ec71a694acd8
Adriana Luchian <adriana.dumitras@gmail.de>
2023-03-13 14:30:31 +0100

    EDU-1221 Support inverting site logo color

--- src/components/default-site-logo.js
+++ src/components/default-site-logo.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-export default function DefaultSiteLogo() {
+export default function DefaultSiteLogo({ inverted }) {
   return (
-    <div className="DefaultSiteLogo">educandu</div>
+    <div className={classNames('DefaultSiteLogo', { 'DefaultSiteLogo--inverted': inverted })}>educandu</div>
   );
 }
+
+DefaultSiteLogo.propTypes = {
+  inverted: PropTypes.bool
+};
+
+DefaultSiteLogo.defaultProps = {
+  inverted: false
+};
