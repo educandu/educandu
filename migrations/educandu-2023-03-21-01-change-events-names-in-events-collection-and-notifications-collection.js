8657b054de587618ca5b7964b4ac1f4daee7e7cb
Adriana Luchian <adriana.dumitras@gmail.de>
2023-03-21 11:13:53 +0100

    NOTICKET: Correct events and notifications migration script

--- migrations/educandu-2023-03-21-01-change-events-names-in-events-collection-and-notifications-collection.js
+++ migrations/educandu-2023-03-21-01-change-events-names-in-events-collection-and-notifications-collection.js
@@ -1,20 +1,28 @@
-export default class Educandu_2023_03_20_01_change_events_names_in_events_collection_and_notifications_collection {
+export default class Educandu_2023_03_21_01_change_events_names_in_events_collection_and_notifications_collection {
   constructor(db) {
     this.db = db;
   }
 
   async up() {
+    await this.db.collection('events').updateMany({ type: 'revision-created' }, { $rename: { 'params.revisionId': 'params.documentRevisionId' } });
+
     await this.db.collection('events').updateMany({ type: 'revision-created' }, { $set: { type: 'document-revision-created' } });
-    await this.db.collection('events').updateMany({ type: 'revision-created' }, { $rename: { 'eventParams.revisionId': 'eventParams.documentRevisionId' } });
     await this.db.collection('events').updateMany({ type: 'comment-created' }, { $set: { type: 'document-comment-created' } });
+
+    await this.db.collection('notifications').updateMany({ eventType: 'revision-created' }, { $rename: { 'eventParams.revisionId': 'eventParams.documentRevisionId' } });
+
     await this.db.collection('notifications').updateMany({ eventType: 'revision-created' }, { $set: { eventType: 'document-revision-created' } });
     await this.db.collection('notifications').updateMany({ eventType: 'comment-created' }, { $set: { eventType: 'document-comment-created' } });
   }
 
   async down() {
+    await this.db.collection('events').updateMany({ type: 'document-revision-created' }, { $rename: { 'params.documentRevisionId': 'params.revisionId' } });
+
     await this.db.collection('events').updateMany({ type: 'document-revision-created' }, { $set: { type: 'revision-created' } });
-    await this.db.collection('events').updateMany({ type: 'revision-created' }, { $rename: { 'eventParams.documentRevisionId': 'eventParams.revisionId' } });
     await this.db.collection('events').updateMany({ type: 'document-comment-created' }, { $set: { type: 'comment-created' } });
+
+    await this.db.collection('notifications').updateMany({ eventType: 'document-revision-created' }, { $rename: { 'eventParams.documentRevisionId': 'eventParams.revisionId' } });
+
     await this.db.collection('notifications').updateMany({ eventType: 'document-revision-created' }, { $set: { eventType: 'revision-created' } });
     await this.db.collection('notifications').updateMany({ eventType: 'document-comment-created' }, { $set: { eventType: 'comment-created' } });
   }
