04760c65e2c4c182b7ba1a03973ee020296cc0d1
Andreas Helmberger <public@ahelmberger.de>
2023-03-21 18:13:16 +0100

    Revert "NOTICKET: Preload the display component of all built-in plugins"
    This reverts commit 528dd9f513d2e2033c81fa4adc8e0d4ae7c2720c.

--- src/plugins/registered-plugin.js
+++ src/plugins/registered-plugin.js
@@ -1,26 +1,17 @@
-import Logger from '../common/logger.js';
-
-const logger = new Logger(import.meta.url);
-
 class RegisteredPlugin {
-  constructor(name, info, displayComponent = null) {
+  constructor(name, info) {
     this.name = name;
     this.info = info;
     this.editorComponent = null;
-    this.displayComponent = displayComponent || null;
+    this.displayComponent = null;
   }
 
   async ensureDisplayComponentIsLoaded() {
-    if (!this.displayComponent) {
-      logger.warn(`Loading display component asynchronously for plugin ${this.name}`);
-      this.displayComponent = await this.info.resolveDisplayComponent();
-    }
+    this.displayComponent = await this.info.resolveDisplayComponent();
   }
 
   async ensureEditorComponentIsLoaded() {
-    if (!this.editorComponent) {
-      this.editorComponent = await this.info.resolveEditorComponent();
-    }
+    this.editorComponent = await this.info.resolveEditorComponent();
   }
 }
 
