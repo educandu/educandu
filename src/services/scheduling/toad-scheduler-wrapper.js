8828c61bcfb792893c4bd24f1025edc1aa377293
Andreas Helmberger <public@ahelmberger.de>
2023-03-21 18:21:55 +0100

    EDU-1237 Remove toad scheduler to check for any performance improvements

--- src/services/scheduling/toad-scheduler-wrapper.js
+++ src/services/scheduling/toad-scheduler-wrapper.js
@@ -1,22 +1,24 @@
-import 'croner';
-import toadScheduler from 'toad-scheduler';
+/* eslint-disable no-unused-vars */
 
-const { ToadScheduler, AsyncTask, SimpleIntervalJob, CronJob } = toadScheduler;
+// import 'croner';
+// import toadScheduler from 'toad-scheduler';
+
+// const { ToadScheduler, AsyncTask, SimpleIntervalJob, CronJob } = toadScheduler;
 
 export default class ToadSchedulerWrapper {
-  constructor() {
-    this._scheduler = new ToadScheduler();
-  }
+  // constructor() {
+  //   this._scheduler = new ToadScheduler();
+  // }
 
   addIntervalJob({ name, schedule, preventOverrun, onProcess, onError }) {
-    const task = new AsyncTask(name, onProcess, onError);
-    const job = new SimpleIntervalJob(schedule, task);
-    this._scheduler.addSimpleIntervalJob(job, { preventOverrun });
+    // const task = new AsyncTask(name, onProcess, onError);
+    // const job = new SimpleIntervalJob(schedule, task);
+    // this._scheduler.addSimpleIntervalJob(job, { preventOverrun });
   }
 
   addCronJob({ name, cronExpression, preventOverrun, onProcess, onError }) {
-    const task = new AsyncTask(name, onProcess, onError);
-    const job = new CronJob({ cronExpression }, task, { preventOverrun });
-    this._scheduler.addCronJob(job);
+    // const task = new AsyncTask(name, onProcess, onError);
+    // const job = new CronJob({ cronExpression }, task, { preventOverrun });
+    // this._scheduler.addCronJob(job);
   }
 }
