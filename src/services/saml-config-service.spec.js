cf1ae7fea313d98abb3e5cd1ffca4d0cfdcb7459
Andreas Helmberger <public@ahelmberger.de>
2023-03-22 17:48:41 +0100

    EDU-1256 Use SAX streaming XML parser for memory efficient large file processing

--- src/services/saml-config-service.spec.js
+++ src/services/saml-config-service.spec.js
@@ -1,12 +1,9 @@
-import xmldom from 'xmldom';
 import { createSandbox } from 'sinon';
 import { fileURLToPath } from 'node:url';
 import { promises as fs } from 'node:fs';
 import SamlConfigService from './saml-config-service.js';
 import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, it } from 'vitest';
 import { destroyTestEnvironment, pruneTestEnvironment, setupTestEnvironment } from '../test-helper.js';
-
-const { DOMParser } = xmldom;
 
 describe('saml-config-service', () => {
   let sut;
@@ -31,24 +28,23 @@ describe('saml-config-service', () => {
   });
 
   describe('resolveMetadata', () => {
-    let testDoc;
+    let testXml;
 
     beforeEach(async () => {
       const testDocFileName = fileURLToPath(new URL('./saml-config-service-test-file.xml', import.meta.url));
-      const text = await fs.readFile(testDocFileName, 'utf8');
-      testDoc = new DOMParser().parseFromString(text);
+      testXml = await fs.readFile(testDocFileName, 'utf8');
     });
 
-    it('parses the metadata correctly for an existing entity ID', () => {
-      const metadata = sut.resolveMetadata(testDoc, 'https://samltest.id/saml/idp');
+    it('parses the metadata correctly for an existing entity ID', async () => {
+      const metadata = await sut.resolveMetadata(testXml, 'https://samltest.id/saml/idp');
       expect(metadata).toStrictEqual({
         entryPoint: 'https://samltest.id/idp/profile/SAML2/Redirect/SSO',
         certificates: [expect.any(String), expect.any(String), expect.any(String)]
       });
     });
 
-    it('returns null for a non-existing entity ID', () => {
-      const metadata = sut.resolveMetadata(testDoc, 'https://somewhere.over/the/rainbow');
+    it('returns null for a non-existing entity ID', async () => {
+      const metadata = await sut.resolveMetadata(testXml, 'https://somewhere.over/the/rainbow');
       expect(metadata).toBe(null);
     });
   });
