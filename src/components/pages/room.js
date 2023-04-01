e2d72174e3db246c30ef953143622224779ca3e2
Adriana Luchian <adriana.dumitras@gmail.de>
2023-04-01 15:12:45 +0200

    EDU-1279 Extract room-settings component

--- src/components/pages/room.js
+++ src/components/pages/room.js
@@ -1,37 +1,32 @@
 import PropTypes from 'prop-types';
 import Markdown from '../markdown.js';
+import { Tabs, Breadcrumb } from 'antd';
 import EmptyState from '../empty-state.js';
 import routes from '../../utils/routes.js';
 import Logger from '../../common/logger.js';
 import { useUser } from '../user-context.js';
 import FavoriteStar from '../favorite-star.js';
 import { useTranslation } from 'react-i18next';
-import MarkdownInput from '../markdown-input.js';
 import RoomMembers from '../room/room-members.js';
 import MessageBoard from '../room/message-board.js';
+import RoomSettings from '../room/room-settings.js';
 import RoomIcon from '../icons/general/room-icon.js';
 import RoomDocuments from '../room/room-documents.js';
 import WriteIcon from '../icons/general/write-icon.js';
-import RoomMetadataForm from '../room-metadata-form.js';
-import DeleteIcon from '../icons/general/delete-icon.js';
 import { handleApiError } from '../../ui/error-helper.js';
 import { FAVORITE_TYPE } from '../../domain/constants.js';
+import { confirmLeaveRoom } from '../confirmation-dialogs.js';
 import SettingsIcon from '../icons/main-menu/settings-icon.js';
-import { Button, Tabs, message, Breadcrumb, Form } from 'antd';
 import { TeamOutlined, UserOutlined } from '@ant-design/icons';
 import RoomApiClient from '../../api-clients/room-api-client.js';
 import { useSessionAwareApiClient } from '../../ui/api-helper.js';
 import { RoomMediaContextProvider } from '../room-media-context.js';
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { Fragment, useEffect, useMemo, useState } from 'react';
 import RoomExitedIcon from '../icons/user-activities/room-exited-icon.js';
-import IrreversibleActionsSection from '../irreversible-actions-section.js';
-import { confirmRoomDelete, confirmLeaveRoom } from '../confirmation-dialogs.js';
 import { isRoomInvitedCollaborator, isRoomOwner } from '../../utils/room-utils.js';
 import { roomShape, roomInvitationShape, documentExtendedMetadataShape, roomMediaContextShape } from '../../ui/default-prop-types.js';
 
 const logger = new Logger(import.meta.url);
-
-const FormItem = Form.Item;
 
 const VIEW_MODE = {
   owner: 'owner',
@@ -39,19 +34,20 @@ const VIEW_MODE = {
   nonCollaboratingMember: 'non-collaborating-member'
 };
 
+const TAB_KEYS = {
+  view: 'view',
+  members: 'members',
+  settings: 'settings'
+};
+
 export default function Room({ PageTemplate, initialState }) {
   const user = useUser();
-  const contentFormRef = useRef(null);
-  const metadataFormRef = useRef(null);
   const { t } = useTranslation('room');
   const roomApiClient = useSessionAwareApiClient(RoomApiClient);
 
   const [room, setRoom] = useState(initialState.room);
   const [membersCount, setMembersCount] = useState(initialState.room.members.length);
   const [invitationsCount, setInvitationsCount] = useState(initialState.invitations.length);
-
-  const [isRoomContentUpdateButtonDisabled, setIsRoomContentUpdateButtonDisabled] = useState(true);
-  const [isRoomMetadataUpdateButtonDisabled, setIsRoomMetadataUpdateButtonDisabled] = useState(true);
 
   const viewMode = useMemo(() => {
     if (isRoomOwner({ room, userId: user?._id })) {
@@ -67,15 +63,6 @@ export default function Room({ PageTemplate, initialState }) {
     history.replaceState(null, '', routes.getRoomUrl(room._id, room.slug));
   }, [room._id, room.slug]);
 
-  const handleRoomDelete = async () => {
-    try {
-      await roomApiClient.deleteRoom({ roomId: room._id });
-      window.location = routes.getDashboardUrl();
-    } catch (error) {
-      handleApiError({ error, t, logger });
-    }
-  };
-
   const handleLeaveRoom = async () => {
     try {
       await roomApiClient.deleteRoomMember({ roomId: room._id, memberUserId: user._id });
@@ -85,150 +72,134 @@ export default function Room({ PageTemplate, initialState }) {
     }
   };
 
-  const handleDeleteRoomClick = () => {
-    confirmRoomDelete(t, room.name, handleRoomDelete);
-  };
-
   const handleLeaveRoomClick = () => {
     confirmLeaveRoom(t, room.name, handleLeaveRoom);
   };
 
-  const handleUpdateRoomContentClick = () => {
-    if (contentFormRef.current) {
-      contentFormRef.current.submit();
-    }
+  const handleRoomSettingsChange = updatedRoom => {
+    setRoom(updatedRoom);
   };
 
-  const handleRoomContentFormFieldsChanged = () => {
-    setIsRoomContentUpdateButtonDisabled(false);
+  const handleRoomMembersChange = membersInfo => {
+    setMembersCount(membersInfo.membersCount);
+    setInvitationsCount(membersInfo.invitationsCount);
   };
 
-  const handleRoomContentFormSubmitted = async ({ overview }) => {
-    try {
-      const response = await roomApiClient.updateRoomContent({ roomId: room._id, overview });
+  const renderSubtitleLink = () => {
+    const ownerProfileUrl = routes.getUserProfileUrl(room.owner._id);
 
-      setRoom(response.room);
-      setIsRoomContentUpdateButtonDisabled(true);
-      message.success(t('common:changesSavedSuccessfully'));
-    } catch (error) {
-      handleApiError({ error, logger, t });
-    }
+    return (
+      <div>
+        {t('common:by')} <a className="RoomPage-subtitleLink" href={ownerProfileUrl}>{room.owner.displayName}</a>
+      </div>
+    );
   };
 
-  const handleUpdateRoomMetadataClick = () => {
-    if (metadataFormRef.current) {
-      metadataFormRef.current.submit();
-    }
-  };
-
-  const handleRoomMetadataFormSubmitted = async ({ name, slug, isCollaborative, shortDescription }) => {
-    try {
-      const response = await roomApiClient.updateRoomMetadata({ roomId: room._id, name, slug, isCollaborative, shortDescription });
-
-      setRoom(response.room);
-      setIsRoomMetadataUpdateButtonDisabled(true);
-      message.success(t('common:changesSavedSuccessfully'));
-    } catch (error) {
-      handleApiError({ error, logger, t });
-    }
-  };
-
-  const handleRoomMetadataFormFieldsChanged = () => {
-    setIsRoomMetadataUpdateButtonDisabled(false);
-  };
-
-  const handleRoomMembersChange = info => {
-    setMembersCount(info.membersCount);
-    setInvitationsCount(info.invitationsCount);
+  const renderLeaveRoomLink = () => {
+    return (
+      <a className="RoomPage-leaveRoomLink" onClick={handleLeaveRoomClick}>
+        <RoomExitedIcon />{t('cancelRoomMembership')}
+      </a>
+    );
   };
 
   const renderRoomOverview = () => {
+    const showEmptyState = viewMode === VIEW_MODE.owner && !room.overview;
+
+    if (showEmptyState) {
+      return (
+        <EmptyState
+          icon={<WriteIcon />}
+          title={t('overviewEmptyStateTitle')}
+          subtitle={t('overviewEmptyStateSubtitle')}
+          />
+      );
+    }
+
     return <Markdown className="RoomPage-overview">{room.overview}</Markdown>;
   };
 
-  const membersTabIcon = room.isCollaborative ? <TeamOutlined /> : <UserOutlined />;
-  const membersTabCount = invitationsCount
-    ? `${membersCount}/${membersCount + invitationsCount}`
-    : membersCount;
+  const renderRoomView = () => {
+    return (
+      <Fragment>
+        {renderRoomOverview()}
+        <RoomDocuments
+          roomId={room._id}
+          initialRoomDocumentIds={initialState.room.documents}
+          initialRoomDocuments={initialState.documents}
+          canDeleteDocuments={viewMode === VIEW_MODE.owner}
+          canManageDocuments={viewMode === VIEW_MODE.owner || viewMode === VIEW_MODE.collaboratingMember}
+          canManageDraftDocuments={viewMode === VIEW_MODE.owner}
+          />
+        <MessageBoard
+          roomId={room._id}
+          initialMessages={initialState.room.messages}
+          canManageMessages={viewMode === VIEW_MODE.owner}
+          />
+      </Fragment>
+    );
+  };
 
-  const membersTabTitle = room.isCollaborative
-    ? `${t('common:collaborators')} (${membersTabCount})`
-    : `${t('common:members')} (${membersTabCount}) `;
+  const getMembersTabTitle = () => {
+    const icon = room.isCollaborative ? <TeamOutlined /> : <UserOutlined />;
 
-  const showOverviewEmptyState = !room.overview;
+    const count = invitationsCount
+      ? `${membersCount}/${membersCount + invitationsCount}`
+      : membersCount;
+
+    const text = room.isCollaborative
+      ? `${t('common:collaborators')} (${count})`
+      : `${t('common:members')} (${count})`;
+
+    return `${icon}${text}`;
+  };
 
   return (
     <RoomMediaContextProvider context={initialState.roomMediaContext}>
       <PageTemplate>
         <div className="RoomPage">
           <Breadcrumb className="Breadcrumbs">
-            <Breadcrumb.Item href={routes.getDashboardUrl({ tab: 'rooms' })}>{t('common:roomsBreadcrumbPart')}</Breadcrumb.Item>
-            <Breadcrumb.Item>{room.name}</Breadcrumb.Item>
+            <Breadcrumb.Item href={routes.getDashboardUrl({ tab: 'rooms' })}>
+              {t('common:roomsBreadcrumbPart')}
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+              {room.name}
+            </Breadcrumb.Item>
           </Breadcrumb>
+
           <div className="RoomPage-title">
             <div>{room.name}</div>
             <div className="RoomPage-titleStar">
               <FavoriteStar type={FAVORITE_TYPE.room} id={room._id} />
             </div>
           </div>
+
           <div className="RoomPage-subtitle">
-            <div>
-              {t('common:by')} <a className="RoomPage-subtitleLink" href={routes.getUserProfileUrl(room.owner._id)}>{room.owner.displayName}</a>
-            </div>
-            {viewMode !== VIEW_MODE.owner && (
-              <a className="RoomPage-leaveRoomLink" onClick={handleLeaveRoomClick}><RoomExitedIcon />{t('cancelRoomMembership')}</a>
-            )}
+            {renderSubtitleLink()}
+            {viewMode !== VIEW_MODE.owner && renderLeaveRoomLink()}
           </div>
 
-          {viewMode !== VIEW_MODE.owner && (
-            <div className="RoomPage-documents RoomPage-documents--roomMemberView">
-              {renderRoomOverview()}
-              <RoomDocuments
-                roomId={room._id}
-                initialRoomDocumentIds={room.documents}
-                initialRoomDocuments={initialState.documents}
-                canDeleteDocuments={viewMode === VIEW_MODE.owner}
-                canManageDocuments={viewMode === VIEW_MODE.owner || viewMode === VIEW_MODE.collaboratingMember}
-                canManageDraftDocuments={viewMode === VIEW_MODE.owner}
-                />
-              <MessageBoard roomId={room._id} initialMessages={room.messages} canManageMessages={viewMode === VIEW_MODE.owner} />
-            </div>
-          )}
+          {viewMode !== VIEW_MODE.owner && renderRoomView()}
+
           {viewMode === VIEW_MODE.owner && (
             <Tabs
-              className="Tabs"
-              defaultActiveKey="1"
               type="line"
               size="middle"
+              className="Tabs"
+              defaultActiveKey="1"
               items={[
                 {
-                  key: '1',
+                  key: TAB_KEYS.view,
                   label: <div><RoomIcon />{t('roomViewTitle')}</div>,
                   children: (
                     <div className="Tabs-tabPane">
-                      {!!showOverviewEmptyState && (
-                        <EmptyState
-                          icon={<WriteIcon />}
-                          title={t('overviewEmptyStateTitle')}
-                          subtitle={t('overviewEmptyStateSubtitle')}
-                          />
-                      )}
-                      {!showOverviewEmptyState && renderRoomOverview()}
-                      <RoomDocuments
-                        roomId={room._id}
-                        initialRoomDocumentIds={room.documents}
-                        initialRoomDocuments={initialState.documents}
-                        canDeleteDocuments={viewMode === VIEW_MODE.owner}
-                        canManageDocuments={viewMode === VIEW_MODE.owner || viewMode === VIEW_MODE.collaboratingMember}
-                        canManageDraftDocuments={viewMode === VIEW_MODE.owner}
-                        />
-                      <MessageBoard roomId={room._id} initialMessages={room.messages} canManageMessages={viewMode === VIEW_MODE.owner} />
+                      {renderRoomView()}
                     </div>
                   )
                 },
                 {
-                  key: '2',
-                  label: <div>{membersTabIcon}{membersTabTitle}</div>,
+                  key: TAB_KEYS.members,
+                  label: <div>{getMembersTabTitle()}</div>,
                   children: (
                     <div className="Tabs-tabPane">
                       <RoomMembers
@@ -242,62 +213,11 @@ export default function Room({ PageTemplate, initialState }) {
                   )
                 },
                 {
-                  key: '3',
+                  key: TAB_KEYS.settings,
                   label: <div><SettingsIcon />{t('common:settings')}</div>,
                   children: (
                     <div className="Tabs-tabPane" >
-                      <div className="RoomPage-sectionHeadline">{t('roomContentHeadline')}</div>
-                      <section className="RoomPage-settingsSection">
-                        <Form
-                          layout="vertical"
-                          ref={contentFormRef}
-                          name="room-content-form"
-                          onFinish={handleRoomContentFormSubmitted}
-                          onFieldsChange={handleRoomContentFormFieldsChanged}
-                          >
-                          <FormItem label={t('overview')} name="overview" initialValue={room.overview}>
-                            <MarkdownInput preview />
-                          </FormItem>
-                        </Form>
-                        <Button
-                          type="primary"
-                          disabled={isRoomContentUpdateButtonDisabled}
-                          onClick={handleUpdateRoomContentClick}
-                          >
-                          {t('common:update')}
-                        </Button>
-                      </section>
-                      <div className="RoomPage-sectionHeadline">{t('roomMetadataHeadline')}</div>
-                      <section className="RoomPage-settingsSection">
-                        <RoomMetadataForm
-                          editMode
-                          room={room}
-                          formRef={metadataFormRef}
-                          onSubmit={handleRoomMetadataFormSubmitted}
-                          onFieldsChange={handleRoomMetadataFormFieldsChanged}
-                          />
-                        <Button
-                          type="primary"
-                          disabled={isRoomMetadataUpdateButtonDisabled}
-                          onClick={handleUpdateRoomMetadataClick}
-                          >
-                          {t('common:update')}
-                        </Button>
-                      </section>
-                      <IrreversibleActionsSection
-                        className="RoomPage-irreversibleActionsSection"
-                        actions={[
-                          {
-                            name: t('deleteRoomTitle'),
-                            description: t('deleteRoomDescription'),
-                            button: {
-                              text: t('deleteRoomButton'),
-                              icon: <DeleteIcon />,
-                              onClick: handleDeleteRoomClick
-                            }
-                          }
-                        ]}
-                        />
+                      <RoomSettings room={room} onChange={handleRoomSettingsChange} />
                     </div>
                   )
                 }
