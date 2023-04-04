2e93e7bfd84bfc4c9f04c074d4d8578a2b59f2f4
Andreas Helmberger <public@ahelmberger.de>
2023-04-04 18:48:45 +0200

    NOTICKET: Fix migration script

--- migrations/educandu-2023-04-04-02-rename-accreditedEditors-to-allowedEditors-in-documents.js
+++ migrations/educandu-2023-04-04-02-rename-accreditedEditors-to-allowedEditors-in-documents.js
@@ -4,14 +4,16 @@ export default class Educandu_2023_04_04_02_rename_accreditedEditors_to_allowedE
   }
 
   async up() {
+    const filterSpec = { publicContext: { $ne: null } };
     const updateSpec = { $rename: { 'publicContext.accreditedEditors': 'publicContext.allowedEditors' } };
-    await this.db.collection('documentRevisions').updateMany({}, updateSpec);
-    await this.db.collection('documents').updateMany({}, updateSpec);
+    await this.db.collection('documentRevisions').updateMany(filterSpec, updateSpec);
+    await this.db.collection('documents').updateMany(filterSpec, updateSpec);
   }
 
   async down() {
+    const filterSpec = { publicContext: { $ne: null } };
     const updateSpec = { $rename: { 'publicContext.allowedEditors': 'publicContext.accreditedEditors' } };
-    await this.db.collection('documentRevisions').updateMany({}, updateSpec);
-    await this.db.collection('documents').updateMany({}, updateSpec);
+    await this.db.collection('documentRevisions').updateMany(filterSpec, updateSpec);
+    await this.db.collection('documents').updateMany(filterSpec, updateSpec);
   }
 }
