d8d9b0bfe5b9b39373dff14534ae19dfad194c9e
Adriana Luchian <adriana.dumitras@gmail.de>
2023-10-16 13:50:09 +0200

    EDU-1358 Show document input info on dedicated page

--- src/components/pages/document-input.js
+++ src/components/pages/document-input.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Markdown from '../markdown.js';
+import routes from '../../utils/routes.js';
+import { useTranslation } from 'react-i18next';
+import { ALERT_TYPE } from '../custom-alert.js';
 import CreditsFooter from '../credits-footer.js';
 import SectionsDisplay from '../sections-display.js';
+import { useDateFormat } from '../locale-context.js';
 import PluginRegistry from '../../plugins/plugin-registry.js';
 import { ensurePluginComponentAreLoadedForSections } from '../../utils/plugin-utils.js';
 import { documentInputShape, documentRevisionShape } from '../../ui/default-prop-types.js';
@@ -23,10 +28,31 @@ class DocumentInputPreloader {
 }
 
 function DocumentInput({ initialState, PageTemplate }) {
+  const { formatDate } = useDateFormat();
+  const { t } = useTranslation('documentInput');
   const { documentInput, documentRevision } = initialState;
 
+  const userUrl = routes.getUserProfileUrl(documentInput.createdBy._id);
+  const documentUrl = routes.getDocUrl({ id: documentInput.documentId });
+
+  const alerts = [
+    {
+      message: (
+        <Markdown>{t('alertMarkdown', {
+          userName: documentInput.createdBy.displayName,
+          userUrl,
+          date: formatDate(documentInput.createdOn),
+          documentTitle: documentInput.documentTitle,
+          documentUrl
+        })}
+        </Markdown>
+      ),
+      type: ALERT_TYPE.info
+    }
+  ];
+
   return (
-    <PageTemplate >
+    <PageTemplate alerts={alerts}>
       <div>
         <SectionsDisplay
           documentInput={documentInput}
