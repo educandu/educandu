a859ec6f4ce4dce5e94db7f3ac53e6972a43078e
Andreas Helmberger <public@ahelmberger.de>
2023-10-17 18:34:21 +0200

    EDU-1366 Use existing drag and drop component for file upload

--- src/components/resource-selector/shared/upload-button.js
+++ src/components/resource-selector/shared/upload-button.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Button } from 'antd';
+import PropTypes from 'prop-types';
 import prettyBytes from 'pretty-bytes';
 import { useTranslation } from 'react-i18next';
 import UploadIcon from '../../icons/general/upload-icon.js';
 import { STORAGE_FILE_UPLOAD_LIMIT_IN_BYTES } from '../../../domain/constants.js';
 
-function UploadButton({ ...buttonProps }) {
+function UploadButton({ maxFiles, ...buttonProps }) {
   const { t } = useTranslation();
 
   return (
     <span className="UploadButton">
       <Button icon={<UploadIcon />} {...buttonProps}>{t('common:upload')}</Button>
-      <span className="UploadButton-uploadLimitInfo">{t('common:uploadLimitInfo', { limit: prettyBytes(STORAGE_FILE_UPLOAD_LIMIT_IN_BYTES) })}</span>
+      <span className="UploadButton-uploadLimitInfo">{t('common:uploadLimitInfo', { limit: prettyBytes(STORAGE_FILE_UPLOAD_LIMIT_IN_BYTES), maxFiles })}</span>
     </span>
   );
 }
+
+UploadButton.propTypes = {
+  maxFiles: PropTypes.number
+};
+
+UploadButton.defaultProps = {
+  maxFiles: 1
+};
 
 export default UploadButton;
--- src/plugins/file-upload-field/file-upload-field-display.js
+++ src/plugins/file-upload-field/file-upload-field-display.js
@@ -1,21 +1,33 @@
-import React, { useMemo } from 'react';
-import { Button, Form, Upload } from 'antd';
+import classNames from 'classnames';
+import { Form, Upload } from 'antd';
+import prettyBytes from 'pretty-bytes';
+import reactDropzoneNs from 'react-dropzone';
 import { useTranslation } from 'react-i18next';
-import { UploadOutlined } from '@ant-design/icons';
+import React, { useMemo, useRef } from 'react';
+import uniqueId from '../../utils/unique-id.js';
 import Markdown from '../../components/markdown.js';
+import { CloudUploadOutlined } from '@ant-design/icons';
+import EmptyState from '../../components/empty-state.js';
 import ClientConfig from '../../bootstrap/client-config.js';
 import { getAccessibleUrl } from '../../utils/source-utils.js';
 import { useService } from '../../components/container-context.js';
 import { sectionDisplayProps } from '../../ui/default-prop-types.js';
+import { STORAGE_FILE_UPLOAD_LIMIT_IN_BYTES } from '../../domain/constants.js';
 
+const ReactDropzone = reactDropzoneNs.default || reactDropzoneNs;
 const FormItem = Form.Item;
 
-export default function FileUploadFieldDisplay({ content, input, canModifyInput, onInputChanged }) {
+export default function FileUploadFieldDisplay({ context, content, input, canModifyInput, onInputChanged }) {
+  const dropzoneRef = useRef();
   const clientConfig = useService(ClientConfig);
   const { t } = useTranslation('fileUploadField');
 
-  const { label, maxCount, width } = content;
+  const { isPreview } = context;
   const { data, files } = input;
+  const { label, maxCount, width } = content;
+
+  const maximumAddableFiles = Math.max(0, maxCount - files.length);
+  const canAcceptFiles = !isPreview && canModifyInput && maximumAddableFiles > 0;
 
   const fileList = useMemo(() => files.map(file => ({
     uid: file.key,
@@ -27,23 +39,52 @@ export default function FileUploadFieldDisplay({ content, input, canModifyInput,
   const handleChange = ({ file }) => {
     if (file.status === 'removed') {
       onInputChanged(data, { removeFiles: [file.uid] });
-    } else if (files.length < maxCount) {
-      onInputChanged(data, { addFiles: [[file.uid, file]] });
     }
   };
 
-  const handleBeforeUpload = () => {
-    return false;
+  const handleFileDrop = newFiles => {
+    const filesToAdd = newFiles.filter((_file, index) => index < maximumAddableFiles).map(file => {
+      file.uid = uniqueId.create();
+      return [file.uid, file];
+    });
+    onInputChanged(data, { addFiles: filesToAdd });
   };
 
-  const uploadButton = (
-    <Button icon={<UploadOutlined />} disabled={files.length >= maxCount}>{t('common:upload')}</Button>
+  const handleUploadButtonClick = () => {
+    dropzoneRef.current.open();
+  };
+
+  const getDropzoneClasses = isDragActive => classNames(
+    'FileUploadFieldDisplay-dropzone',
+    { 'is-dropping': isDragActive && canAcceptFiles },
+    { 'is-drop-rejected': isDragActive && !canAcceptFiles }
   );
 
   return (
-    <div className={`u-horizontally-centered u-width-${width}`}>
+    <div className={`FileUploadFieldDisplay u-horizontally-centered u-width-${width}`}>
       <Form layout="vertical">
         <FormItem label={label ? <Markdown inline>{label}</Markdown> : null}>
+          {!!canModifyInput && (
+            <ReactDropzone ref={dropzoneRef} noClick noKeyboard onDrop={canAcceptFiles ? handleFileDrop : null}>
+              {({ getRootProps, getInputProps, isDragActive }) => (
+                <div {...getRootProps({ className: getDropzoneClasses(isDragActive) })}>
+                  <input {...getInputProps()} hidden />
+                  <EmptyState
+                    icon={<CloudUploadOutlined />}
+                    title={maxCount === 1 ? t('common:mediaUploadEmptyStateTitle') : t('common:mediaUploadMultipleEmptyStateTitle')}
+                    subtitle={maxCount === 1 ? t('common:mediaUploadEmptyStateSubtitle') : t('common:mediaUploadMultipleEmptyStateSubtitle')}
+                    button={{
+                      isDefaultType: true,
+                      isDisabled: !canAcceptFiles,
+                      text: t('common:browse'),
+                      subtext: t('common:uploadLimitInfo', { limit: prettyBytes(STORAGE_FILE_UPLOAD_LIMIT_IN_BYTES), maxFiles: maxCount }),
+                      onClick: handleUploadButtonClick
+                    }}
+                    />
+                </div>
+              )}
+            </ReactDropzone>
+          )}
           <Upload
             multiple
             listType="picture"
@@ -51,10 +92,7 @@ export default function FileUploadFieldDisplay({ content, input, canModifyInput,
             fileList={fileList}
             onChange={handleChange}
             disabled={!canModifyInput}
-            beforeUpload={handleBeforeUpload}
-            >
-            {uploadButton}
-          </Upload>
+            />
         </FormItem>
       </Form>
     </div>
