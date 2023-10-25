68338a66669aaaaea105e7ba2388fe5e6bc61d16
Andreas Helmberger <public@ahelmberger.de>
2023-10-25 13:40:04 +0200

    EDU-1398 Create document-input-media directory on startup

--- src/services/maintenance-service.spec.js
+++ src/services/maintenance-service.spec.js
@@ -67,7 +67,7 @@ describe('maintenance-service', () => {
       });
 
       it('should have created the initial CDN directories', () => {
-        assert.calledTwice(cdn.ensureDirectory);
+        assert.calledThrice(cdn.ensureDirectory);
       });
     });
 
