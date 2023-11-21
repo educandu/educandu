874b1e216a1ead6dbcac613e3d7ac7b904a11481
Andreas Helmberger <public@ahelmberger.de>
2023-11-21 12:03:40 +0100

    EDU-1434 Extract AudioContext cache

--- src/components/abc-player.js
+++ src/components/abc-player.js
@@ -5,10 +5,10 @@ import slugify from '@sindresorhus/slugify';
 import { useTranslation } from 'react-i18next';
 import { handleError } from '../ui/error-helper.js';
 import MediaPlayer from './media-player/media-player.js';
-import { AudioContext } from 'standardized-audio-context';
 import { MEDIA_SCREEN_MODE } from '../domain/constants.js';
 import React, { useEffect, useRef, useState } from 'react';
 import { useIsMounted, useOnComponentUnmount } from '../ui/hooks.js';
+import { useRunningAudioContext } from './media-player/media-hooks.js';
 import MediaPlayerProgressBar from './media-player/media-player-progress-bar.js';
 import { hasMoreWordCharactersThanNonWordCharacters } from '../utils/string-utils.js';
 import MediaPlayerControls, { MEDIA_PLAYER_CONTROLS_STATE } from './media-player/media-player-controls.js';
@@ -19,8 +19,6 @@ const synthOptions = {
   chordsOff: true,
   soundFontUrl: 'https://educandu.github.io/abcjs-soundfonts/FluidR3_Salamander_GM'
 };
-
-let globalAudioContext;
 
 const registerUrlForDisposal = url => url && setTimeout(() => URL.revokeObjectURL(url), 1000);
 
@@ -37,8 +35,8 @@ function AbcPlayer({ renderResult, initialVolume }) {
   const mediaPlayerRef = useRef(null);
   const lastRenderResult = useRef(null);
   const [soundUrl, setSoundUrl] = useState(null);
-  const [runningAudioContext, setRunningAudioContext] = useState(null);
   const [shouldPlayAfterRendering, setShouldPlayAfterRendering] = useState(false);
+  const [runningAudioContext, createRunningAudioContext] = useRunningAudioContext();
   const [downloadFileName, setDownloadTitle] = useState(createDownloadFileName(renderResult));
 
   lastRenderResult.current = renderResult;
@@ -46,26 +44,6 @@ function AbcPlayer({ renderResult, initialVolume }) {
   useEffect(() => {
     setShouldPlayAfterRendering(false);
   }, [renderResult]);
-
-  useEffect(() => {
-    if (!globalAudioContext) {
-      globalAudioContext = new AudioContext();
-    }
-
-    if (globalAudioContext.state !== 'running') {
-      const listener = () => {
-        if (globalAudioContext.state === 'running') {
-          setRunningAudioContext(globalAudioContext);
-        }
-      };
-
-      globalAudioContext.addEventListener('statechange', listener);
-      return () => globalAudioContext.removeEventListener('statechange', listener);
-    }
-
-    setRunningAudioContext(globalAudioContext);
-    return () => {};
-  }, []);
 
   useOnComponentUnmount(() => {
     registerUrlForDisposal(soundUrl);
@@ -96,7 +74,7 @@ function AbcPlayer({ renderResult, initialVolume }) {
 
   const handlePlayClick = () => {
     setShouldPlayAfterRendering(true);
-    setTimeout(() => globalAudioContext.resume(), 0);
+    createRunningAudioContext();
   };
 
   const handleMediaPlayerDuration = () => {
