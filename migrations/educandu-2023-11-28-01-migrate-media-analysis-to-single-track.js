21036a29427215b1016197936b6aa40f9499aede
Adriana Luchian <adriana.dumitras@gmail.de>
2023-11-28 15:12:16 +0100

    EDU-1464 Migrate media-analysis sections to single track

---
+++ migrations/educandu-2023-11-28-01-migrate-media-analysis-to-single-track.js
@@ -0,0 +1,89 @@
+export default class Educandu_2023_11_28_01_migrate_media_analysis_to_single_track {
+  constructor(db) {
+    this.db = db;
+  }
+
+  async collectionUp(collectionName) {
+    const documents = await this.db.collection(collectionName)
+      .find({ 'sections.type': 'media-analysis' })
+      .toArray();
+
+    let count = 0;
+
+    for (const doc of documents) {
+      const sectionsToProcess = doc.sections
+        .filter(section => section.type === 'media-analysis' && section.content?.tracks.length > 1);
+
+      if (sectionsToProcess.length) {
+        count += 1;
+
+        sectionsToProcess.forEach(section => {
+          const mainTrack = section.content.tracks[0];
+          section.content.sourceUrl = mainTrack.sourceUrl;
+          section.content.copyrightNotice = mainTrack.copyrightNotice;
+          section.content.playbackRange = mainTrack.playbackRange;
+
+          delete section.content.tracks;
+          delete section.content.volumePresets;
+        });
+
+        await this.db.collection(collectionName).replaceOne({ _id: doc._id }, doc);
+        console.log(`Updated ${collectionName} record with _id ${doc._id}.`);
+      }
+    }
+
+    console.log(`Updated ${count} ${collectionName}.`);
+  }
+
+  async collectionDown(collectionName) {
+    const documents = await this.db.collection(collectionName)
+      .find({ 'sections.type': 'media-analysis' })
+      .toArray();
+
+    let count = 0;
+
+    for (const doc of documents) {
+      const sectionsToProcess = doc.sections
+        .filter(section => section.type === 'media-analysis' && !!section.content);
+
+      if (sectionsToProcess.length) {
+        count += 1;
+
+        sectionsToProcess.forEach(section => {
+          section.content.tracks = [
+            {
+              sourceUrl: section.content.sourceUrl,
+              copyrightNotice: section.content.copyrightNotice,
+              playbackRange: section.content.playbackRange
+            }
+          ];
+          section.content.volumePresets = [
+            {
+              name: 'default',
+              tracks: [1]
+            }
+          ];
+
+          delete section.content.sourceUrl;
+          delete section.content.copyrightNotice;
+          delete section.content.playbackRange;
+        });
+
+        await this.db.collection(collectionName).replaceOne({ _id: doc._id }, doc);
+        console.log(`Updated ${collectionName} record with _id ${doc._id}.`);
+      }
+    }
+
+    console.log(`Updated ${count} ${collectionName}.`);
+  }
+
+  async up() {
+    await this.collectionUp('documents');
+    await this.collectionUp('documentRevisions');
+  }
+
+  async down() {
+    await this.collectionDown('documents');
+    await this.collectionDown('documentRevisions');
+  }
+}
