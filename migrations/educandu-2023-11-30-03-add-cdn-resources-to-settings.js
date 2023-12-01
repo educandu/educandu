8bfb2805c0f4cc6efebb224f74a0340adcab4a17
Adriana Luchian <adriana.dumitras@gmail.de>
2023-12-01 10:06:53 +0100

    NOTICKET: Fix esbuild problem due to (indirect) jsx import in migration file

--- migrations/educandu-2023-11-30-03-add-cdn-resources-to-settings.js
+++ migrations/educandu-2023-11-30-03-add-cdn-resources-to-settings.js
@@ -1,33 +1,13 @@
-import GithubFlavoredMarkdown from '../src/common/github-flavored-markdown.js';
-
 export default class Educandu_2023_11_30_03_add_cdn_resources_to_settings {
   constructor(db) {
     this.db = db;
-    this.gfm = new GithubFlavoredMarkdown();
-  }
-
-  _getFlattenedCdnResources(texts) {
-    const allCdnResources = texts.flatMap(text => this.gfm.extractCdnResources(text));
-    return [...new Set(allCdnResources)].sort();
-  }
-
-  _extractCdnResources(_id, value) {
-    switch (_id) {
-      case 'consentText':
-        return this._getFlattenedCdnResources(Object.values(value));
-      case 'pluginsHelpTexts':
-        return this._getFlattenedCdnResources(Object.values(value).flatMap(plugin => Object.values(plugin)));
-      default:
-        return [];
-    }
   }
 
   async up() {
     const cursor = this.db.collection('settings').find();
     while (await cursor.hasNext()) {
       const setting = await cursor.next();
-      const cdnResources = this._extractCdnResources(setting._id, setting.value);
-      await this.db.collection('settings').updateOne({ _id: setting._id }, { $set: { cdnResources } });
+      await this.db.collection('settings').updateOne({ _id: setting._id }, { $set: { cdnResources: [] } });
       console.log(`Migrated setting with ID '${setting._id}'`);
     }
   }
--- migrations/educandu-2023-11-30-04-add-cdn-resources-to-users.js
+++ migrations/educandu-2023-11-30-04-add-cdn-resources-to-users.js
@@ -1,17 +1,13 @@
-import GithubFlavoredMarkdown from '../src/common/github-flavored-markdown.js';
-
 export default class Educandu_2023_11_30_04_add_cdn_resources_to_users {
   constructor(db) {
     this.db = db;
-    this.gfm = new GithubFlavoredMarkdown();
   }
 
   async up() {
     const cursor = this.db.collection('users').find();
     while (await cursor.hasNext()) {
       const user = await cursor.next();
-      const cdnResources = this.gfm.extractCdnResources(user.profileOverview);
-      await this.db.collection('users').updateOne({ _id: user._id }, { $set: { cdnResources } });
+      await this.db.collection('users').updateOne({ _id: user._id }, { $set: { cdnResources: [] } });
       console.log(`Migrated user with ID '${user._id}'`);
     }
   }
