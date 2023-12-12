e2873c3662981bbe83f0caa4dd0b78c7e0659c06
Adriana Luchian <adriana.dumitras@gmail.de>
2023-12-12 17:16:41 +0100

    EDU-1438 Set up editor for player 2 tracks

--- src/plugins/combined-multitrack-media/combined-multitrack-media-editor.js
+++ src/plugins/combined-multitrack-media/combined-multitrack-media-editor.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import { Form } from 'antd';
+import { Button, Form } from 'antd';
 import Info from '../../components/info.js';
+import React, { useId, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
+import { PlusOutlined } from '@ant-design/icons';
+import cloneDeep from '../../utils/clone-deep.js';
 import ItemPanel from '../../components/item-panel.js';
 import { FORM_ITEM_LAYOUT } from '../../domain/constants.js';
 import { sectionEditorProps } from '../../ui/default-prop-types.js';
-import ObjectWidthSlider from '../../components/object-width-slider.js';
 import TrackEditor from '../../components/media-player/track-editor.js';
+import ObjectWidthSlider from '../../components/object-width-slider.js';
 import WarningIcon from '../../components/icons/general/warning-icon.js';
+import DragAndDropContainer from '../../components/drag-and-drop-container.js';
+import { createDefaultPlayer2Track } from './combined-multitrack-media-utils.js';
 import PlayerSettingsEditor from '../../components/media-player/player-settings-editor.js';
+import { moveItem, removeItemAt, replaceItemAt, swapItemsAt } from '../../utils/array-utils.js';
 
 function CombinedMultitrackMediaEditor({ content, onContentChanged }) {
+  const droppableIdRef = useRef(useId());
   const { t } = useTranslation('combinedMultitrackMedia');
 
-  const { player1, width } = content;
+  const { player1, player2, width } = content;
 
   const changeContent = newContentValues => {
     const newContent = { ...content, ...newContentValues };
@@ -24,6 +30,11 @@ function CombinedMultitrackMediaEditor({ content, onContentChanged }) {
     changeContent({ player1: { ...player1, track: newTrack } });
   };
 
+  const handlePlayer2TrackContentChange = (index, newTrack) => {
+    const newTracks = replaceItemAt(player2.tracks, newTrack, index);
+    changeContent({ player2: { ...player2, tracks: newTracks } });
+  };
+
   const handlePlayer1SettingsContentChange = newSettings => {
     changeContent({ player1: { ...player1, ...newSettings } });
   };
@@ -31,6 +42,83 @@ function CombinedMultitrackMediaEditor({ content, onContentChanged }) {
   const handleWidthChanged = newValue => {
     changeContent({ width: newValue });
   };
+
+  const handleMovePlayer2TrackUp = index => {
+    const newTracks = swapItemsAt(player2.tracks, index, index - 1);
+    const newVolumePresets = cloneDeep(player2.volumePresets);
+    newVolumePresets.forEach(preset => {
+      preset.tracks = swapItemsAt(preset.tracks, index, index - 1);
+    });
+    changeContent({ player2: { ...player2, tracks: newTracks, volumePresets: newVolumePresets } });
+  };
+
+  const handleMovePlayer2TrackDown = index => {
+    const newTracks = swapItemsAt(player2.tracks, index, index + 1);
+    const newVolumePresets = cloneDeep(player2.volumePresets);
+    newVolumePresets.forEach(preset => {
+      preset.tracks = swapItemsAt(preset.tracks, index, index + 1);
+    });
+    changeContent({ player2: { ...player2, tracks: newTracks, volumePresets: newVolumePresets } });
+  };
+
+  const handleMovePlayer2Track = (fromIndex, toIndex) => {
+    const newTracks = moveItem(player2.tracks, fromIndex, toIndex);
+    const newVolumePresets = cloneDeep(player2.volumePresets);
+    newVolumePresets.forEach(preset => {
+      preset.tracks = moveItem(preset.tracks, fromIndex, toIndex);
+    });
+    changeContent({ player2: { ...player2, tracks: newTracks, volumePresets: newVolumePresets } });
+  };
+
+  const handleAddPlayer2TrackButtonClick = () => {
+    const newTracks = cloneDeep(player2.tracks);
+    newTracks.push(createDefaultPlayer2Track());
+    const newVolumePresets = cloneDeep(player2.volumePresets);
+    newVolumePresets.forEach(preset => preset.tracks.push(1));
+    changeContent({ player2: { ...player2, tracks: newTracks, volumePresets: newVolumePresets } });
+  };
+
+  const handleDeletePlayer2Track = index => {
+    const newTracks = removeItemAt(player2.tracks, index);
+    const newVolumePresets = cloneDeep(player2.volumePresets);
+    newVolumePresets.forEach(preset => {
+      preset.tracks = removeItemAt(preset.tracks, index);
+    });
+    changeContent({ player2: { ...player2, tracks: newTracks, volumePresets: newVolumePresets } });
+  };
+
+  const dragAndDropPlayer2Tracks = player2.tracks.map((track, index) => {
+    const headerPrefix = t('playerNumberTrackNumber', { playerNumber: 2, trackNumber: index + 1 });
+    const header = `${headerPrefix}${track.name ? ': ' : ''}${track.name}`;
+
+    return {
+      key: track.key,
+      render: ({ dragHandleProps, isDragged, isOtherDragged }) => {
+        return (
+          <ItemPanel
+            collapsed
+            canDeleteLastItem={false}
+            header={header}
+            isDragged={isDragged}
+            isOtherDragged={isOtherDragged}
+            dragHandleProps={dragHandleProps}
+            index={index}
+            itemsCount={player2.tracks.length}
+            key={track.key}
+            onMoveUp={() => handleMovePlayer2TrackUp(index)}
+            onMoveDown={() => handleMovePlayer2TrackDown(index)}
+            onDelete={() => handleDeletePlayer2Track(index)}
+            >
+            <TrackEditor
+              content={track}
+              usePlaybackRange={false}
+              onContentChange={value => handlePlayer2TrackContentChange(index, value)}
+              />
+          </ItemPanel>
+        );
+      }
+    };
+  });
 
   return (
     <div className="MultitrackMediaEditor">
@@ -53,9 +141,15 @@ function CombinedMultitrackMediaEditor({ content, onContentChanged }) {
             />
         </ItemPanel>
 
-        <ItemPanel header={t('playerNumber', { number: 2 })}>
-          player2
-        </ItemPanel>
+        <DragAndDropContainer
+          droppableId={droppableIdRef.current}
+          items={dragAndDropPlayer2Tracks}
+          onItemMove={handleMovePlayer2Track}
+          />
+
+        <Button type="primary" icon={<PlusOutlined />} onClick={handleAddPlayer2TrackButtonClick}>
+          {t('common:addTrack')}
+        </Button>
 
         <Form.Item
           label={<Info tooltip={t('common:widthInfo')}>{t('common:width')}</Info>}
