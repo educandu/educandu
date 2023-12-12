a7522c911de14bb0ed2fab6d725a62a8ab18a3ae
Adriana Luchian <adriana.dumitras@gmail.de>
2023-12-12 16:08:35 +0100

    EDU-1438 God giveth combined-multitrack-media plugin

---
+++ src/plugins/combined-multitrack-media/combined-multitrack-media-editor.js
@@ -0,0 +1,217 @@
+import { Button, Form } from 'antd';
+import { useTranslation } from 'react-i18next';
+import { PlusOutlined } from '@ant-design/icons';
+import cloneDeep from '../../utils/clone-deep.js';
+import ItemPanel from '../../components/item-panel.js';
+import ClientConfig from '../../bootstrap/client-config.js';
+import { getAccessibleUrl } from '../../utils/source-utils.js';
+import { shouldDisableVideo } from '../../utils/media-utils.js';
+import React, { useId, useMemo, useRef, useState } from 'react';
+import { useService } from '../../components/container-context.js';
+import { sectionEditorProps } from '../../ui/default-prop-types.js';
+import TrackEditor from '../../components/media-player/track-editor.js';
+import WarningIcon from '../../components/icons/general/warning-icon.js';
+import { createDefaultPlayer1Track } from './combined-multitrack-media-utils.js';
+import DragAndDropContainer from '../../components/drag-and-drop-container.js';
+import { removeItemAt, swapItemsAt, moveItem } from '../../utils/array-utils.js';
+import TrackMixerEditor from '../../components/media-player/track-mixer-editor.js';
+import PlayerSettingsEditor from '../../components/media-player/player-settings-editor.js';
+import MultitrackMediaPlayer from '../../components/media-player/multitrack-media-player.js';
+
+function CombinedMultitrackMediaEditor({ content, onContentChanged }) {
+  const droppableIdRef = useRef(useId());
+  const clientConfig = useService(ClientConfig);
+  const { t } = useTranslation('combinedMultitrackMedia');
+
+  const [selectedVolumePresetIndex, setSelectedVolumePresetIndex] = useState(0);
+  const [disableVideo, setDisableVideo] = useState(shouldDisableVideo(content.tracks[0].sourceUrl));
+
+  const { tracks, volumePresets, showVideo, aspectRatio, posterImage, initialVolume } = content;
+
+  const sources = useMemo(() => {
+    return tracks.map(track => ({
+      ...track,
+      sourceUrl: getAccessibleUrl({ url: track.sourceUrl, cdnRootUrl: clientConfig.cdnRootUrl })
+    }));
+  }, [tracks, clientConfig]);
+
+  const changeContent = newContentValues => {
+    const newContent = { ...content, ...newContentValues };
+    const shouldDisableVideoOnNewUrl = shouldDisableVideo(newContent.sourceUrl);
+    const shouldDisableVideoOnOldUrl = shouldDisableVideo(content.sourceUrl);
+
+    const autoEnableVideo = !!shouldDisableVideoOnOldUrl && !shouldDisableVideoOnNewUrl;
+    const autoDisableVideo = !!shouldDisableVideoOnNewUrl;
+    newContent.showVideo = autoDisableVideo ? false : autoEnableVideo || newContent.showVideo;
+    newContent.posterImage = autoDisableVideo ? { sourceUrl: '' } : newContent.posterImage;
+
+    setDisableVideo(autoDisableVideo);
+    onContentChanged(newContent);
+  };
+
+  const handleTrackContentChange = (index, value) => {
+    const newTracks = cloneDeep(tracks);
+    newTracks[index] = value;
+    changeContent({ tracks: newTracks });
+  };
+
+  const handleMoveTrackUp = index => {
+    const newTracks = swapItemsAt(tracks, index, index - 1);
+    const newVolumePresets = cloneDeep(volumePresets);
+    newVolumePresets.forEach(preset => {
+      preset.tracks = swapItemsAt(preset.tracks, index, index - 1);
+    });
+    changeContent({ tracks: newTracks, volumePresets: newVolumePresets });
+  };
+
+  const handleMoveTrackDown = index => {
+    const newTracks = swapItemsAt(tracks, index, index + 1);
+    const newVolumePresets = cloneDeep(volumePresets);
+    newVolumePresets.forEach(preset => {
+      preset.tracks = swapItemsAt(preset.tracks, index, index + 1);
+    });
+    changeContent({ tracks: newTracks, volumePresets: newVolumePresets });
+  };
+
+  const handleMoveSecondaryTrack = (fromSecondaryTrackIndex, toSecondaryTrackIndex) => {
+    const fromIndex = fromSecondaryTrackIndex + 1;
+    const toIndex = toSecondaryTrackIndex + 1;
+    const newTracks = moveItem(tracks, fromIndex, toIndex);
+    const newVolumePresets = cloneDeep(volumePresets);
+    newVolumePresets.forEach(preset => {
+      preset.tracks = moveItem(preset.tracks, fromIndex, toIndex);
+    });
+    changeContent({ tracks: newTracks, volumePresets: newVolumePresets });
+  };
+
+  const handleDeleteTrack = index => {
+    const newTracks = removeItemAt(tracks, index);
+    const newVolumePresets = cloneDeep(volumePresets);
+    newVolumePresets.forEach(preset => {
+      preset.tracks = removeItemAt(preset.tracks, index);
+    });
+    changeContent({ tracks: newTracks, volumePresets: newVolumePresets });
+  };
+
+  const handleAddTrackButtonClick = () => {
+    const newTracks = cloneDeep(tracks);
+    newTracks.push(createDefaultPlayer1Track());
+    const newVolumePresets = cloneDeep(volumePresets);
+    newVolumePresets.forEach(preset => preset.tracks.push(1));
+    changeContent({ tracks: newTracks, volumePresets: newVolumePresets });
+  };
+
+  const handlePlayerSettingsContentChange = changedContent => {
+    changeContent(changedContent);
+  };
+
+  const handleSelectedVolumePresetChange = volumePresetIndex => {
+    setSelectedVolumePresetIndex(volumePresetIndex);
+  };
+
+  const handleVolumePresetsChange = updatedVolumePresets => {
+    changeContent({ volumePresets: updatedVolumePresets });
+  };
+
+  const secondaryTracksCount = tracks.length - 1;
+
+  const dragAndDropSecondaryTracks = tracks.slice(1).map((track, secondaryTrackIndex) => {
+    const headerPrefix = t('common:secondaryTrack', { number: secondaryTrackIndex + 2 });
+    const header = `${headerPrefix}${track.name ? ': ' : ''}${track.name}`;
+    const trackIndex = secondaryTrackIndex + 1;
+
+    return {
+      key: track.key,
+      render: ({ dragHandleProps, isDragged, isOtherDragged }) => {
+        return (
+          <ItemPanel
+            collapsed
+            canDeleteLastItem
+            header={header}
+            isDragged={isDragged}
+            isOtherDragged={isOtherDragged}
+            dragHandleProps={dragHandleProps}
+            index={secondaryTrackIndex}
+            itemsCount={secondaryTracksCount}
+            key={track.key}
+            onMoveUp={() => handleMoveTrackUp(trackIndex)}
+            onMoveDown={() => handleMoveTrackDown(trackIndex)}
+            onDelete={() => handleDeleteTrack(trackIndex)}
+            >
+            <TrackEditor
+              content={track}
+              usePlaybackRange={false}
+              onContentChange={value => handleTrackContentChange(trackIndex, value)}
+              />
+          </ItemPanel>
+        );
+      }
+    };
+  });
+
+  return (
+    <div className="CombinedMultitrackMediaEditor">
+      <Form layout="horizontal" labelAlign="left">
+        <div className="CombinedMultitrackMediaEditor-warning">
+          <WarningIcon className="CombinedMultitrackMediaEditor-warningIcon" />
+          {t('common:playerNotSupportedOnIOS')}
+        </div>
+        <ItemPanel
+          collapsed
+          key={tracks[0].key}
+          header={`${t('common:mainTrack')}${tracks[0].name ? ': ' : ''}${tracks[0].name}`}
+          >
+          <TrackEditor
+            content={tracks[0]}
+            onContentChange={value => handleTrackContentChange(0, value)}
+            />
+        </ItemPanel>
+        <DragAndDropContainer droppableId={droppableIdRef.current} items={dragAndDropSecondaryTracks} onItemMove={handleMoveSecondaryTrack} />
+
+        <Button type="primary" icon={<PlusOutlined />} onClick={handleAddTrackButtonClick}>
+          {t('common:addTrack')}
+        </Button>
+
+        <ItemPanel header={t('common:player')}>
+          <PlayerSettingsEditor
+            content={content}
+            disableVideo={disableVideo}
+            onContentChange={handlePlayerSettingsContentChange}
+            />
+        </ItemPanel>
+
+        <ItemPanel header={t('common:trackMixer')}>
+          <div className="CombinedMultitrackMediaEditor-trackMixerPreview">
+            <div className="CombinedMultitrackMediaEditor-trackMixerPreviewLabel">
+              {t('common:preview')}
+            </div>
+            <MultitrackMediaPlayer
+              aspectRatio={aspectRatio}
+              initialVolume={initialVolume}
+              posterImageUrl={getAccessibleUrl({ url: posterImage.sourceUrl, cdnRootUrl: clientConfig.cdnRootUrl })}
+              screenWidth={50}
+              selectedVolumePresetIndex={selectedVolumePresetIndex}
+              showVideo={!disableVideo && showVideo}
+              showTrackMixer={false}
+              sources={sources}
+              volumePresets={volumePresets}
+              />
+          </div>
+          <TrackMixerEditor
+            tracks={sources}
+            volumePresets={volumePresets}
+            onVolumePresetsChange={handleVolumePresetsChange}
+            selectedVolumePresetIndex={selectedVolumePresetIndex}
+            onSelectedVolumePresetIndexChange={handleSelectedVolumePresetChange}
+            />
+        </ItemPanel>
+      </Form>
+    </div>
+  );
+}
+
+CombinedMultitrackMediaEditor.propTypes = {
+  ...sectionEditorProps
+};
+
+export default CombinedMultitrackMediaEditor;
---
+++ src/plugins/combined-multitrack-media/combined-multitrack-media-info.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import cloneDeep from '../../utils/clone-deep.js';
+import MultitrackMediaIcon from './combined-multitrack-media-icon.js';
+import GithubFlavoredMarkdown from '../../common/github-flavored-markdown.js';
+import { isInternalSourceType, couldAccessUrlFromRoom } from '../../utils/source-utils.js';
+import { createDefaultContent, validateContent } from './combined-multitrack-media-utils.js';
+
+class CombinedMultitrackMediaInfo {
+  static dependencies = [GithubFlavoredMarkdown];
+
+  static typeName = 'combined-multitrack-media';
+
+  constructor(gfm) {
+    this.gfm = gfm;
+  }
+
+  getDisplayName(t) {
+    return t('combinedMultitrackMedia:name');
+  }
+
+  getIcon() {
+    return <MultitrackMediaIcon />;
+  }
+
+  async resolveDisplayComponent() {
+    return (await import('./combined-multitrack-media-display.js')).default;
+  }
+
+  async resolveEditorComponent() {
+    return (await import('./combined-multitrack-media-editor.js')).default;
+  }
+
+  getDefaultContent(t) {
+    return createDefaultContent(t);
+  }
+
+  validateContent(content) {
+    validateContent(content);
+  }
+
+  cloneContent(content) {
+    return cloneDeep(content);
+  }
+
+  redactContent(content, targetRoomId) {
+    const redactedContent = cloneDeep(content);
+
+    redactedContent.player1.track.copyrightNotice = this.gfm.redactCdnResources(
+      redactedContent.player1.track.copyrightNotice,
+      url => couldAccessUrlFromRoom(url, targetRoomId) ? url : ''
+    );
+
+    if (!couldAccessUrlFromRoom(redactedContent.player1.track.sourceUrl, targetRoomId)) {
+      redactedContent.player1.track.sourceUrl = '';
+    }
+
+    if (!couldAccessUrlFromRoom(redactedContent.player1.posterImage.sourceUrl, targetRoomId)) {
+      redactedContent.player1.posterImage.sourceUrl = '';
+    }
+
+    redactedContent.player2.tracks.forEach(track => {
+      track.copyrightNotice = this.gfm.redactCdnResources(
+        track.copyrightNotice,
+        url => couldAccessUrlFromRoom(url, targetRoomId) ? url : ''
+      );
+
+      if (!couldAccessUrlFromRoom(track.sourceUrl, targetRoomId)) {
+        track.sourceUrl = '';
+      }
+    });
+
+    return redactedContent;
+  }
+
+  getCdnResources(content) {
+    const cdnResources = [];
+
+    cdnResources.push(...this.gfm.extractCdnResources(content.player1.track.copyrightNotice));
+
+    if (isInternalSourceType({ url: content.player1.track.sourceUrl })) {
+      cdnResources.push(content.player1.track.sourceUrl);
+    }
+
+    if (isInternalSourceType({ url: content.player1.posterImage.sourceUrl })) {
+      cdnResources.push(content.player1.posterImage.sourceUrl);
+    }
+
+    content.player2.tracks.forEach(track => {
+      cdnResources.push(...this.gfm.extractCdnResources(track.copyrightNotice));
+
+      if (isInternalSourceType({ url: track.sourceUrl })) {
+        cdnResources.push(track.sourceUrl);
+      }
+    });
+
+    return [...new Set(cdnResources)].filter(cdnResource => cdnResource);
+  }
+}
+
+export default CombinedMultitrackMediaInfo;
