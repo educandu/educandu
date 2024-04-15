6e610a48c0e21a34160c80f7b711b9bae60a17fc
Andreas Helmberger <public@ahelmberger.de>
2024-04-15 11:25:25 +0200

    EDU-1541 Reset document requests collection

--- src/stores/collection-specs/document-requests.js
+++ src/stores/collection-specs/document-requests.js
@@ -1,9 +1,4 @@
 export default {
   name: 'documentRequests',
-  indexes: [
-    {
-      name: '_idx_documentId_registeredOn_registeredOnDayOfWeek_isWriteRequest_isLoggedInRequest',
-      key: { documentId: 1, registeredOn: 1, registeredOnDayOfWeek: 1, isWriteRequest: 1, isLoggedInRequest: 1 }
-    }
-  ]
+  indexes: []
 };
