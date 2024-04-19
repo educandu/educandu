a3af47211b54d5df6326e0d8b49a21f1c2aef8db
Adriana Luchian <adriana.dumitras@gmail.de>
2024-04-19 13:12:18 +0200

    EDU-1557 Add form to document category modal

--- src/components/maintenance/document-category-metadata-modal.js
+++ src/components/maintenance/document-category-metadata-modal.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import { Modal } from 'antd';
 import PropTypes from 'prop-types';
+import { Form, Input, Modal } from 'antd';
 import { useTranslation } from 'react-i18next';
+import React, { useMemo, useRef, useState } from 'react';
+import { maxDocumentCategoryNameLength } from '../../domain/validation-constants.js';
+
+function getValidationState({ name, t }) {
+  const isValidName = !!name.trim();
+
+  return {
+    name: {
+      required: true,
+      validateStatus: isValidName ? 'success' : 'error',
+      help: isValidName ? null : t('nameRequired')
+    }
+  };
+}
 
 function DocumentCategoryMetadataModal({ isOpen, isEditing, initialDocumentCategory, onSave, onClose }) {
+  const formRef = useRef(null);
   const { t } = useTranslation('documentCategoryMetadataModal');
 
+  const [name, setName] = useState(initialDocumentCategory.name);
+
+  const validationState = useMemo(
+    () => getValidationState({ t, name }),
+    [t, name]
+  );
+
+  const modalFormHasInvalidFields = () => Object.values(validationState).some(field => field.validateStatus === 'error');
+
   const handleOk = () => {
-    onSave();
+    if (formRef.current) {
+      formRef.current.submit();
+    }
   };
 
   const handleCancel = () => {
     onClose();
+  };
+
+  const handleModalFormFinish = () => {
+    if (modalFormHasInvalidFields()) {
+      return;
+    }
+
+    onSave();
+  };
+
+  const handleNameChange = event => {
+    const { value } = event.target;
+    setName(value);
+  };
+
+  const renderModalFormNameInputCount = ({ count, maxLength }) => {
+    return (
+      <div className="u-input-count">{`${count} / ${maxLength}`}</div>
+    );
   };
 
   return (
@@ -24,7 +68,16 @@ function DocumentCategoryMetadataModal({ isOpen, isEditing, initialDocumentCateg
       onOk={handleOk}
       onCancel={handleCancel}
       >
-      {initialDocumentCategory.name}
+      <Form ref={formRef} layout="vertical" onFinish={handleModalFormFinish} className="u-modal-body">
+        <Form.Item label={t('common:name')} {...validationState.name}>
+          <Input
+            value={name}
+            maxLength={maxDocumentCategoryNameLength}
+            showCount={{ formatter: renderModalFormNameInputCount }}
+            onChange={handleNameChange}
+            />
+        </Form.Item>
+      </Form>
     </Modal>
   );
 }
