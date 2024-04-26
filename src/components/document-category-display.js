1520039fc29e990f2f9da529a6b37fc1db48df4e
Adriana Luchian <adriana.dumitras@gmail.de>
2024-04-26 10:04:15 +0200

    EDU-1566 Reuse document-category-display component

--- src/components/document-category-display.js
+++ src/components/document-category-display.js
@@ -1,8 +1,8 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Markdown from './markdown.js';
 import routes from '../utils/routes.js';
+import React, { Fragment } from 'react';
 import slugify from '@sindresorhus/slugify';
 import { useTranslation } from 'react-i18next';
 import { useService } from './container-context.js';
@@ -10,13 +10,15 @@ import ClientConfig from '../bootstrap/client-config.js';
 import { getAccessibleUrl } from '../utils/source-utils.js';
 import { documentCategoryShape } from '../ui/default-prop-types.js';
 
-function DocumentCategoryDisplay({ documentCategory, bordered, detailed }) {
+function DocumentCategoryDisplay({ documentCategory, bordered, detailed, asLink }) {
   const { t } = useTranslation();
   const clientConfig = useService(ClientConfig);
 
   const iconUrl = documentCategory.iconUrl
     ? getAccessibleUrl({ url: documentCategory.iconUrl, cdnRootUrl: clientConfig.cdnRootUrl })
     : null;
+
+  const getDocumentCategoryUrl = () => routes.getDocumentCategoryUrl({ id: documentCategory._id, slug: slugify(documentCategory.name) });
 
   const rootClasses = classNames({
     'DocumentCategoryDisplay': true,
@@ -34,26 +36,42 @@ function DocumentCategoryDisplay({ documentCategory, bordered, detailed }) {
     'DocumentCategoryDisplay-name--detailed': detailed
   });
 
-  return (
-    <div className={rootClasses}>
-      <div>
-        {!!iconUrl && <img src={iconUrl} className={iconClasses} />}
-      </div>
-      <div>
-        <div className={nameClasses}>{documentCategory.name}</div>
-        {!!detailed && !!documentCategory.description && (
+  const renderContent = () => {
+    return (
+      <Fragment>
+        <div>
+          {!!iconUrl && <img src={iconUrl} className={iconClasses} />}
+        </div>
+        <div>
+          <div className={nameClasses}>{documentCategory.name}</div>
+          {!!detailed && !!documentCategory.description && (
           <Markdown className="DocumentCategoryDisplay-description">
             {documentCategory.description}
           </Markdown>
-        )}
-        {!!detailed && (
+          )}
+          {!!detailed && (
           <div className="DocumentCategoryDisplay-documentCategoryPageLink">
-            <a href={routes.getDocumentCategoryUrl({ id: documentCategory._id, slug: slugify(documentCategory.name) })}>
+            <a href={getDocumentCategoryUrl()}>
               {t('common:navigateToDocumentCategoryPage', { documentCategoryName: documentCategory.name })}
             </a>
           </div>
-        )}
-      </div>
+          )}
+        </div>
+      </Fragment>
+    );
+  };
+
+  if (asLink) {
+    return (
+      <a href={getDocumentCategoryUrl()} className={rootClasses}>
+        {renderContent()}
+      </a>
+    );
+  }
+
+  return (
+    <div className={rootClasses}>
+      {renderContent()}
     </div>
   );
 }
@@ -61,12 +79,14 @@ function DocumentCategoryDisplay({ documentCategory, bordered, detailed }) {
 DocumentCategoryDisplay.propTypes = {
   documentCategory: documentCategoryShape.isRequired,
   bordered: PropTypes.bool,
-  detailed: PropTypes.bool
+  detailed: PropTypes.bool,
+  asLink: PropTypes.bool
 };
 
 DocumentCategoryDisplay.defaultProps = {
   bordered: false,
-  detailed: false
+  detailed: false,
+  asLink: false
 };
 
 export default DocumentCategoryDisplay;
