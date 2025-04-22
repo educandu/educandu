d70c9aafea112079bcc332f8d6ce3d2c8e557843
Andreas Helmberger <public@ahelmberger.de>
2025-04-22 19:07:31 +0200

    Set reveal action for reveal effect in image plugins to switch

---
+++ migrations/educandu-2025-04-22-01-set-reveal-action-in-all-image-plugins-to-switch.js
@@ -0,0 +1,36 @@
+export default class Educandu_2025_04_22_01_set_reveal_action_in_all_image_plugins_to_switch {
+  constructor(db) {
+    this.db = db;
+  }
+
+  async collectionUp(collectionName) {
+    const result = await this.db.collection(collectionName).updateMany(
+      {},
+      {
+        $set: {
+          'sections.$[sectionElement].content.revealEffect.revealAction': 'switch'
+        }
+      },
+      {
+        arrayFilters: [
+          {
+            'sectionElement.type': 'image',
+            'sectionElement.content': { $ne: null }
+          }
+        ],
+        multi: true
+      }
+    );
+
+    console.log(`Updated ${collectionName}: ${JSON.stringify(result)}`);
+  }
+
+  async up() {
+    await this.collectionUp('documents');
+    await this.collectionUp('documentRevisions');
+  }
+
+  down() {
+    throw new Error('Not supported');
+  }
+}
